d1f6bb7578a9251a79312b342de3bdd8534e33e5
Dylan Broussard <deraw@outlook.fr>
2019-06-19 15:21:55 +0200

    👷 cli-helpers: Add build

---
+++ packages/cli-helpers/build.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs-extra';
+import { renderHeader } from './';
+import { author } from './package.json';
+
+renderHeader('Self Build', author.name);
+
+const { execSync } = require('child_process');
+
+/** Dist folder */
+const dist = 'bin';
+
+// Create dist folder if not exists
+if (!fs.existsSync(dist)) {
+	fs.mkdirSync(dist);
+}
+
+// Clear the content of dist folder
+fs.emptyDirSync(`./${dist}`);
+
+// Build the package
+execSync(
+	'tsc',
+	{
+		stdio: 'inherit'
+	}
+);
