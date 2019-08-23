83d89435ce2005650f44fa9174e420e75cc4aa88
Dylan Broussard <deraw@outlook.fr>
2019-08-23 15:57:22 +0200

    🏷️ vue-dot: Fix TS typings

--- packages/vue-dot/types/index.d.ts
+++ packages/vue-dot/types/index.d.ts
@@ -22,8 +22,12 @@ export interface VueDotOptions {
 	theme?: Theme;
 }
 
-export default interface VueDotPlugin {
+declare const VueDot: VueDotPlugin;
+export default VueDot;
+
+export interface VueDotPlugin {
 	install: PluginFunction<VueDotOptions>;
 }
 
+// Export generic type
 export type Refs<T extends object> = Vue['$refs'] & T;
