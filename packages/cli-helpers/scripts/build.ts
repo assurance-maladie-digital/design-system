a68a7046b922e7f6528b358c00fd6d83f46702ae
Dylan Broussard <deraw@outlook.fr>
2019-11-23 20:58:48 +0100

    🔧 cli-helpers: Update dist folder

--- packages/cli-helpers/scripts/build.ts
+++ packages/cli-helpers/scripts/build.ts
@@ -1,14 +1,14 @@
 import * as fs from 'fs-extra';
 import { execSync } from 'child_process';
 
-import renderHeader from '../src/renderHeader';
+import { renderHeader } from '../';
 
 import { author } from '../package.json';
 
 renderHeader('Self Build', author.name);
 
 /** Dist folder */
-const DIST_FOLDER = './bin';
+const DIST_FOLDER = './dist';
 
 // Create dist folder if not exists
 if (!fs.existsSync(DIST_FOLDER)) {
