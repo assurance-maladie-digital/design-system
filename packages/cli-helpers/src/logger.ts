6fbe0692160c3937c2631c2d0f414a4bc110c663
Dylan Broussard <deraw@outlook.fr>
2019-11-26 10:34:18 +0100

    ♻️ Refactor cli-helpers (#25)

---
+++ packages/cli-helpers/src/logger.ts
@@ -0,0 +1,74 @@
+// eslint-disable no-console
+// tslint:disable: no-console
+
+import chalk from 'chalk';
+
+import colors from './colors';
+
+/** Wrapper for console.log to avoid lint errors */
+export const log = console.log;
+
+/**
+ * Display a command
+ *
+ * @param {string} text The text to render
+ */
+export function cmd(text: string): void {
+	console.log(chalk.grey('$ ') + chalk.cyan(text));
+}
+
+/**
+ * Display an information
+ *
+ * @param {string} text The text to render
+ */
+export function info(text: string): void {
+	console.log(chalk.bgBlue.black(' INFO ') + ` ${chalk.blue(text)}`);
+}
+
+/**
+ * Display a success message (done)
+ *
+ * @param {string} text The text to render
+ */
+export function done(text: string): void {
+	console.log(chalk.bgGreen.black(' DONE ') + ` ${chalk.green(text)}`);
+}
+
+/**
+ * Display a warning
+ *
+ * @param {string} text The text to render
+ */
+export function warn(text: string): void {
+	console.log(chalk.bgYellow.black(' WARN ') + ` ${chalk.yellow(text)}`);
+}
+
+/**
+ * Display an error
+ *
+ * @param {string} text The text to render
+ */
+export function error(text: string): void {
+	console.log(chalk.bgRed(' ERROR ') + ` ${chalk.red(text)}`);
+}
+
+/**
+ * Display a verbose log
+ *
+ * @param {string} text The text to render
+ */
+export function verbose(text: string): void {
+	console.log(chalk.bgWhite.black(' VERBOSE ') + ` ${chalk.hex(colors.lightGrey)(text)}`);
+}
+
+/**
+ * Display an event
+ *
+ * @param {string} text The text to render
+ */
+export function event(text: string): void {
+	console.log(chalk.bold.white(text));
+}
+
+export default log;
---
+++ packages/cli-helpers/tests/renderHeader.spec.ts
@@ -0,0 +1,33 @@
+import { mockConsoleLog } from 'jest-mock-process';
+
+import renderHeader from '@/renderHeader';
+
+describe('renderHeader', () => {
+	let mockLog: jest.SpyInstance;
+
+	beforeEach(() => {
+		mockLog = mockConsoleLog();
+	});
+
+	it('should render the header correctly', () => {
+		renderHeader('test');
+
+		expect(mockLog).toHaveBeenCalled();
+	});
+
+	it('should render the header correctly with an author', () => {
+		renderHeader('test', 'author');
+
+		expect(mockLog).toHaveBeenCalled();
+	});
+
+	it('should render the header correctly with a version', () => {
+		renderHeader('test', undefined, '1.0.0');
+
+		expect(mockLog).toHaveBeenCalled();
+	});
+
+	afterAll(() => {
+		mockLog.mockRestore();
+	});
+});
