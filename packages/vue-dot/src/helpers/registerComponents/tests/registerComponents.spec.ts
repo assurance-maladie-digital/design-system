d255f597b0bef5472ea069c6f668cd61de454fae
Dylan Broussard <deraw@outlook.fr>
2019-11-27 12:01:57 +0100

    ♻️ vue-dot: Refactor registerComponents

---
+++ packages/vue-dot/src/helpers/registerComponents/tests/registerComponents.spec.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+
+import registerComponents, { Components } from '../';
+
+import { getComponents } from '@/tests/integration/utils';
+
+function createTestComponent(name: string) {
+	return Vue.component(name, {
+		template: '<div />'
+	});
+}
+
+const components: Components = {
+	TestA: createTestComponent('TestA'),
+	TestB: createTestComponent('TestB')
+};
+
+describe('registerComponents', () => {
+	it('should register the components', () => {
+		registerComponents(Vue, components);
+
+		expect(getComponents(Vue)).toMatchSnapshot();
+	});
+});
