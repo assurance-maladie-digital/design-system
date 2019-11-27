389b983c64379727e1988e516ad578cf93492f4a
Dylan Broussard <deraw@outlook.fr>
2019-11-27 10:52:43 +0100

    ♻️ vue-dash: Use PageCard from Vue Dot

--- packages/vue-cli-plugin-vue-dash/generator/functions/extendPackage.js
+++ packages/vue-cli-plugin-vue-dash/generator/functions/extendPackage.js
@@ -7,34 +7,34 @@ function extendPackage(api, options) {
 		dependencies: {
 			'@cnamts/vue-dot': `^${VueDotVersion}`,
 			'axios': '^0.19.0',
-			'core-js': '^3.4.2',
+			'core-js': '^3.4.4',
 			'dayjs': '^1.8.17',
 			'languages': '^0.1.3',
 			'vue-class-component': '^7.1.0',
 			'vue-meta': '^2.3.1',
 			'vue-the-mask': '^0.11.1',
-			'vuetify': '^2.1.11'
+			'vuetify': '^2.1.12'
 		},
 		devDependencies: {
-			'@babel/core': '^7.7.2',
-			'@babel/preset-env': '^7.7.1',
+			'@babel/core': '^7.7.4',
+			'@babel/preset-env': '^7.7.4',
 			'@types/webfontloader': '^1.6.29',
-			'@mdi/js': '^4.5.95',
-			'@typescript-eslint/eslint-plugin': '^2.8.0',
-			'@typescript-eslint/parser': '^2.8.0',
+			'@mdi/js': '^4.6.95',
+			'@typescript-eslint/eslint-plugin': '^2.9.0',
+			'@typescript-eslint/parser': '^2.9.0',
 			'babel-jest': '^24.9.0',
 			'jest': '^24.9.0',
 			'jest-serializer-vue': '^2.0.2',
-			'ts-jest': '^24.1.0',
+			'ts-jest': '^24.2.0',
 			'vue-cli-plugin-vuetify': '^2.0.2',
-			'vuetify-loader': '^1.3.0',
+			'vuetify-loader': '^1.4.2',
 			'webfontloader': '^1.6.28',
 			'webpack': '^4.41.2'
 		}
 	};
 
 	if (options.i18n) {
-		newPackageProperties.dependencies['vue-i18n'] = '^8.15.0';
+		newPackageProperties.dependencies['vue-i18n'] = '^8.15.1';
 	}
 
 	if (options.vuexPersist) {
