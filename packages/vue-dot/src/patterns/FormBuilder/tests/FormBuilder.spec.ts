670954cadbd587fdfc365d787eaf418c8e37eb2b
Baptiste Coulon <bcoulon72@gmail.com>
2019-12-23 10:48:22 +0100

    update formBuilder test with render form question update filedMap test snap

---
+++ packages/vue-dot/src/patterns/FormBuilder/tests/FormBuilder.spec.ts
@@ -0,0 +1,90 @@
+import Vue from 'vue';
+import { Wrapper } from '@vue/test-utils';
+
+import { mountComponent } from '@/tests';
+import html from '@/tests/html';
+
+import FormBuilder from '../FormBuilder.vue';
+
+import FormField from '../../FormField';
+
+let wrapper: Wrapper<Vue>;
+
+const questionForm = {
+	question_string: {
+		type: 'string',
+		title: 'Question ?',
+		description: 'Informations supplémentaires',
+		tooltip: 'Texte d\'aide',
+		value: null,
+		metadata: {
+			label: 'Label du champ'
+		}
+	},
+	questionDate: {
+		type: 'date',
+		title: 'Question',
+		description: 'Informations supplémentaires',
+		tooltip: 'date info',
+		value: null,
+		metadata: {
+			label: 'date'
+		}
+	}
+};
+
+const testForm = {
+	question: {
+		title: 'Test ?',
+		type: 'string',
+		value: null,
+		description: 'Test description',
+		tooltip: 'Test',
+		metadata: {
+			label: 'Question field',
+			placeholder: 'Value example',
+			outlined: true
+		}
+	},
+	field: {
+		type: 'string',
+		value: null,
+		metadata: {
+			label: 'Classic field',
+			outlined: true
+		}
+	}
+};
+
+// Tests
+describe('FormBuilder', () => {
+	it('renders form classic correctly', () => {
+		// Mount component
+		wrapper = mountComponent(FormBuilder, {
+			stubs: {
+				FormField
+			},
+			propsData: {
+				value: testForm
+			}
+		}, true);
+
+		expect(html(wrapper)).toMatchSnapshot();
+	});
+
+	it('renders correctly with a specified question layout', () => {
+		// Mount component
+		wrapper = mountComponent(FormBuilder, {
+			stubs: {
+				FormField
+			},
+			propsData: {
+				value: questionForm,
+				defaultLayout:'question'
+
+			}
+		}, true);
+
+		expect(html(wrapper)).toMatchSnapshot();
+	});
+});
