8c7caed60f1a63eafb9eae374a8a24b433516781
Dylan Broussard <deraw@outlook.fr>
2020-01-29 11:15:47 +0100

    ✨ vue-dash: Configure Vuetify to FR by default (#73)

--- packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vuetify.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vuetify.ts
@@ -3,6 +3,8 @@ import Vue from 'vue';
 // Import vuetify/lib to use vuetify-loader
 import Vuetify from 'vuetify/lib';
 
+import fr from 'vuetify/src/locale/fr';
+
 // Import theme colors
 import colors from '@/theme/colors';
 
@@ -17,5 +19,9 @@ export default new Vuetify({
 	},
 	icons: {
 		iconfont: 'mdiSvg'
+	},
+	lang: {
+		locales: { fr },
+		current: 'fr'
 	}
 });
