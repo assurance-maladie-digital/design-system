d7af3e7c9a0a376a1153d09a2f7b77b2611816be
Dylan Broussard <deraw@outlook.fr>
2020-04-16 17:18:10 +0200

    ♻️ vue-dot: Update CopyBtn API

--- packages/vue-dot/src/elements/CopyBtn/config.ts
+++ packages/vue-dot/src/elements/CopyBtn/config.ts
@@ -2,13 +2,9 @@ export const config = {
 	menu: {
 		offsetX: true,
 		zIndex: 8,
-		retainFocusOnClick: true,
 		contentClass: 'vd-copy-tooltip-menu white--text body-2 ml-2'
 	},
 	btn: {
 		icon: true
-	},
-	icon: {
-		color: 'grey darken-3'
 	}
 };
