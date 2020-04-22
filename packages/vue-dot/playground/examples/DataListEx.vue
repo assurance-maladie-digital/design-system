ec8f28d255932216573da019f6bc793fb7af3145
Dylan Broussard <deraw@outlook.fr>
2020-04-22 17:07:50 +0200

    ♻️ vue-dot: Refactor DataList (#155)

--- packages/vue-dot/playground/examples/DataListEx.vue
+++ packages/vue-dot/playground/examples/DataListEx.vue
@@ -1,38 +1,8 @@
 <template>
 	<DocSection title="DataList">
 		<DataList
-			:list="[
-				{
-					key: 'Civility',
-					value: 'Mr'
-				},
-				{
-					key: 'Name',
-					value: 'Dupont'
-				},
-				{
-					key: 'First name',
-					value: 'Paul'
-				},
-				{
-					key: 'Birth date',
-					value: '09/24/1970'
-				},
-				{
-					key: 'Nationality',
-					value: 'French'
-				},
-				{
-					key: 'Native country',
-					value: 'France'
-				},
-				{
-					key: 'Date of registration',
-					value: ''
-				}
-			]"
-			:label-color="dark ? 'white' : '#757575'"
-			:value-color="dark ? '#ccc' : '#333'"
+			:list="data"
+			:icons="icons"
 			list-title="Informations"
 			flex
 		/>
@@ -43,10 +13,52 @@
 	import Vue from 'vue';
 	import Component from 'vue-class-component';
 
+	import { IDataListItem } from '../../src/elements/DataList/types';
+
+	import { mdiCalendar } from '@mdi/js';
+
 	@Component
 	export default class DataListEx extends Vue {
-		get dark() {
-			return this.$vuetify.theme.dark;
-		}
+		data: IDataListItem[] = [
+			{
+				key: 'Civility',
+				value: 'Mr',
+				chip: true,
+				options: {
+					chip: {
+						color: 'success'
+					}
+				}
+			},
+			{
+				key: 'Name',
+				value: 'Dupont'
+			},
+			{
+				key: 'First name',
+				value: 'Paul'
+			},
+			{
+				key: 'Birthdate',
+				value: '09/24/1970',
+				icon: 'mdiCalendar'
+			},
+			{
+				key: 'Nationality',
+				value: 'French'
+			},
+			{
+				key: 'Native country',
+				value: 'France'
+			},
+			{
+				key: 'Date of registration',
+				value: ''
+			}
+		];
+
+		icons = {
+			mdiCalendar
+		};
 	}
 </script>
