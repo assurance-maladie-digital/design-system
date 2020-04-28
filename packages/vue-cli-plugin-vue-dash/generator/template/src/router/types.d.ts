426e88ce4dff63277671cca42d62703b532c0e81
baptiste coulon <bcoulon72@gmail.com>
2020-04-28 14:44:21 +0200

    fix(vue-dash): router review

--- packages/vue-cli-plugin-vue-dash/generator/template/src/router/types.d.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/router/types.d.ts
@@ -1,3 +1,4 @@
+import Vue from 'vue';
 import { RawLocation } from 'vue-router';
 
-export type Next = (to?: RawLocation | false | ((vm: V) => any) | void) => void;
+export type Next = (to?: RawLocation | false | ((vm: Vue) => any) | void) => void;
