3eb5798b2bd4531683758577d9bd40a78fee3ba5
Coulon Baptiste <bcoulon72@gmail.com>
2020-05-05 19:42:56 +0200

    ✨ vue-dot: Add rule exact length (#203)

---
+++ packages/vue-dot/src/rules/exactLength/tests/exactLength.spec.ts
@@ -0,0 +1,48 @@
+import { exactLength } from '../';
+
+// Tests
+describe('exactLength', () => {
+	it('returns an error when the value is longer than the exact length', () => {
+		const rule = exactLength(2);
+
+		expect(typeof rule('0 1')).toBe('string');
+	});
+
+	it('returns an error when the value is smaller than the exact length', () => {
+		const rule = exactLength(2);
+
+		expect(typeof rule('s')).toBe('string');
+	});
+
+	it('returns true when the value is the exact length without counting spaces', () => {
+		const rule = exactLength(2, true);
+
+		expect(rule('x x')).toBe(true);
+	});
+
+	it('returns true when the value is equal than the exact length', () => {
+		const rule = exactLength(2);
+
+		expect(rule('01')).toBe(true);
+	});
+
+	it('returns true if the value is empty', () => {
+		const rule = exactLength(10);
+
+		expect(rule('')).toBe(true);
+	});
+
+	it('returns true if the value is undefined', () => {
+		const rule = exactLength(10);
+
+		expect(rule(undefined)).toBe(true);
+	});
+
+	it('works with custom error messages', () => {
+		const rule = exactLength(1, false, {
+			default: 'test'
+		});
+
+		expect(rule('01')).toBe('test');
+	});
+});
