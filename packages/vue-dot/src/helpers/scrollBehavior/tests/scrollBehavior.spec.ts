97964a2d2b9346a09bd494f474f4e61f5caf52e1
Dylan Broussard <deraw@outlook.fr>
2020-05-07 14:46:02 +0200

    Apply suggestions from code review

--- packages/vue-dot/src/helpers/scrollBehavior/tests/scrollBehavior.spec.ts
+++ packages/vue-dot/src/helpers/scrollBehavior/tests/scrollBehavior.spec.ts
@@ -3,38 +3,38 @@ import { Route } from 'vue-router';
 import { Position } from 'vue-router/types/router';
 
 const defaultRoute: Route = {
-    path: '/',
-    hash: '',
-    params: {},
-    query: {},
-    fullPath: '/',
-    matched: []
+	path: '/',
+	hash: '',
+	params: {},
+	query: {},
+	fullPath: '/',
+	matched: []
 };
 
 const defaultPosition: Position = {
-    x:0,
-    y:0
+	x: 0,
+	y: 0
 };
 
 const savedPosition: Position = {
-    x:1,
-    y:1
+	x: 1,
+	y: 1
 };
 
 // Tests
 describe('scrollBehavior', () => {
-    it('returns the default scroll postion', () => {
+	it('returns the default scroll postion', () => {
 		expect(scrollBehavior(defaultRoute, defaultRoute)).toStrictEqual(defaultPosition);
-    });
+	});
 
-    it('returns the selector custom hash instead of saved position', () => {
-        const customHashSelector = '#customHashSelector';
-        const hashRoute = { ...defaultRoute, hash: customHashSelector };
+	it('returns the selector custom hash instead of saved position', () => {
+		const customHashSelector = '#customHashSelector';
+		const hashRoute = { ...defaultRoute, hash: customHashSelector };
 
 		expect(scrollBehavior(hashRoute, defaultRoute, savedPosition)).toStrictEqual({ selector: customHashSelector });
-    });
+	});
 
-    it('returns the saved position', () => {
+	it('returns the saved position', () => {
 		expect(scrollBehavior(defaultRoute, defaultRoute,savedPosition)).toBe(savedPosition);
-    });
+	});
 });
