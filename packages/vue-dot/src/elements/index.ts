42032a90a7da8eccce2168095c00a64ff9b89819
Dylan Broussard <deraw@outlook.fr>
2020-05-19 12:36:15 +0200

    🏗 vue-dot: Move PageCard to elements

--- packages/vue-dot/src/elements/index.ts
+++ packages/vue-dot/src/elements/index.ts
@@ -2,10 +2,12 @@ import CopyBtn from './CopyBtn';
 import CustomIcon from './CustomIcon';
 import DataList from './DataList';
 import HeaderLoading from './HeaderLoading';
+import PageCard from './PageCard';
 
 export const elements = {
 	CopyBtn,
 	CustomIcon,
 	DataList,
-	HeaderLoading
+	HeaderLoading,
+	PageCard
 };
