cbf5e57c29489d3475d1234d690d72c14076ca9f
Dylan Broussard <deraw@outlook.fr>
2020-05-19 21:37:36 +0200

    🏗 vue-dot: Move PageCard to elements (#258)

--- packages/vue-dot/src/patterns/index.ts
+++ packages/vue-dot/src/patterns/index.ts
@@ -4,7 +4,6 @@ import FileUpload from './FileUpload';
 import FooterWrapper from './FooterWrapper';
 import LangBtn from './LangBtn';
 import NotificationBar from './NotificationBar';
-import PageCard from './PageCard';
 import PaginatedTable from './PaginatedTable';
 import SubHeader from './SubHeader';
 import UploadWorkflow from './UploadWorkflow';
@@ -16,7 +15,6 @@ export const patterns = {
 	FooterWrapper,
 	LangBtn,
 	NotificationBar,
-	PageCard,
 	PaginatedTable,
 	SubHeader,
 	UploadWorkflow
