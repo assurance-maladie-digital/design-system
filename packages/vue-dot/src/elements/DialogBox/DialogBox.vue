53f7bd3cf999dcc798f6e33bca6eaad26dfec2d6
Yvon MICHEL <44802047+mmi16389@users.noreply.github.com>
2020-05-19 13:12:31 +0200

    Update packages/vue-dot/src/elements/DialogBox/DialogBox.vue
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/vue-dot/src/elements/DialogBox/DialogBox.vue
+++ packages/vue-dot/src/elements/DialogBox/DialogBox.vue
@@ -83,7 +83,7 @@
 	});
 	@Component
 	export default class DialogBox extends Props {
-		closeIcon = mdiClose; // default icon
+		closeIcon = mdiClose;
 		phoneIcon = mdiPhone; // default icon for iphone
 		locales = locales;
 		/**
