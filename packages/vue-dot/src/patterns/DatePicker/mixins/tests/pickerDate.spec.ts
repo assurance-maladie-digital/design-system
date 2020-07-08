ebadf9654d3e1de5dd6721aad52e59ae2d5a8f2a
Dylan Broussard <deraw@outlook.fr>
2020-07-08 23:27:09 +0200

    🚚 vue-dot: Move DatePicker mixins tests to dedicated folder

--- packages/vue-dot/src/patterns/DatePicker/mixins/tests/pickerDate.spec.ts
+++ packages/vue-dot/src/patterns/DatePicker/mixins/tests/pickerDate.spec.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { mount, Wrapper } from '@vue/test-utils';
 
-import { PickerDate } from '../mixins/pickerDate';
+import { PickerDate } from '../pickerDate';
 
 interface TestComponent extends Vue {
 	pickerDate: string;
