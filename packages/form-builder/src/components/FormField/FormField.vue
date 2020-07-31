52df15d07bf8ed25a67e9e4da98156cb9faa08a4
Coulon Baptiste <bcoulon72@gmail.com>
2020-07-31 17:33:23 +0200

    🐛 form-builder: Handle subtypes that are not in type select (#480)
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/form-builder/src/components/FormField/FormField.vue
+++ packages/form-builder/src/components/FormField/FormField.vue
@@ -87,7 +87,15 @@
 		 * @returns {string} The field type
 		 */
 		getFieldType(): string {
-			return this.getField(this.field.type);
+			let fieldType: string = this.field.type;
+
+			// Handle subtypes that are not in type 'select'
+			if (fieldType !== 'select') {
+				const fieldOptionsType = this.field.fieldOptions?.type as string || undefined;
+				fieldType = fieldOptionsType || fieldType;
+			}
+
+			return this.getField(fieldType);
 		}
 
 		/**
