9399134e1954e271af9db386f1c836fd93fd7e4c
Dylan Broussard <deraw@outlook.fr>
2020-08-04 17:04:25 +0200

    🏗 Extract Design Tokens to dedicated package (#483)

---
+++ packages/design-tokens/scripts/modules.d.ts
@@ -0,0 +1,1 @@
+declare module 'json-to-scss/lib/jsValueToSassString';
---
+++ packages/design-tokens/src/tokens.ts
@@ -0,0 +1,18 @@
+import { Tokens } from './types';
+
+import { colors } from './colors';
+
+export const tokens: Tokens = {
+	colors,
+	dialogWidth: {
+		dialogSmall: '560px',
+		dialogMedium: '800px'
+	},
+	pageWidth: '1440px',
+	inputWidth: {
+		inputSmall: '128px',
+		inputMedium: '328px',
+		inputLarge: '384px',
+		inputXLarge: '536px'
+	}
+};
