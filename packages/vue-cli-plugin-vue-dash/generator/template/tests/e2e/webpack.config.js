1c3fc65a618071fd2436c8ce62ec95400c944bfe
Yvon MICHEL <44802047+mmi16389@users.noreply.github.com>
2020-10-05 21:14:37 +0200

    Update packages/vue-cli-plugin-vue-dash/generator/template/tests/e2e/webpack.config.js
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/vue-cli-plugin-vue-dash/generator/template/tests/e2e/webpack.config.js
+++ packages/vue-cli-plugin-vue-dash/generator/template/tests/e2e/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
                     {
                         loader: 'ts-loader',
                         options: {
-                            // skip type checking for speed 
+                            // Skip type checking for speed 
                             transpileOnly: false
                         }
                     }
