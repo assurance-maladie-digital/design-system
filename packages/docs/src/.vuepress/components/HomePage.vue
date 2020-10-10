f5080dc63712443eec60af16b7e58725986b53d8
Dylan Broussard <deraw@outlook.fr>
2020-10-10 20:01:17 +0200

    ♻️ Use media queries in HomePage

--- packages/docs/src/.vuepress/components/HomePage.vue
+++ packages/docs/src/.vuepress/components/HomePage.vue
@@ -1,32 +1,23 @@
 <template>
-	<div
-		class="home-page d-flex white"
-		:class="{ 'flex-column-reverse': xs }"
-	>
-		<div
-			class="d-flex flex-column align-start justify-center flex-grow-1"
-			:class="smAndDown ? 'px-10 py-6' : 'px-16 py-10'"
-		>
+	<div class="home-page d-flex white">
+		<div class="home-content d-flex flex-column align-start justify-center flex-grow-1 px-16 py-10">
 			<div class="d-flex align-center flex-wrap ma-n4">
 				<img
-					:width="smAndDown ? '135px' : '200px'"
+					width="200px"
 					src="../assets/logo-am.svg"
 					alt="l'Assurance Maladie"
-					class="ma-4"
+					class="logo-am ma-4"
 				>
 
 				<img
-					:width="smAndDown ? '60px' : '90px'"
+					width="90px"
 					src="../assets/logo-digital.svg"
 					alt="Le Digital de l'Assurance Maladie"
-					class="ma-4"
+					class="logo-digital ma-4"
 				>
 			</div>
 
-			<h1
-				class="home-title text-h3 mt-16 font-weight-bold"
-				:class="{ 'text-h5': smAndDown }"
-			>
+			<h1 class="home-title text-h3 mt-16 font-weight-bold">
 				Un Design System pour l'Assurance Maladie
 			</h1>
 
@@ -50,9 +41,8 @@
 
 		<img
 			src="../assets/design-system-background.png"
-			alt=""
 			class="home-background"
-			:class="{ 'horizontal': $vuetify.breakpoint.xs }"
+			alt=""
 		>
 	</div>
 </template>
@@ -63,31 +53,18 @@
 
 	import { version } from '../../../package.json';
 
+	import dayjs from 'dayjs';
+
+	const LAST_UPDATED = '2020-09-18';
+
 	@Component
 	export default class HomePage extends Vue {
 		version = version;
-		lastUpdated = '6 jours';
 
-		isClient = false;
+		get lastUpdated(): string {
+			const days = dayjs().diff(dayjs(LAST_UPDATED), 'day');
 
-		mounted() {
-			this.isClient = true;
-		}
-
-		get smAndDown(): boolean {
-			if (!this.isClient) {
-				return false;
-			}
-
-			return this.$vuetify.breakpoint.smAndDown;
-		}
-
-		get xs(): boolean {
-			if (!this.isClient) {
-				return false;
-			}
-
-			return this.$vuetify.breakpoint.xs;
+			return `${days} jours`;
 		}
 	}
 </script>
@@ -105,8 +82,33 @@
 		width: 50%;
 		height: 100%;
 		object-fit: cover;
+	}
 
-		&.horizontal {
+	@media only screen and (max-width: 960px) {
+		.home-content {
+			padding: 24px 40px !important;
+		}
+
+		.logo-am {
+			width: 135px;
+		}
+
+		.logo-digital {
+			width: 60px;
+		}
+
+		.home-title {
+			font-size: 1.5rem !important;
+			line-height: 2rem !important;
+		}
+	}
+
+	@media only screen and (max-width: 600px) {
+		.home-page {
+			flex-direction: column-reverse;
+		}
+
+		.home-background {
 			width: 100%;
 			height: 200px;
 		}
