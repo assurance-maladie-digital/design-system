692542fc4a50ab42c1a8e2659bfc619b0597ab7f
Baptiste Coulon <bcoulon72@gmail.com>
2020-10-13 12:12:26 +0200

    feat(vue-dash): remove common styles utility and vuetify added to vue-dot

--- packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vue-dot.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vue-dot.ts
@@ -6,9 +6,6 @@ import 'dayjs/locale/fr';
 import '@cnamts/vue-dot/dist/vue-dot.css';
 import VueDot from '@cnamts/vue-dot';
 
-// Import the theme styles
-import '@/theme/styles/index.scss';
-
 // Register v-facade directive
 import InputFacade from 'vue-input-facade';
 Vue.use(InputFacade);
