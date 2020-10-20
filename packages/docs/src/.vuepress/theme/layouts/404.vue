8a7fffb4e807b97a372b1aa35206dc6d1ec36deb
Dylan Broussard <deraw@outlook.fr>
2020-10-20 15:33:19 +0200

    ✨ Add NotFound page

---
+++ packages/docs/src/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,17 @@
+<template>
+	<ErrorPage
+		code="404"
+		title="Page non trouvée"
+		message="Il semblerait qu'il y ait eu une erreur !"
+		cta="Retour à la page d'accueil"
+	/>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	/** 404 page */
+	@Component
+	export default class NotFound extends Vue {}
+</script>
