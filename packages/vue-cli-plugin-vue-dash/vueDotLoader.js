de1bf9ea69a2797707202e8213d950ecfd0c9b41
Dylan Broussard <deraw@outlook.fr>
2020-11-10 18:29:35 +0100

    ✨ vue-dash: Add VueDot tree-shaking (#685)

---
+++ packages/vue-cli-plugin-vue-dash/vueDotLoader.js
@@ -0,0 +1,57 @@
+// Vue CLI configuration
+// see https://cli.vuejs.org/guide/ for help
+
+process.env.VUE_APP_VERSION = require('./package.json').version;
+
+const componentMap = {
+	// Elements
+	CopyBtn: 'elements',
+	CustomIcon: 'elements',
+	DataList: 'elements',
+	HeaderLoading: 'elements',
+	PageCard: 'elements',
+	DialogBox: 'elements',
+	// Patterns
+	DatePicker: 'patterns',
+	FileUpload: 'patterns',
+	FooterWrapper: 'patterns',
+	LangBtn: 'patterns',
+	NotificationBar: 'patterns',
+	PaginatedTable: 'patterns',
+	SubHeader: 'patterns',
+	UploadWorkflow: 'patterns',
+	// Templates
+	ErrorPage: 'templates'
+};
+
+/**
+ * @param {string} componentName The component name
+ * @returns {boolean} Is the component a Vue Dot component
+ */
+function isVueDotComponent(componentName) {
+	const vueDotComponents = Object.keys(componentMap);
+
+	return vueDotComponents.includes(componentName);
+}
+
+/**
+ * @param {string} componentName The component name
+ * @returns {Array} The Vue Dot component
+ */
+function getVueDotComponent(componentName) {
+	const componentType = componentMap[componentName];
+
+	return [componentName, `import ${componentName} from '@cnamts/vue-dot/src/${componentType}/${componentName}'`];
+}
+
+function match(_, { camelTag }) {
+	if (isVueDotComponent(camelTag)) {
+		return getVueDotComponent(camelTag);
+	}
+}
+
+const vueDotLoader = () => [{
+	match
+}];
+
+module.exports = { vueDotLoader };
