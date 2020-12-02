4a5bcd25cf8827be559c68ae6b31d2ec464a774a
Dylan Broussard <deraw@outlook.fr>
2020-12-02 14:39:21 +0100

    Add DocAlert

---
+++ packages/docs-v2/src/components/global/DocAlert.vue
@@ -0,0 +1,27 @@
+<template>
+	<VAlert
+		border="left"
+		class="doc-alert"
+		text
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<slot />
+	</VAlert>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from "vue-class-component";
+
+	@Component({
+		inheritAttrs: false
+	})
+	export default class DocAlert extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+	.doc-alert ::v-deep p:last-child {
+		margin-bottom: 0 !important;
+	}
+</style>
