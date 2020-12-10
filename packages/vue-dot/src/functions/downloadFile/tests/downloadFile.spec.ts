a5359380a11ed9d8f42b139099becbf2b6ac9bc5
Baptiste Coulon <bcoulon72@gmail.com>
2020-12-10 18:30:10 +0100

    feat(vue-dot): update download file

--- packages/vue-dot/src/functions/downloadFile/tests/downloadFile.spec.ts
+++ packages/vue-dot/src/functions/downloadFile/tests/downloadFile.spec.ts
@@ -1,40 +1,48 @@
 import { downloadFile } from '..';
 
-const file = new File(['hello', ' ', 'world'], 'hello_world.txt', { type: 'text/plain' });
+const file = new File(['hello world'], 'hello_world.txt', { type: 'text/plain' });
 
 // Tests
 describe('downloadFile', () => {
 	global.URL.createObjectURL = jest.fn();
 	global.URL.revokeObjectURL = jest.fn();
-	it('is called correctly', () => {
-		const link: any = {
-			download: String,
-			href: String,
-			click: jest.fn(),
-			style: {}
-		};
+	it('Navigateur version .NET correctly', () => {
+		Object.defineProperty(global.navigator, 'appVersion', '.NET');
 		const mockCallback = jest.fn(downloadFile);
-		const linkSpy = jest.spyOn(document, 'createElement').mockImplementation(() => link);
 
-		const windowSpy = jest.spyOn(global, 'window', 'get');
-		const DocumentSpy = jest.spyOn(global, 'document', 'get');
-		windowSpy.mockImplementation((): any => ({
-			body: {
-				appendChild: link
-			}
-		}));
-		// windowSpy.mockImplementation((): any => ({
-		// 	location: {
-		// 		replace: mockCallback
-		// 	}
-		// }));
+		const msSaveOrOpenBlobSpy = jest.spyOn(window.navigator, 'msSaveOrOpenBlob');
 
 		mockCallback('test_content', file.name, file.type);
 
-		// expect(mockCallback.mock.calls.length).toBe(1);
+		expect(msSaveOrOpenBlobSpy).toHaveBeenCalledTimes(1);
+
+	});
+
+	it('is called correctly', () => {
+		const mockCallback = jest.fn(downloadFile);
+
+		const windowSpy = jest.spyOn(global, 'window', 'get');
+		const appendChilSpy = jest.spyOn(document.body, 'appendChild');
+		const removeChildSpy = jest.spyOn(document.body, 'removeChild');
+		const DocumentSpy = jest.spyOn(global, 'document', 'get');
+
+		mockCallback('test_content', file.name, file.type);
 		expect(windowSpy).toHaveBeenCalledTimes(2);
-		expect(DocumentSpy).toHaveBeenCalledTimes(2);
-		expect(linkSpy).toHaveBeenCalledTimes(2);
+		expect(DocumentSpy).toHaveBeenCalledTimes(3);
+		expect(appendChilSpy).toHaveBeenCalledTimes(1);
+		expect(removeChildSpy).toHaveBeenCalledTimes(1);
+
+		expect(mockCallback).toHaveBeenCalled();
+		expect(mockCallback.mock.calls.length).toEqual(1);
+
+		// const link: any = {
+		// 	download: String,
+		// 	href: String,
+		// 	click: jest.fn(),
+		// 	style: {},
+		// 	target: String
+		// };
+		// jest.spyOn(document, 'createElement').mockImplementation(() => link);
 		// expect(link.download).toEqual(file.name);
 		// expect(link.href).toEqual('test_content');
 		// expect(link.click).toHaveBeenCalledTimes(1);
