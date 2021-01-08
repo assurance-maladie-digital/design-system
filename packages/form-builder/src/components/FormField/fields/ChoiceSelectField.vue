e324e5efcebb0273cbf2192816b06113b13e6de5
Baptiste Coulon <bcoulon72@gmail.com>
2021-01-08 17:31:57 +0100

    feat(form-builder): remove forgotten unwanted 'selectField' that was made for select without other type

--- packages/form-builder/src/components/FormField/fields/ChoiceSelectField.vue
+++ packages/form-builder/src/components/FormField/fields/ChoiceSelectField.vue
@@ -18,5 +18,5 @@
 
 	/** Form field to select a value from a list */
 	@Component
-	export default class SelectField extends MixinsDeclaration {}
+	export default class ChoiceSelectField extends MixinsDeclaration {}
 </script>
