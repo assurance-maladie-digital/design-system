afee545455c0ef31d8890562a78857d169f61f4e
HAUBERT Mathilde <mathilde.haubert@soprasteria.com>
2021-01-22 17:24:51 +0100

    [VUEDOT][DATEPICKER] Anomalie de saisie du datePicker en fonction des fuseaux horaires

--- packages/vue-dot/src/helpers/parseDate/index.ts
+++ packages/vue-dot/src/helpers/parseDate/index.ts
@@ -1,10 +1,24 @@
-import dayjs from 'dayjs';
+import dayjs, { tz } from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 /** Parse custom format with dayjs */
 export function parseDate(value: string, format = 'DD/MM/YYYY'): dayjs.Dayjs {
-	return dayjs(value, format);
+
+	let keepLocalTime = true;
+	const date = new Date();
+	let tzOffset = date.getTimezoneOffset();
+	if (tzOffset === 0) {
+		tzOffset = 1;
+	}
+	if (tzOffset > 0) {
+		keepLocalTime = false;
+	}
+	return dayjs(value, format).hour(2).utcOffset(tzOffset, keepLocalTime);
 }
