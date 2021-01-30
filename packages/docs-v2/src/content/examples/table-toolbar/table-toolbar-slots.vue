b3e3b852de2a2be47ac65bed7588e2666b08e8ab
Coulon Baptiste <bcoulon72@gmail.com>
2021-01-30 20:41:39 +0100

    📝 docs: Add TableToolbar exemples (#864)
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

---
+++ packages/docs-v2/src/content/examples/table-toolbar/table-toolbar-slots.vue
@@ -0,0 +1,66 @@
+<template>
+	<VDataTable
+		:headers="headers"
+		:items="items"
+		:search="search"
+		hide-default-footer
+	>
+		<template #top>
+			<TableToolbar
+				v-model="search"
+				:nb-total="items.length"
+			>
+				<template #search-left>
+					<VBtn
+						color="primary"
+						class="mx-5"
+						outlined
+						small
+					>
+						Exemple
+					</VBtn>
+				</template>
+			</TableToolbar>
+		</template>
+	</VDataTable>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	import { DataTableHeader } from 'vuetify';
+
+	@Component
+	export default class TableToolbarSlots extends Vue {
+		search = '';
+
+		headers: DataTableHeader[] = [
+			{
+				text: 'Nom',
+				value: 'lastname'
+			},
+			{
+				text: 'Prénom',
+				value: 'firstname'
+			},
+			{
+				text: 'Email',
+				value: 'email'
+			}
+		];
+
+		items = [
+			{
+				firstname: 'Rosemarie',
+				lastname: 'Quessy',
+				email: 'rosemarie.quessy@example.com'
+			},
+			{
+				firstname: 'Alphonse',
+				lastname: 'Bouvier',
+				email: 'alphonse.bouvier@example.com'
+			}
+		];
+	}
+</script>
