505b959443b6142e0abab2ba027d792c9b6e826b
Maryvon MICHEL <maryvonmichel@mac-mini-de-maryvon.home>
2021-04-02 19:03:21 +0200

    dialogbox test visuel

--- packages/vue-dot/src/stories/DialogBox.stories.ts
+++ packages/vue-dot/src/stories/DialogBox.stories.ts
@@ -7,30 +7,38 @@ const HeadTitle = () => {
 	  template: '<div style="background:#003463;color: white;height:60px"><p>Dialog box </p><story/></div>',
 	};
 };
-
-
+//
 export default {
 	component: DialogBox,
 	title: 'Elements/DialogBox',
 	excludeStories: /.*Data$/,
 	argTypes : {
 		dialog : {  control: 'boolean' , defaultValue: false },
+		width : { control: 'number', defaultValue: 500 },
+		cancelBtnText: { control: 'text', defaultValue: 'Annuler'},
+		confirmBtnText: { control: 'text', defaultValue: 'Valider'},
 		title :{
 			control : 'text', 
-			defaultValue : `Simple title`
+			defaultValue : `
+			<h4 class="display-1">
+			   Information
+		    </h4>`
 		},
-		content: { control: 'text' , 
+		default: { control: 'text' , 
 		defaultValue: `
-		<VCardText class="px-0">
-		     Il est possible d'utiliser les slots pour modifier l'affichage par défaut.
-	    </VCardText>
+		    <VCardText class="px-0">
+		        Il est possible d'utiliser les slots pour modifier l'affichage par défaut.
+	        </VCardText>
 	   ` , table: {category: 'slots'} },
-		width : { control: 'number', defaultValue: 500 }
+	    actions: {
+			control : 'text', 
+			defaultValue : ''
+		},
 	},
 	decorators: [ HeadTitle ] 
 };
 
-export const UseCase = (args, { argTypes} ) : unknown => ({
+const Template = (args, { argTypes} ) : unknown => ({
 	components: { DialogBox },
 	props: Object.keys(argTypes),
     template : `
@@ -40,9 +48,44 @@ export const UseCase = (args, { argTypes} ) : unknown => ({
 	    @cancel="dialog = false"
 	    @confirm="dialog = false"
     >
-	     ${args.content}
+	    <template v-if="${args.title !=='' }" #title> 
+		    ${args.title}
+		</template>
+		<template v-if="${ args.default !=='' }" #default> 
+		    ${args.default}
+		</template>
+		<template v-if="${ args.actions !=='' }" #actions> 
+		    ${args.actions}
+		</template>
 	</DialogBox>
 	`
 });
+// Use case
+export const UseCase = Template.bind({})
 UseCase.args = {
 }
+// Vuetify options
+export const VuetifyOptions = Template.bind({})
+VuetifyOptions.args = {
+	vuetifyOptions : {
+		cardTitle: {
+			class: 'pa-0 mb-4 accent--text'
+		},
+		closeBtn: {
+			class: {
+				'd-none': true
+			}
+		},
+		icon: {
+			color: 'accent'
+		},
+		cancelBtn: {
+			color: 'accent',
+			text: false
+		},
+		confirmBtn: {
+			color: 'accent',
+			text: true
+		}
+	},
+  };
