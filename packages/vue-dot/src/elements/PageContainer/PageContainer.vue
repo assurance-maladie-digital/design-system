82c73501923169b1be8b884778b3a777e4527b9f
Kevin Petitnicolas-Yoeusley <kevin.petitnicolas@hotmail.fr>
2021-04-07 14:33:36 +0200

    :sparkles: vue-dot: Add PageContainer component basic structure file and logic

---
+++ packages/vue-dot/src/elements/PageContainer/PageContainer.vue
@@ -0,0 +1,83 @@
+<template>
+	<VSheet
+		class="vd-page-container"
+		:class="paddingClass"
+		:width="containerSize"
+	>
+		<slot />
+	</VSheet>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component, { mixins } from 'vue-class-component';
+
+	import { IndexedObject } from '../../types';
+
+	const Props = Vue.extend({
+		props: {
+			/**
+			 * Define the size of the container
+			 *
+			 * Default value: "xl"
+			 */
+			size: {
+				type: String,
+				validator: value => [
+					'xl',
+					'l',
+					'm',
+					'xs'
+				].includes(value.toLowerCase()),
+				default: 'xl'
+			}
+		}
+	});
+
+	const MixinsDeclaration = mixins(Props);
+
+	@Component({
+
+	})
+
+	export default class PageContainer extends MixinsDeclaration {
+		/**
+		 * Use vutify breackpoint to return responsive margin for the container
+		 */
+		get paddingClass(): string | IndexedObject<string> {
+			const breakpoints : IndexedObject<string> = {
+				'xs': 'mx-0',
+				'sm': 'mx-4',
+				'md': 'mx-8',
+				'lg': 'mx-8',
+				'xl': 'mx-8'
+			};
+			return breakpoints[this.$vuetify.breakpoint.name];
+		}
+
+		/**
+		 * Use "size" props to define the size of the container
+		 */
+		get containerSize(): number | IndexedObject<string> {
+			const containerWidth : IndexedObject<number> = {
+				'xl': 1440,
+				'l': 960,
+				'm': 800,
+				'xs': 600
+			};
+			return containerWidth[this.size];
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import '@cnamts/design-tokens/dist/tokens';
+
+	.vd {
+		&-page {
+			&-container {
+				display: flex;
+			}
+		}
+	}
+</style>
