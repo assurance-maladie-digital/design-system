560612979440be91b9d8fa0a470036e6c555fcf7
Dylan Broussard <deraw@outlook.fr>
2021-04-29 21:40:20 +0200

    ♻️ vue-dot: Use inner wrapper in PageContainer

--- packages/vue-dot/src/elements/PageContainer/PageContainer.vue
+++ packages/vue-dot/src/elements/PageContainer/PageContainer.vue
@@ -1,12 +1,16 @@
 <template>
-	<VSheet
-		:width="containerSize"
+	<div
 		:class="spacingClass"
-		:color="color"
-		class="vd-page-container"
+		class="d-flex justify-center"
 	>
-		<slot />
-	</VSheet>
+		<VSheet
+			:width="containerSize"
+			:color="color"
+			class="vd-page-container"
+		>
+			<slot />
+		</VSheet>
+	</div>
 </template>
 
 <script lang="ts">
@@ -52,16 +56,16 @@
 			}
 
 			const containerMargin: IndexedObject<string> = {
-				xs: 'mx-0',
-				sm: 'mx-4',
-				md: 'mx-8',
-				lg: 'mx-8',
-				xl: 'mx-8'
+				xs: 'px-0',
+				sm: 'px-4',
+				md: 'px-8',
+				lg: 'px-8',
+				xl: 'px-8'
 			};
 
 			const margin = containerMargin[this.$vuetify.breakpoint.name];
 
-			return `my-10 ${margin}`;
+			return `py-10 ${margin}`;
 		}
 
 		get containerSize(): number {
