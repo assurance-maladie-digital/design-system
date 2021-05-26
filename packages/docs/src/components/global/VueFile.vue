9f60b2dc26d1ff34d0d89057858b0eb73aa886cb
Dylan Broussard <deraw@outlook.fr>
2021-05-26 23:00:27 +0200

    🎨 docs: Clean-up VueFile

--- packages/docs/src/components/global/VueFile.vue
+++ packages/docs/src/components/global/VueFile.vue
@@ -29,23 +29,25 @@
 		inheritAttrs: false
 	})
 	export default class VueFile extends MixinsDeclaration {
-		component = null;
+		component: Vue | null = null;
 
 		created() {
 			this.load();
 		}
 
-		load(): Promise<void | this> {
-			return import(
-				/* webpackChunkName: "examples" */
-				/* webpackMode: "lazy-once" */
-				`../../content/examples/${this.file}.vue`
-			)
-				.then((component) => {
-					this.component = component.default;
-					this.$emit('loaded', component.default);
-				})
-				.catch((error) => this.$emit('error', error));
+		async load(): Promise<this | void> {
+			try {
+				const component = await import(
+					/* webpackChunkName: "examples" */
+					/* webpackMode: "lazy-once" */
+					`../../content/examples/${this.file}.vue`
+				);
+
+				this.component = component.default;
+				this.$emit('loaded', component.default);
+			} catch(error) {
+				return this.$emit('error', error);
+			}
 		}
 	}
 </script>
