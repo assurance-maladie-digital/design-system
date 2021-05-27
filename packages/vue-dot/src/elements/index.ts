a1512520f6edc5973a35102bf18363a1aaff11b2
Yvon MICHEL <44802047+mmi16389@users.noreply.github.com>
2021-05-27 08:58:35 +0200

    Update packages/vue-dot/src/elements/index.ts
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/vue-dot/src/elements/index.ts
+++ packages/vue-dot/src/elements/index.ts
@@ -3,9 +3,9 @@ import CustomIcon from './CustomIcon';
 import DataList from './DataList';
 import DialogBox from './DialogBox';
 import DownloadBtn from './DownloadBtn';
+import ExternalLinks from './ExternalLinks';
 import HeaderLoading from './HeaderLoading';
 import PageContainer from './PageContainer';
-import ExternalLinks from './ExternalLinks';
 
 export const elements = {
 	CopyBtn,
