849adeccb5d21e3c5be9ad959e4307665ec97e8e
Aubin Mottier <aubin.mottier@gmail.com>
2021-08-23 11:44:57 +0200

    ✨feature: add DocTextIcon

---
+++ packages/docs/src/components/global/DocTextIcon.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="doc-text-icon pl-0 mt-10 w-100 d-flex align-center mb-10">
+		<VIcon
+			:size="iconSize"
+			color="secondary"
+			class="flex-grow-1 mr-10 d-sm-inline-flex d-none"
+		>
+			{{ icon }}
+		</VIcon>
+
+		<DocMarkdown>{{ content }}</DocMarkdown>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	import DocMarkdown from '../code/DocMarkdown.vue';
+
+	import { homePageListItems } from '../../data/homePageListItems';
+
+	const Props = Vue.extend({
+		props: {
+			icon: {
+				type: String,
+				required: true
+			},
+			content: {
+				type: String,
+				required: true
+			}
+		}
+	})
+
+	@Component({
+		components: {
+			DocMarkdown
+		}
+	})
+	export default class DocTextIcon extends Props {
+		homePageListItems = homePageListItems;
+
+		get iconSize(): string {
+			switch (this.$vuetify.breakpoint.name) {
+				case 'md': {
+					return '60px';
+				}
+
+				case 'sm':
+				case 'xs': {
+					return '40px';
+				}
+
+				default: {
+					return '80px';
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.v-icon {
+		flex: none;
+	}
+</style>
