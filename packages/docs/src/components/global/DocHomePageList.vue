bc4785c7a4be64b844361a7c17bd5c7e219e05e7
Aubin Mottier <aubin.mottier@gmail.com>
2021-08-23 11:45:58 +0200

    ✨feature: use DocTextIcon in home page

--- packages/docs/src/components/global/DocHomePageList.vue
+++ packages/docs/src/components/global/DocHomePageList.vue
@@ -1,21 +1,12 @@
 <template>
-	<ul class="doc-home-page-list pl-0 mt-10 w-100">
-		<li
+	<div>
+		<DocTextIcon
 			v-for="(item, index) in homePageListItems"
 			:key="index"
-			class="d-flex align-center mb-10"
-		>
-			<VIcon
-				:size="iconSize"
-				color="secondary"
-				class="flex-grow-1 mr-10 d-sm-inline-flex d-none"
-			>
-				{{ item.icon }}
-			</VIcon>
-
-			<DocMarkdown>{{ item.content }}</DocMarkdown>
-		</li>
-	</ul>
+			:icon="item.icon"
+			:content="item.content"
+		/>
+	</div>
 </template>
 
 <script lang="ts">
@@ -23,38 +14,17 @@
 	import Component from 'vue-class-component';
 
 	import DocMarkdown from '../code/DocMarkdown.vue';
+	import DocTextIcon from './DocTextIcon.vue';
 
 	import { homePageListItems } from '../../data/homePageListItems';
 
 	@Component({
 		components: {
-			DocMarkdown
+			DocMarkdown,
+			DocTextIcon: DocTextIcon
 		}
 	})
 	export default class DocHomePageList extends Vue {
 		homePageListItems = homePageListItems;
-
-		get iconSize(): string {
-			switch (this.$vuetify.breakpoint.name) {
-				case 'md': {
-					return '60px';
-				}
-
-				case 'sm':
-				case 'xs': {
-					return '40px';
-				}
-
-				default: {
-					return '80px';
-				}
-			}
-		}
 	}
 </script>
-
-<style lang="scss" scoped>
-	.v-icon {
-		flex: none;
-	}
-</style>
