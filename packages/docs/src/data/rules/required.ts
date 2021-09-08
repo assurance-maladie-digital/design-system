57eed8860ba028c73459bfce8410451a67faf1a7
Aubin Mottier <aubin.mottier@gmail.com>
2021-09-08 10:38:18 +0200

    📝 docs: update content

--- packages/docs/src/data/rules/required.ts
+++
@@ -1,9 +0,0 @@
-import { RuleOption } from '~/types';
-
-export const ruleOptions: RuleOption[] = [
-    {
-        property: '`default`',
-        description: 'Le message d’erreur qui sera affiché si la règle n’est pas respectée',
-        default: '`Ce champs est requis.`'
-    }
-];
