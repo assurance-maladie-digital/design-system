8b081908d818075a46ae15511346d5c76435aa9e
Maryvon MICHEL <maryvonmichel@mac-mini-de-maryvon.home>
2021-10-11 17:44:53 +0200

    test coverage cypress

--- packages/vue-dot/babel.config.js
+++ packages/vue-dot/babel.config.js
@@ -1,6 +1,16 @@
+const plugins = [];
+if (process.env.NODE_ENV === 'test') {
+  plugins.push([
+    'babel-plugin-istanbul', {
+      extension: ['.ts','.js', '.vue']
+    }
+  ]);
+}
+
 module.exports = {
 	// For dev environment
 	presets: [
 		'@vue/cli-plugin-babel/preset'
-	]
+	],
+	plugins
 };
--- packages/vue-dot/e2e/plugins/index.js
+++ packages/vue-dot/e2e/plugins/index.js
@@ -2,6 +2,10 @@ const { startDevServer } = require('@cypress/webpack-dev-server')
 const webpackConfig = require('@vue/cli-service/webpack.config.js')
 
 module.exports = (on, config) => {
+  if (process.env.NODE_ENV === 'test') {
+    require('@cypress/code-coverage/task')(on, config)
+    on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  }
   on('dev-server:start', options => {
       return startDevServer({
         options, 
