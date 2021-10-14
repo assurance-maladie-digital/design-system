8f4def98c2b8eb84d8e4f6cce7b9d7db2608f6d4
Maryvon MICHEL <maryvonmichel@mac-mini-de-maryvon.home>
2021-10-14 13:14:33 +0200

    confing intellisence vs code

--- packages/vue-dot/src/elements/CustomIcon/tests/CustomIcon.e2e.spec.ts
+++ packages/vue-dot/src/elements/CustomIcon/tests/CustomIcon.e2e.spec.ts
@@ -2,8 +2,6 @@ import { mountComponent } from '@/tests/e2e';
 import CustomIcon from '../';
 
 describe('CustomIcon', () => {
-	// const textToCopy = 'Texte copié !';
-
 	beforeEach(() => {
 		mountComponent(CustomIcon);
 	});
