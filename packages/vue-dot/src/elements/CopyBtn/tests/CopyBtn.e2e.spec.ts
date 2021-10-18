c97accc60c832d846c5948f7fa6c8faafa9d7f45
Maryvon MICHEL <m.michel@groupe-creative.fr>
2021-10-18 19:43:48 +0200

    config: cypress

--- packages/vue-dot/src/elements/CopyBtn/tests/CopyBtn.e2e.spec.ts
+++ packages/vue-dot/src/elements/CopyBtn/tests/CopyBtn.e2e.spec.ts
@@ -8,26 +8,41 @@ describe('CopyBtn component testing', () => {
 
 	const textToCopy = 'Texte copié !';
 
-	it('render correctly ', () => {
-		// const btn = () => import('../CopyBtn.vue');
-			// Mount component
-		const wrapper = mountComponent(CopyBtn, {
-			props: {
-				label: 'test',
-				textToCopy: 'test'
-			}
+	const vuetifyOptions = {
+		menu: {
+			offsetX: false,
+			offsetY: true,
+			nudgeLeft: 50
+		},
+		btn: {
+			large: true
+		},
+		icon: {
+			color: 'grey darken-3'
+		}
+	};
+
+	const props = {
+		hideTooltip: false,
+		label: 'test',
+		textToCopy: 'test',
+		tooltipDuration: 250,
+		vuetifyOptions: vuetifyOptions
+	};
+
+	context(' Visual testing ', () => {
+
+		it('render correctly ', () => {
+			// const btn = () => import('../CopyBtn.vue');
+				// Mount component
+			mountComponent(CopyBtn, { props });
+			// console.log(' wrapper ', cy.document())
+			cy.get('body').toMatchSnapshot();
 		});
-		// console.log(' wrapper ', cy.document())
-		//cy.wrap({ vbtn: CopyBtn }).toMatchSnapshot();
+
 	});
 
-	context(' CopyBtn mounting ', () => {
-
-        const props = {
-            hideTooltip: false,
-            label: 'test',
-            textToCopy: 'test'
-        };
+	context(' Unit testing ', () => {
 
         beforeEach(()=> {
             mountComponent(CopyBtn, { props });
@@ -40,6 +55,8 @@ describe('CopyBtn component testing', () => {
 
         it('copies the text to the clipboard', () => {
             cy.get('@btn').click();
+			//const copy = Cypress.sinon.stub();
+			// expect(copy).to.have.been.calledTwice;
             cy.contains(textToCopy);
         });
 
@@ -47,6 +64,10 @@ describe('CopyBtn component testing', () => {
 			Cypress.vueWrapper.setProps({ hideTooltip: true });
 			cy.get('@btn').click().contains(textToCopy).should('not.exist');
 		});
-    });
 
+		it('calls mixin "customizable" method', () => {
+			const customizable = Cypress.sinon.stub();
+			//expect(customizable).to.have.been.calledTwice;
+		});
+    });
 });
