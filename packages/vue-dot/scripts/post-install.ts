c9ab8d84f755ac26677d3cfda6b84f9cf5a0ecec
Dylan Broussard <deraw@outlook.fr>
2021-11-08 17:47:00 +0100

    test

--- packages/vue-dot/scripts/post-install.ts
+++ packages/vue-dot/scripts/post-install.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs-extra';
 const devTargetFolder = './dev';
 const devTargetFile = `${devTargetFolder}/Playground.vue`;
 
-if (!fs.existsSync(devTargetFile)) {
+if (!fs.existsSync(devTargetFile) && !process.env.CI) {
 	fs.copySync(`${devTargetFolder}/Playground.template.vue`, devTargetFile);
 }
+// TODO skip in CI
