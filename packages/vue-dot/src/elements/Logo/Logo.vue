829b88e234b5ae5b192ef213a43f4754c85fd8ce
Dylan Broussard <deraw@outlook.fr>
2021-12-07 18:39:35 +0100

    ♿️ vue-dot: Add role & focusable attributes

--- packages/vue-dot/src/elements/Logo/Logo.vue
+++ packages/vue-dot/src/elements/Logo/Logo.vue
@@ -4,6 +4,8 @@
 		:aria-label="ariaLabel"
 		:width="dimensions.width"
 		:height="dimensions.height"
+		role="img"
+		focusable="false"
 		xmlns="http://www.w3.org/2000/svg"
 		viewBox="0 0 211 64"
 	>
