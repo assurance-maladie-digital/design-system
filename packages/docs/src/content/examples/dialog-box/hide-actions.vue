18972d60561a4904d9519a3ab1ba6987805fe055
Dylan Broussard <deraw@outlook.fr>
2021-12-10 22:42:37 +0100

    ✨ vue-dot: Add hide-actions prop in DialogBox (#1656)

---
+++ packages/docs/src/content/examples/dialog-box/hide-actions.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<VBtn
+			color="primary"
+			@click="dialog = true"
+		>
+			Afficher le composant
+		</VBtn>
+
+		<DialogBox
+			v-model="dialog"
+			title="Enregistrement"
+			hide-actions
+		>
+			<p class="mb-1">
+				Souhaitez-vous procéder à l’enregistrement ?
+			</p>
+		</DialogBox>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	@Component
+	export default class DialogBoxHideActions extends Vue {
+		dialog = false;
+	}
+</script>
