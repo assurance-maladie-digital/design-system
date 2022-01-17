50e09381f38102e42ab321a3915607efbd111ebe
Dylan Broussard <deraw@outlook.fr>
2022-01-17 17:13:53 +0100

    ♻️ vue-dot: Refactor menu button on mobile in HeaderNavigationBar (#1726)

--- packages/vue-dot/src/patterns/HeaderBar/HeaderNavigationBar/HeaderNavigationBar.vue
+++ packages/vue-dot/src/patterns/HeaderBar/HeaderNavigationBar/HeaderNavigationBar.vue
@@ -22,14 +22,9 @@
 						<VIcon v-bind="options.menuIcon">
 							{{ menuIcon }}
 						</VIcon>
-					</VBtn>
 
-					<h2
-						v-if="activeTabLabel"
-						class="text-body-2 text-sm-subtitle-1 ml-4"
-					>
-						{{ activeTabLabel }}
-					</h2>
+						{{ locales.menu }}
+					</VBtn>
 				</div>
 
 				<VTabs
@@ -57,7 +52,6 @@
 	import Vue, { PropType } from 'vue';
 	import Component, { mixins } from 'vue-class-component';
 
-	import { RawLocation } from 'vue-router';
 	import { mdiClose, mdiMenu } from '@mdi/js';
 
 	import { NavigationItem } from '../types';
@@ -115,49 +109,10 @@
 			return colorMapping[this.theme];
 		}
 
-		get activeTabLabel(): string | undefined {
-			if (this.tab === null) {
-				return undefined;
-			}
-
-			let item: NavigationItem | undefined;
-
-			if (typeof this.tab === 'string') {
-				item = this.findItemByPath(this.tab);
-			} else {
-				item = this.items[this.tab];
-			}
-
-			return item?.label;
-		}
-
 		get menuBtnActionLabel(): string {
 			const action = this.drawer ? locales.close : locales.open;
 
 			return locales.menuBtnLabel(action);
-		}
-
-		findItemByPath(tab: string): NavigationItem | undefined {
-			return this.items.find((item) => tab === this.getItemPath(item));
-		}
-
-		/**
-		 * Get item by path using Vuetify resolution algorithm
-		 *
-		 * @see https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/components/VTabs/VTab.ts#L51
-		 */
-		getItemPath(item: NavigationItem): string {
-			let path: string;
-
-			if (item.to && typeof item.to === 'object') {
-				const resolve = this.$router.resolve(item.to as RawLocation, this.$route);
-				path = resolve.href;
-			} else {
-				const itemTo = item.to as unknown as string;
-				path = itemTo || item.href || '';
-			}
-
-			return path.replace('#', '');
 		}
 
 		toggleDrawer(value: boolean): void {
