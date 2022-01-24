95969345b2c554809d3a2e49ba609b911e7025da
Dylan Broussard <deraw@outlook.fr>
2022-01-24 20:54:15 +0100

    ♻️ vue-dash: Use NotificationModule interface (#1772)

--- packages/vue-cli-plugin-vue-dash/generator/template/src/store/types.d.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/store/types.d.ts
@@ -1,15 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 export interface RootState {}
 
-import { NotificationObj } from '@cnamts/vue-dot/src/modules/notification';
-
 /**
  * This is used to properly type components using mappers from Vuex
- * see https://github.com/vuejs/vuex/pull/1121
+ * @see https://github.com/vuejs/vuex/pull/1121
  */
-export interface VuexBindings {
-	// modules/notification
-	addNotification: (notification: NotificationObj) => void;
-	clearNotification: () => void;
-	notification: NotificationObj | null;
-}
+export interface VuexBindings {}
