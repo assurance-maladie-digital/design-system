2beaccbdf047eccbc0b453f6067b2d8254bfd6c2
Dylan Broussard <deraw@outlook.fr>
2022-03-11 17:09:37 +0100

    ♻️ vue-dot: Rename isDateAfterValue function to isDateAfter (#1847)

--- packages/vue-dot/src/rules/notAfterDate/index.ts
+++ packages/vue-dot/src/rules/notAfterDate/index.ts
@@ -5,7 +5,7 @@ import { defaultErrorMessages } from './locales';
 
 import { parseDate } from '../../helpers/parseDate';
 import { formatDate } from '../../functions/formatDate';
-import { isDateAfterValue } from '../../functions/isDateAfterValue';
+import { isDateAfter } from '../../functions/validation/isDateAfter';
 
 /** Check that the value is not after the specified date (DD/MM/YYYY format) */
 export function notAfterDate(date: string, errorMessages = defaultErrorMessages): ValidationRule {
@@ -16,6 +16,6 @@ export function notAfterDate(date: string, errorMessages = defaultErrorMessages)
 
 		const formattedValue = formatDate(parseDate(date));
 
-		return !isDateAfterValue(date, value) || ruleMessage(errorMessages, 'default', [formattedValue]);
+		return !isDateAfter(date, value) || ruleMessage(errorMessages, 'default', [formattedValue]);
 	};
 }
