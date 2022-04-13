4d8d1d06a600ba63f4e8bb12b9745359dd6ed66b
Dylan Broussard <deraw@outlook.fr>
2022-04-13 15:02:08 +0200

    ✨ vue-dot: Add CookieBanner pattern (#1922)

---
+++ packages/docs/src/content/examples/cookie-banner/usage.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="d-flex flex-wrap align-center justify-center">
+		<CookieBanner
+			v-if="active"
+			cookies-route="/"
+			@reject="active = false"
+			@accept="active = false"
+		/>
+
+		<VBtn
+			v-if="!active"
+			color="primary"
+			class="text-none"
+			@click="active = true"
+		>
+			Réinitialiser
+		</VBtn>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	@Component
+	export default class CookieBannerUsage extends Vue {
+		active = true;
+	}
+</script>
+
+<style lang="scss" scoped>
+	// Display inside component
+	.vd-cookie-banner {
+		position: relative;
+		bottom: 0;
+		z-index: 1;
+	}
+</style>
