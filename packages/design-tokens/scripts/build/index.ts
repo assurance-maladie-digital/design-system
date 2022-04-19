a9c6736d20a9e529fb022b11cee257b7e4488a5b
Dylan Broussard <deraw@outlook.fr>
2022-04-19 16:29:51 +0200

    🔧 design-tokens: Refactor build script

---
+++ packages/design-tokens/scripts/build/index.ts
@@ -0,0 +1,20 @@
+import consola from 'consola';
+
+import { setupDistFolder } from './setupDistFolder';
+import { generateJsonFile } from './generateJsonFile';
+import { generateScssFile } from './generateScssFile';
+import { generateJsFile } from './generateJsFile';
+
+import { tokens } from '../../src/tokens';
+
+const SRC_PATH = './src';
+const DIST_PATH = './dist';
+
+consola.info('Generating Design Tokens');
+
+setupDistFolder(DIST_PATH);
+generateJsonFile(tokens, DIST_PATH);
+generateScssFile(tokens, DIST_PATH);
+generateJsFile(SRC_PATH, DIST_PATH);
+
+consola.success('Generation completed');
