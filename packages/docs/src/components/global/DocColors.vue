80127a15b4af75276c63119d06d9285dbd1ce5c9
Julien Baylac <julien.baylac@protonmail.com>
2022-07-18 15:05:45 +0200

    pull request fixes

--- packages/docs/src/components/global/DocColors.vue
+++ packages/docs/src/components/global/DocColors.vue
@@ -12,11 +12,11 @@
 					<span class="white--text overline">{{ color[0] }}</span>
 				</div>
 				<div 
-					v-for="subcolor in color[1]" 
-					v-bind:key="subcolor.name"
+					v-for="( subcolor, index) in color[1]" 
+					v-bind:key="subcolor"
 					class="pa-3 d-flex justify-space-between"
-					:style="{ 'background-color': subcolor }">
-					<span class="caption">{{ getColorName(subcolor.name) }}</span>
+					:style="{ 'background-color': subcolor, 'color': decideFontColor(hexToRgb(subcolor)) }">
+					<span class="caption">{{ getColorName(index) }}</span>
 					<span class="caption">{{ subcolor }}</span>
 				</div>
 			</v-col>
@@ -34,25 +34,37 @@
 	export default class DocColors extends Vue {
 
 		paletteList = Object.keys(palette).map(function(cle) {
-			console.log(cle)
 			return [String(cle), palette[cle]];
 		});
 
-		getColorName(key:string):string {
-			let string: string[] = []
-			let number: string[] = []
-			key.split('').forEach(el => 
-				if(isNaN(el)) {
-					string.push(el)
-				} else {
-					number.push(el)
-				}
-			)
-			return string.join('') + '-' + number.join('') 
+		getColorName(colorName: string): string{
+			if(colorName === 'base') {
+				return colorName
+			} else {
+				return colorName.substr(0, colorName.length - 2) + '-' + colorName.substr(colorName.length - 2, 2)
+			}
 		}
 
-		mounted() {
-			console.log(this.paletteList)
+		hexToRgb(hex: string) {
+			var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+			hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+				return r + r + g + g + b + b;
+			});
+
+			var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			return result ? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16)
+			} : null;
+		}
+		
+		decideFontColor(color: { r: number; g: number; b: number; }) {
+			if((color.r * 0.299 + color.g * 0.587 + color.b * 0.114) > 176) {
+				return '#000000'
+			} else {
+				return '#ffffff'
+			}
 		}
 	}
 </script>
