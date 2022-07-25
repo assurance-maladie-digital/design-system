c74cf3613582dc307ce336288f9c81a839386104
Julien Baylac <julien.baylac@protonmail.com>
2022-07-25 20:56:23 +0200

    add team and contributors doc

---
+++ packages/docs/src/components/global/DocHomeContributors.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div v-for="contributor in contributors" v-bind:key="contributor">
+			<div class="subtitle-2 text-uppercase font-weight-bold">{{contributor}}</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	@Component
+	export default class DocHomeContributors extends Vue {
+		contributors = ['Contributeur 1', 'Contributeur 2', 'Contributeur 3']
+	}
+</script>
---
+++ packages/docs/src/components/global/DocHomeTeam.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="mb-5">
+		<VRow>
+			<VCol v-for="member in team" v-bind:key="member.name" cols="4">
+				<img class="ds-img-team" :src="require('../../assets/img/team/' + member.img_url)">
+				<div class="subtitle-2 text-uppercase font-weight-bold">{{member.name}}</div>
+				<div class="caption">{{member.role}}</div>
+			</VCol>
+		</VRow>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	@Component
+	export default class DocHomeTeam extends Vue {
+		team = [
+			{
+				name: 'Julien Baylac',
+				role: 'Développeur Front',
+				img_url: 'julien_b.png'
+			},
+			{
+				name: 'Dr Valentin Becquet',
+				role: 'UX/UI Designer',
+				img_url: 'valentin_b.png'
+			},
+			{
+				name: 'Dylan Broussard',
+				role: 'Tech Lead, Développeur Front',
+				img_url: 'dylan_b.png'
+			},
+			{
+				name: 'François Coué',
+				role: 'UX/UI Designer',
+				img_url: 'francois_c.png'
+			},
+			{
+				name: 'Aubin Mottier',
+				role: 'Développeur Front',
+				img_url: 'aubin_m.png'
+			}
+		]
+
+	}
+</script>
+
+<style lang="scss" scoped>
+.ds-img-team {
+	height: 160px;
+	width: 160px;
+}
+</style>
