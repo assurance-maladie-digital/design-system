b4b83e576b57902281921cfc276a261a1b99ba63
Julien Baylac <julien.baylac@protonmail.com>
2022-08-01 10:29:56 +0200

    Add design system colors to vuetify theme

--- packages/vue-dot/dev/vuetify.ts
+++ packages/vue-dot/dev/vuetify.ts
@@ -3,14 +3,26 @@ import Vue from 'vue';
 // Import vuetify/lib to use vuetify-loader
 import Vuetify from 'vuetify/lib';
 
-import { lightTheme } from '@cnamts/design-tokens/src/colors';
+import { palette } from '@cnamts/design-tokens/src/colors';
+import { IndexedObject } from 'src/types';
 
 Vue.use(Vuetify);
+
+const colorsClasses: IndexedObject = {};
+
+Object.entries(palette).forEach(color => {
+	Object.entries(color[1]).forEach(subColor => {
+		const colorName = subColor[0] === 'base' ? 'base' : subColor[0].substring(-20, subColor[0].length - 2) + '-' + subColor[0].substring(subColor[0].length - 2);
+		const name = color[0].toLowerCase() + '-' + colorName;
+		colorsClasses[name] = subColor[1] as string;
+	});
+});
 
 export const vuetify = new Vuetify({
 	theme: {
 		themes: {
-			light: lightTheme
+			light: colorsClasses,
+			dark: colorsClasses
 		}
 	},
 	icons: {
