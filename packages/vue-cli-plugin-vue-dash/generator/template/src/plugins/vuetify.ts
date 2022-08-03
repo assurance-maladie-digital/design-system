a5aa09f20b52ad644474d7306ffea78d709b95c4
Julien Baylac <julien.baylac@protonmail.com>
2022-08-03 21:21:07 +0200

    add vuetify theme to vue-cli-plugin-vue-dash

--- packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vuetify.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/plugins/vuetify.ts
@@ -6,13 +6,27 @@ import Vuetify from 'vuetify/lib';
 import fr from 'vuetify/src/locale/fr';
 
 // Import theme colors
-import { themeColors } from '@/theme/colors';
+import { palette } from '@cnamts/design-tokens/src/colors';
+import { IndexedObject } from 'src/types';
 
 Vue.use(Vuetify);
 
+const colorsClasses: IndexedObject = {};
+
+Object.entries(palette).forEach(color => {
+	Object.entries(color[1]).forEach(subColor => {
+		const colorName = subColor[0] === 'base' ? 'base' : subColor[0].substring(-20, subColor[0].length - 2) + '-' + subColor[0].substring(subColor[0].length - 2);
+		const name = color[0].replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase() + '-' + colorName;
+		colorsClasses[name] = subColor[1] as string;
+	});
+});
+
 export const vuetify = new Vuetify({
 	theme: {
-		themes: themeColors
+		themes: {
+			light: colorsClasses,
+			dark: colorsClasses
+		}
 	},
 	icons: {
 		iconfont: 'mdiSvg'
--- packages/vue-cli-plugin-vue-dash/generator/template/src/types/index.d.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/types/index.d.ts
@@ -1,6 +1,10 @@
 // Here you can define types that
 // you'll be using across your application
 
+export interface IndexedObject<T = string> {
+	[key: string]: T;
+}
+
 export interface Link {
 	id: number;
 	url: string;
