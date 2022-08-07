1a1f71d568fd926772c40b56f9e6d532f3a1cb25
Dylan Broussard <deraw@outlook.fr>
2022-08-07 21:43:40 +0200

    📝 docs: Update roadmap

--- packages/docs/src/data/roadmap/index.ts
+++ packages/docs/src/data/roadmap/index.ts
@@ -11,10 +11,34 @@ export const roadmap: IndexedObject<RoadmapSection[]> = {
 			description: 'Mise à jour de composants et complétion de la documentation.',
 			items: [
 				{
+					title: 'Ajout du composant PhoneField',
+					description: 'Ajout d’un composant permettant la saisie d’un numéro de téléphone.',
+					label: LabelMappingEnum.NEW,
+					issue: 2178
+				},
+				{
 					title: 'Mise à jour de la typographie',
 					description: 'Mise à jour des règles typographiques et des typographies utilisées.',
 					label: LabelMappingEnum.REFACTORING,
 					issue: 1794
+				},
+				{
+					title: 'Guide sur les tests unitaires',
+					description: 'Ajour d’un guide sur la rédaction des tests unitaires.',
+					label: LabelMappingEnum.DOCUMENTATION,
+					issue: 2136
+				},
+				{
+					title: 'Guide sur les tests e2e',
+					description: 'Ajour d’un guide sur la rédaction des tests d’interface.',
+					label: LabelMappingEnum.DOCUMENTATION,
+					issue: 2137
+				},
+				{
+					title: 'Documentation du module de notification',
+					description: 'Documentation du module Vuex de notification.',
+					label: LabelMappingEnum.DOCUMENTATION,
+					issue: 2052
 				},
 				{
 					title: 'Visualisation de données',
@@ -23,43 +47,31 @@ export const roadmap: IndexedObject<RoadmapSection[]> = {
 					issue: 1863
 				},
 				{
-					title: 'Documentation éco-conception',
-					description: 'Définition des principes d’éco-conception.',
-					label: LabelMappingEnum.DOCUMENTATION,
-					issue: 1862
-				},
-				{
 					title: 'Connexion',
 					description: 'Ajout d’un template de connexion.',
-					label: LabelMappingEnum.NEW
-				},
+					label: LabelMappingEnum.NEW,
+					issue: 2052
+				}
 			]
 		}
 	],
 	next: [
 		{
-			version: '2.6.0',
-			date: 'Août 2022',
-			description: 'Ajout de nouveaux composants et mise à jour des Design Tokens.',
-			items: [
-				{
-					title: 'Mot de passe oublié',
-					description: 'Ajout d’un template de réinitialisation de mot de passe.',
-					label: LabelMappingEnum.NEW
-				},
-				{
-					title: 'Nouveau mot de passe',
-					description: 'Ajout d’un template pour définir un nouveau mot de passe.',
-					label: LabelMappingEnum.NEW,
-					issue: 1608
-				}
-			]
-		},
-		{
 			version: '3.0.0',
 			date: 'À partir de septembre 2022',
 			description: 'Migration vers Vue.js 3 : réécriture des composants avec l’API de composition, mise à jour du Starter Kit et modifications majeures.',
-			items: []
+			items: [
+				{
+					title: 'Étude sur le nouveau Starter Kit',
+					description: 'Étude sur les nouveautés et modifications comprises dans le nouveau Starter Kit.',
+					label: LabelMappingEnum.NEW
+				},
+				{
+					title: 'Nouveau Starter Kit',
+					description: 'Création du nouveau Starter Kit.',
+					label: LabelMappingEnum.NEW
+				}
+			]
 		}
 	]
 };
