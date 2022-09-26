c5c99c2be0dc33a2a3215ca33ab071ac1f81f398
chris <christophe.gaon@gmail.com>
2022-09-26 16:17:01 +0200

    🐛 eslint-config: Fix missing lint rules (#2295)

--- packages/eslint-config/tests/fixtures/code.ts
+++ packages/eslint-config/tests/fixtures/code.ts
@@ -1,1 +1,2 @@
-export const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n';
+export const javascript = '\nvar foo = 1\nvar bar= function () {}\nbar(foo)\n\n';
+export const typescript = 'var foo = 1\nvar bar = function ():void {}\nbar(foo)\n';
