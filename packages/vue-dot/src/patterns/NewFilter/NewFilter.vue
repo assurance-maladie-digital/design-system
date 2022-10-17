33f27c7f117da353074ed95608850b3adbe4cc5a
Julien Baylac <julien.baylac@protonmail.com>
2022-10-17 18:11:58 +0200

    wip

--- packages/vue-dot/src/patterns/NewFilter/NewFilter.vue
+++ packages/vue-dot/src/patterns/NewFilter/NewFilter.vue
@@ -1,29 +1,6 @@
 <template>
 	<div class="new-filter">
-		<!-- mode with sidebar -->
-		<VBtn
-			v-if="!simpleMode"
-			outlined
-			class="sidebar-filter-button"
-			@click="showSideBar = !showSideBar"
-		>
-			<span
-				v-if="filtersCount"
-				class="mr-2"
-			>
-				{{ filtersCount }}
-			</span>
-			<span
-				class="mr-2"
-			>
-				Filtres
-			</span>
-			<VIcon>
-				{{ filterIcon }}
-			</VIcon>
-		</VBtn>
-
-		<!-- simple mode buttons -->
+		<!-- simple mode -->
 		<div>
 			<div
 				v-if="simpleMode"
@@ -34,60 +11,72 @@
 					min-width="300px"
 					max-width="400px"
 					offset-y
-					nudge-bottom="10px"
 					:close-on-content-click="false"
+					nudge-bottom="10px"
 				>
 					<template #activator="{ on }">
 						<VBtn
 							class="simple-mode-filters mr-4"
-							outlined
+							color="secondary"
+							depressed
+							:outlined="!filterSelection[index]?.selection.length"
 							v-on="on"
 						>
-							<VIcon
-								size="1rem"
-								color="secondary"
-								class="mr-2"
+							<VChip
+								v-if="filterSelection[index]?.selection.length"
+								class="ml-n2 mr-2"
+								color="white"
+								small
 							>
-								{{ getIcon(filterSelection[index]) }}
-							</VIcon>
-							<span>{{ filter.name }}</span>
+								{{ filterSelection[index]?.selection.length }}
+							</VChip>
+							<span>{{ filter.label }}</span>
 							<VIcon>
 								{{ downIcon }}
 							</VIcon>
 						</VBtn>
 					</template>
-
 					<div
 						class="mt-2 px-4"
 					>
 						<span class="description-text-filter mb-2">{{ filter.description }}</span>
 						<v-chip-group
 							v-if="filterSelection[index]"
+							class="mb-4"
 							column
 						>
 							<v-chip
-								v-for="chip in limitChips(filterSelection[index])"
+								v-for="(chip, chipIndex) in limitChips(filterSelection[index])"
 								:key="chip"
 								small
 								close
 								class="mt-2"
 								:close-icon="deleteIcon"
+								color="cyan-darken-40"
+								text-color="white"
+								@click:close="removeChip(index, chipIndex)"
 							>
 								{{ chip }}
 							</v-chip>
 							<v-chip
 								v-if="showExpandChip(index)"
 								small
-								class="mt-2"
-								@click="displayHiddenChips(index)"
+								outlined
+								color="cyan-lighten-60"
+								text-color="cyan-darken-40"
+								class="mt-2 bg-cyan"
+								@click.stop="displayHiddenChips(index)"
 							>
 								{{ `+${filterSelection[index].selection.length - chipsLimit}` }}
 							</v-chip>
 							<v-chip
 								v-if="filterSelection[index].showAll"
 								small
-								class="mt-2"
-								@click="filterSelection[index].showAll = false"
+								outlined
+								color="cyan-lighten-60"
+								text-color="cyan-darken-40"
+								class="mt-2 bg-cyan"
+								@click.stop="filterSelection[index].showAll = false"
 							>
 								<VIcon>
 									{{ upIcon }}
@@ -98,17 +87,12 @@
 								class="text-none align-self-center mt-2"
 								x-small
 								text
-								cla
 								color="primary"
-								@click="resetFilter(index)"
+								@click.stop="resetFilter(index)"
 							>
 								Réinitialiser
 							</v-btn>
 						</v-chip-group>
-						<VDivider
-							v-if="filterSelection[index].selection.length"
-							class="mt-2 mb-4"
-						/>
 						<slot
 							:on="{
 								change: (event) => addChips(event, removeAccents(filter.name))
@@ -119,143 +103,180 @@
 				</VMenu>
 			</div>
 
-			<!-- sidebar -->
-			<VNavigationDrawer
-				v-if="showSideBar && !simpleMode"
-				permanent
-				absolute
-				right
-				class="pt-2"
+			<!-- mode with sidebar -->
+			<VBtn
+				v-if="!simpleMode"
+				depressed
+				rounded
+				color="transparent"
+				@click="showSideBar = !showSideBar"
 			>
-				<v-expansion-panels
-					accordion
-					flat
+				<v-chip
+					v-if="filtersCount"
+					class="mr-2"
+					color="cyan-darken-40"
+					text-color="white"
+					small
 				>
-					<v-expansion-panel
-						v-for="(filter, index) in filters"
-						:key="index"
-					>
-						<v-expansion-panel-header class="d-block">
-							<div
-								class="header-title d-flex justify-space-between mt-3 mx-4"
-							>
-								<span>{{ filter.name }} <span v-if="filterSelection[index].selection.length">({{ filterSelection[index].selection.length }})</span></span>
+					{{ filtersCount }}
+				</v-chip>
+				<span
+					class="mr-2 primary--text"
+				>
+					{{ filters.length > 1 ? 'Filtres' : 'Filtre' }}
+				</span>
+				<VIcon
+					color="primary"
+				>
+					{{ filterIcon }}
+				</VIcon>
+			</VBtn>
 
-								<div>
-									<VIcon
-										id="up-icon"
-										class="header-title"
-									>
-										{{ upIcon }}
-									</VIcon>
-									<VIcon
-										id="down-icon"
-										class="header-title"
-									>
-										{{ downIcon }}
-									</VIcon>
+			<!-- sidebar v-click-outside="closeSidebar" -->
+			<v-scroll-x-transition>
+				<VNavigationDrawer
+					v-if="showSideBar && !simpleMode"
+					permanent
+					absolute
+					right
+					:elevation="6"
+					class="pt-2 d-flex flex-column justify-space-between"
+				>
+					<v-expansion-panels
+						accordion
+						flat
+						class="expansion-panels"
+					>
+						<v-expansion-panel
+							v-for="(filter, index) in filters"
+							:key="index"
+						>
+							<v-expansion-panel-header
+								class="d-block"
+							>
+								<div
+									class="header-title d-flex justify-space-between mt-3 mx-4"
+								>
+									<span>
+										{{ filter.label }}
+										<span v-if="filterSelection[index].selection.length">
+											({{ filterSelection[index].selection.length }})
+										</span>
+									</span>
+									<div>
+										<VIcon
+											id="up-icon"
+											class="header-title"
+										>
+											{{ upIcon }}
+										</VIcon>
+										<VIcon
+											id="down-icon"
+											class="header-title"
+										>
+											{{ downIcon }}
+										</VIcon>
+									</div>
 								</div>
-							</div>
-							<v-chip-group
-								v-if="filterSelection[index]"
-								class="mx-3"
-								column
-							>
-								<v-chip
-									v-for="chip in limitChips(filterSelection[index])"
-									:key="chip"
-									small
-									close
-									class="mt-2"
-									:close-icon="deleteIcon"
+								<v-chip-group
+									v-if="filterSelection[index]"
+									class="mx-3"
+									column
 								>
-									{{ chip }}
-								</v-chip>
-								<v-chip
-									v-if="showExpandChip(index)"
-									small
-									class="mt-2"
-									@click="displayHiddenChips(index)"
-								>
-									{{ `+${filterSelection[index].selection.length - chipsLimit}` }}
-								</v-chip>
-								<v-chip
-									v-if="filterSelection[index].showAll"
-									small
-									class="mt-2"
-									@click="filterSelection[index].showAll = false"
-								>
-									<VIcon>
-										{{ upIcon }}
-									</VIcon>
-								</v-chip>
-								<v-btn
-									v-if="filterSelection[index].selection.length"
-									class="text-none align-self-center mt-2"
-									x-small
-									text
-									cla
-									color="primary"
-									@click="resetFilter(index)"
-								>
-									Réinitialiser
-								</v-btn>
-							</v-chip-group>
-						</v-expansion-panel-header>
-						<v-expansion-panel-content>
-							<span class="description-text-filter mb-2">{{ filter.description }}</span>
-							<div class="mt-4">
-								<slot
-									:on="{
-										change: (event) => addChips(event, removeAccents(filter.name))
-									}"
-									:name="`filter-${removeAccents(filter.name)}`"
-								/>
-							</div>
-						</v-expansion-panel-content>
-						<VDivider />
-					</v-expansion-panel>
-				</v-expansion-panels>
+									<v-chip
+										v-for="(chip, chipIndex) in limitChips(filterSelection[index])"
+										:key="chip"
+										small
+										close
+										class="mt-2"
+										:close-icon="deleteIcon"
+										color="cyan-darken-40"
+										text-color="white"
+										@click:close="removeChip(index, chipIndex)"
+									>
+										{{ parseChips(chip) }}
+									</v-chip>
+									<v-chip
+										v-if="showExpandChip(index)"
+										small
+										outlined
+										color="cyan-lighten-60"
+										text-color="cyan-darken-40"
+										class="mt-2 bg-cyan"
+										@click.stop="displayHiddenChips(index)"
+									>
+										{{ `+${filterSelection[index].selection.length - chipsLimit}` }}
+									</v-chip>
+									<v-chip
+										v-if="showExpandChip(index) && filterSelection[index].showAll"
+										small
+										outlined
+										color="cyan-lighten-60"
+										text-color="cyan-darken-40"
+										class="mt-2 bg-cyan"
+										@click.stop="filterSelection[index].showAll = false"
+									>
+										<VIcon>
+											{{ upIcon }}
+										</VIcon>
+									</v-chip>
+									<v-btn
+										v-if="filterSelection[index].selection.length"
+										class="text-none align-self-center mt-2"
+										x-small
+										text
+										cla
+										color="primary"
+										@click.stop="resetFilter(index)"
+									>
+										Réinitialiser
+									</v-btn>
+								</v-chip-group>
+							</v-expansion-panel-header>
+							<v-expansion-panel-content>
+								<span class="description-text-filter mb-2">{{ filter.description }}</span>
+								<div class="mt-4">
+									<slot
+										:on="{
+											change: (event) => addChips(event, removeAccents(filter.name))
+										}"
+										:name="`filter-${removeAccents(filter.name)}`"
+									/>
+								</div>
+							</v-expansion-panel-content>
+							<VDivider />
+						</v-expansion-panel>
+					</v-expansion-panels>
 
-				<!-- buttons -->
-				<div
-					class="ma-2 mb-16 mt-6"
-					:class="isMobile ? '' : 'd-flex justify-space-between'"
-				>
-					<v-btn
-						:class="isMobile ? '' : 'mb-2'"
-						:outlined="isMobile"
-						:text="!isMobile"
-						:block="isMobile"
-						small
-						color="indigo"
-						@click="showSideBar = !showSideBar"
+					<!-- buttons -->
+					<div
+						class="px-5 mb-6 mt-10"
+						:class="isMobile ? '' : 'd-flex'"
 					>
-						Fermer
-					</v-btn>
-					<div>
+						<v-btn
+							:class="isMobile ? '' : 'mb-2 mr-4'"
+							:outlined="isMobile"
+							:text="!isMobile"
+							:block="isMobile"
+							small
+							color="indigo"
+							@click="closeSidebar"
+						>
+							Fermer
+						</v-btn>
 						<v-btn
 							:class="isMobile ? 'mb-2 mt-2' : ''"
 							:block="isMobile"
 							outlined
 							small
 							color="indigo"
-							@click="resetAllFilters"
+							@click.stop="resetAllFilters"
 						>
 							Réinitialiser
 						</v-btn>
-						<v-btn
-							:class="isMobile ? '' : 'ml-2'"
-							:block="isMobile"
-							small
-							color="primary"
-							@click="emitFilter"
-						>
-							Appliquer
-						</v-btn>
 					</div>
-				</div>
-			</VNavigationDrawer>
+				</VNavigationDrawer>
+			</v-scroll-x-transition>
 		</div>
 	</div>
 </template>
@@ -264,11 +285,12 @@
 	//	eslint-disable max-lines
 	import Vue, { PropType } from 'vue';
 	import Component, { mixins } from 'vue-class-component';
+	import dayjs from 'dayjs';
 	import { FilterItem } from './types';
 
 	import { locales } from './locales';
 
-	import { mdiChevronUp, mdiChevronDown, mdiWindowClose, mdiFilterVariant, mdiNumeric1BoxMultipleOutline, mdiNumeric2BoxMultipleOutline, mdiNumeric3BoxMultipleOutline, mdiNumeric4BoxMultipleOutline, mdiNumeric5BoxMultipleOutline, mdiNumeric6BoxMultipleOutline, mdiNumeric7BoxMultipleOutline, mdiNumeric8BoxMultipleOutline, mdiNumeric9BoxMultipleOutline, mdiNumeric9PlusBoxMultipleOutline } from '@mdi/js';
+	import { mdiChevronUp, mdiChevronDown, mdiWindowClose, mdiFilterVariant } from '@mdi/js';
 
 	const Props = Vue.extend({
 		props: {
@@ -290,24 +312,12 @@
 	const MixinsDeclaration = mixins(Props);
 
 	@Component({})
-	export default class FileUpload extends MixinsDeclaration {
+	export default class NewFilter extends MixinsDeclaration {
 		locales = locales;
 		deleteIcon = mdiWindowClose;
 		filterIcon = mdiFilterVariant;
 		upIcon = mdiChevronUp;
 		downIcon = mdiChevronDown;
-
-		//number icons
-		icon1  = mdiNumeric1BoxMultipleOutline;
-		icon2  = mdiNumeric2BoxMultipleOutline;
-		icon3  = mdiNumeric3BoxMultipleOutline;
-		icon4  = mdiNumeric4BoxMultipleOutline;
-		icon5  = mdiNumeric5BoxMultipleOutline;
-		icon6  = mdiNumeric6BoxMultipleOutline;
-		icon7  = mdiNumeric7BoxMultipleOutline;
-		icon8  = mdiNumeric8BoxMultipleOutline;
-		icon9  = mdiNumeric9BoxMultipleOutline;
-		icon9Plus  = mdiNumeric9PlusBoxMultipleOutline;
 
 		showSideBar = false;
 		filterSelection: FilterItem[] = [];
@@ -335,41 +345,19 @@
 		addChips(event: unknown, value: unknown): void {
 			for (let i = 0; i < this.filters.length; i++) {
 				if (this.removeAccents(this.filterSelection[i].name) === value) {
-					if (event !== '') {
-						this.filterSelection[i].selection.push(event);
+					if (this.filterSelection[i].limited) {
+						this.filterSelection[i].selection = [];
+						if (event !== '') {
+							this.filterSelection[i].selection.push(event);
+						}
+					} else {
+						if (event !== '') {
+							this.filterSelection[i].selection.push(event);
+						}
 					}
+
 				}
 			}
-		}
-
-		getIcon(filterSelection: string): any {
-			const count = filterSelection ? filterSelection.selection.length : 0;
-			let icon;
-			if (count) {
-				if (count === 1) {
-					icon = this.icon1;
-				} else if (count === 2) {
-					icon = this.icon2;
-				} else if (count === 3) {
-					icon = this.icon3;
-				} else if (count === 4) {
-					icon = this.icon4;
-				} else if (count === 5) {
-					icon = this.icon5;
-				} else if (count === 6) {
-					icon = this.icon6;
-				} else if (count === 7) {
-					icon = this.icon7;
-				} else if (count === 8) {
-					icon = this.icon8;
-				} else if (count === 9) {
-					icon = this.icon9;
-				} else if (count > 9) {
-					icon = this.icon9Plus;
-				}
-			}
-
-			return icon;
 		}
 
 		limitChips(filterItem: FilterItem): unknown[] {
@@ -382,6 +370,10 @@
 
 		resetFilter(index: number): void {
 			this.$set(this.filterSelection[index], 'selection', []);
+		}
+
+		removeChip(filterIndex: number, chipIndex: number): void {
+			this.filterSelection[filterIndex].selection.splice(chipIndex, 1);
 		}
 
 		displayHiddenChips(index: number): void {
@@ -399,14 +391,23 @@
 			}
 		}
 
-		emitFilter(): void {
-			console.log(this.filterSelection);
+		closeSidebar(): void {
+			this.showSideBar = false;
+		}
+
+		parseChips(chip: string): string {
+			const FRENCH_DATE_FORMAT = 'DD/MM/YYYY';
+			dayjs(chip).format(FRENCH_DATE_FORMAT);
+
+			return chip;
 		}
 
 		mounted() {
 			this.filterSelection = this.filters.map(filter => ({
 				name: filter.name,
-				description: filter.description,
+				label: filter.label,
+				description: filter.description ?? '',
+				limited: filter.limited ?? false,
 				selection: filter.selection ?? [],
 				showAll: false
 			}));
@@ -435,26 +436,17 @@
 	#down-icon {
 		display: none;
 	}
-	.v-expansion-panel--active {
-		border-radius: 8px !important;
-
-		.open {
-			display: none;
+	.v-expansion-panel-header--active {
+		#up-icon {
+			display: none !important;
 		}
-		.closed {
-			display: inline;
-		}
-
-		.header-title {
-			color: $vd-am-blue-base;
+		#down-icon {
+			display: inline !important;
 		}
 	}
 	.header-title {
 		font-weight: 500;
 		font-size: 16px;
-	}
-	.sidebar-filter-button {
-		border-radius: 20px;
 	}
 
 	//simple mode
@@ -463,10 +455,24 @@
 		border-radius: 20px;
 		text-transform: capitalize;
 	}
-
+	.expansion-panels {
+		margin-left: 1px;
+	}
+	.v-navigation-drawer {
+		box-shadow: -3px 3px 5px #0000001f;
+	}
+	.v-navigation-drawer__content {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
 }
 .description-text-filter {
 	color: $vd-grey-lighten-20;
 	font-size: 14px !important;
 }
+.bg-cyan.v-chip.v-chip--outlined.v-chip.v-chip {
+	background-color: $vd-cyan-lighten-90 !important;
+}
+
 </style>
