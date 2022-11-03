1e697ab6aa97659b60d265c971316a2cad009f36
Dylan Broussard <deraw@outlook.fr>
2022-11-03 16:20:43 +0100

    🐛 vue-dash: Fix missing languages module declaration (#2363)

--- packages/vue-cli-plugin-vue-dash/generator/template/src/modules.d.ts
+++ packages/vue-cli-plugin-vue-dash/generator/template/src/modules.d.ts
@@ -1,6 +1,7 @@
 // Declare module for every package without types
 declare module 'vuex-persist/dist/umd';
 declare module 'vue-input-facade';
+declare module 'languages';
 
 // Make Vue SFC modules
 declare module '*.vue' {
