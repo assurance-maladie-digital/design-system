218af4cec1c5e958a2c7b2df1f3be5950f85f009
David FYON <davidfyon@gmail.com>
2022-11-21 18:24:20 +0100

    add styles and tooltips

--- packages/vue-dot/src/patterns/SearchFilter/SearchFilter.vue
+++ packages/vue-dot/src/patterns/SearchFilter/SearchFilter.vue
@@ -14,31 +14,54 @@
 				</VIcon>
 			</template>
 		</VTextField>
-		<VCol>
-			<div
-				v-for="item in tempListSearch"
-				:key="item"
-			>
-				<VRow>
-					<VCheckbox
-						v-model="selectedItems"
-						height="10"
-						:value="item"
-						@change="emitChangeEvent()"
-					/>
-					<VCol class="d-flex align-center">
-						{{ item }}
-					</VCol>
-				</VRow>
-			</div>
-		</VCol>
+		<VScrollXTransition>
+			<VCol class="px-0 pt-0">
+				<div
+					v-for="item in tempListSearch"
+					:key="item.id"
+				>
+					<VRow
+						class="mx-0 px-3 my-0"
+						:class="{ 'bg-blue': selectedItems.includes(item.name) }"
+					>
+						<VCheckbox
+							v-model="selectedItems"
+							height="9"
+							:value="item.name"
+							@change="emitChangeEvent()"
+						/>
+						<VCol class="d-flex align-center justify-space-between pl-2 pr-0">
+							{{ item.name }}
+							<VTooltip
+								v-if="item.info !== ''"
+								top
+							>
+								<template #activator="{ on, attrs }">
+									<VBtn
+										icon
+										height="9"
+										v-bind="attrs"
+										v-on="on"
+									>
+										<VIcon color="grey lighten-3">
+											{{ infoIcon }}
+										</VIcon>
+									</VBtn>
+								</template>
+								{{ item.info }}
+							</VTooltip>
+						</VCol>
+					</VRow>
+				</div>
+			</VCol>
+		</VScrollXTransition>
 	</div>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
 	import Component, { mixins } from 'vue-class-component';
-	import { mdiMagnify } from '@mdi/js';
+	import { mdiMagnify, mdiInformation } from '@mdi/js';
 
 	const Props = Vue.extend({
 		props: {
@@ -54,6 +77,12 @@
 	});
 
 	const MixinsDeclaration = mixins(Props);
+
+	interface Item {
+		id: number;
+		name: string;
+		info: string;
+	}
 
 	@Component<SearchFilter>({
 		watch: {
@@ -75,15 +104,18 @@
 
 	export default class SearchFilter extends MixinsDeclaration {
 		searchIcon = mdiMagnify;
+		infoIcon = mdiInformation;
 		filterSearch = '';
-		tempListSearch: string[] = [];
-		listSearch: string[] = [
-			'foo',
-			'bar',
-			'baz'
+		tempListSearch: Item[] = [];
+		listSearch:  Item[] = [
+			{ id: 1, name: 'foo', info: 'foo info' },
+			{ id: 2, name: 'bar', info: '' },
+			{ id: 3, name: 'baz', info: '' },
+			{ id: 4, name: 'qux', info: 'qux info' },
+			{ id: 5, name: 'quux', info: '' }
 		];
-		selectedItems: string[] = [];
-		internalValue: string[] = [];
+		selectedItems: Array<string> = [];
+		internalValue: Array<string> = [];
 
 		mounted() {
 			this.tempListSearch = this.listSearch;
@@ -94,7 +126,9 @@
 		}
 
 		filterResults(): void {
-			this.tempListSearch = this.listSearch.filter((item) => item.includes(this.filterSearch));
+			this.tempListSearch = this.listSearch.filter((item: Item) => {
+				return item.name.toLowerCase().includes(this.filterSearch.toLowerCase());
+			});
 			if (this.filterSearch === '' || this.filterSearch === null || this.filterSearch === undefined) {
 				this.clearResults();
 			}
@@ -110,3 +144,10 @@
 		}
 	}
 </script>
+
+<style lang="scss" scoped>
+	@import '@cnamts/design-tokens/dist/tokens';
+	.bg-blue {
+		background-color: $vd-am-blue-lighten-90 !important;
+	}
+</style>
