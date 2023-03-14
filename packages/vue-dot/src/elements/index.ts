595eefbc99f9e74c0d83d26843b037c7d512f046
David FYON <davidfyon@gmail.com>
2023-03-14 15:48:24 +0100

    🐛 vue-dot: Fix missing component exports (#2598)

--- packages/vue-dot/src/elements/index.ts
+++ packages/vue-dot/src/elements/index.ts
@@ -6,6 +6,7 @@ import DataList from './DataList';
 import DialogBox from './DialogBox';
 import DownloadBtn from './DownloadBtn';
 import ExternalLinks from './ExternalLinks';
+import FilePreview from './FilePreview';
 import FranceConnectBtn from './FranceConnectBtn';
 import HeaderLoading from './HeaderLoading';
 import Logo from './Logo';
@@ -21,6 +22,7 @@ export const elements = {
 	DialogBox,
 	DownloadBtn,
 	ExternalLinks,
+	FilePreview,
 	FranceConnectBtn,
 	HeaderLoading,
 	Logo,
