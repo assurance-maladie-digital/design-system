8c7d853d52dba9d27d1f21bc49893d3214da0576
bidule <christophe.gaon@assurance-maladie.fr>
2023-03-23 11:22:21 +0100

    wip NumberPicker refacto delete tests for fail pipeline todo add tests and finish refacto on NumberPicker

--- packages/vue-dot/src/patterns/RatingPicker/NumberPicker/NumberPicker.vue
+++ packages/vue-dot/src/patterns/RatingPicker/NumberPicker/NumberPicker.vue
@@ -1,71 +1,40 @@
 <template>
-	<div class="vd-emotion-picker">
-		<div class="d-flex justify-center">
-			<H6
-				class="mb-6"
-			>
-				{{ questionDatas.question }}
-			</H6>
-		</div>
-		<div
-			v-if="selectedRating"
-			class="d-flex justify-center rate-meaning"
+	<div>
+		<h6>{{ label }}</h6>
+		<v-rating
+			:length="length"
+			color="primary"
+			background-color="primary lighten-4"
+			hover
+			:readonly="haveAnswer"
 		>
-			<VBtn
-				class="rating-button"
-				color="primary"
-				outlined
-				text
-			>
-				{{ selectedRating }}
-			</VBtn>
-			<span class="ml-1 text-selected-rating">
-				/ 10
-			</span>
-		</div>
-		<VRow
-			v-else
-			class="grid justify-center ma-0"
-		>
-			<VCol
-				v-for="i in 10"
-				:key="i"
-				class="pa-0 numbers flex-grow-0"
-			>
-				<VBtn
-					class="rating-button"
+			<template #item="props">
+				<v-btn
+					class="mx-1 my-2 pa-0 rating-height-button"
 					color="primary"
 					outlined
-					text
-					@click="selectRating(i)"
+					x-small
+					@click="onValidate(props.index)"
 				>
-					{{ i }}
-				</VBtn>
-			</VCol>
-		</VRow>
-		<div
-			v-if="!selectedRating"
-			class="mt-3 d-flex justify-space-between rate-meaning"
-		>
-			<span>
-				{{ getLabel(0) }}
-			</span>
-			<span>
-				{{ getLabel(1) }}
-			</span>
-		</div>
+					{{ props.index }}
+				</v-btn>
+			</template>
+		</v-rating>
 	</div>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
 	import Component, { mixins } from 'vue-class-component';
-
 	const Props = Vue.extend({
 		props: {
-			questionDatas: {
-				type: Object,
+			label: {
+				type: String,
 				required: true
+			},
+			length: {
+				type: Number,
+				default: 5
 			}
 		}
 	});
@@ -75,48 +44,24 @@
 	@Component
 	export default class NumberPicker extends MixinsDeclaration {
 
-		selectedRating = 0;
+		haveAnswer =false;
 
-		selectRating(rating: number): void {
-			this.selectedRating = rating;
-			this.$emit(
-				'update-result',
-				{
-					step: this.questionDatas.name,
-					result: this.selectedRating
-				}
-			);
+		onValidate(event: Event): void {
+			console.log(event);
+			this.haveAnswer = true;
+			this.length = Number(event);
 		}
-		getLabel(number: number): string {
-			return this.questionDatas.label ? this.questionDatas.labels[number] : '';
-		}
+
 	}
 </script>
 
 <style lang="scss" scoped>
-@import '@cnamts/design-tokens/dist/tokens';
 
+.rating-height-button {
+	min-height: 36px;
+}
 h6 {
-	font-size: 16px;
+	font-size: 16px
 }
-.rating-button {
-	border-color: $vd-primary !important;
-	min-width: 0 !important;
-	width: 20px !important;
-}
-.selected-rating {
-	border: 1px solid $vd-primary;
-	padding: 1px 10px;
-	border-radius: 3px;
-	margin-top: -1px;
-}
-.rate-meaning {
-	min-height: 25px;
-}
-.text-selected-rating {
-	margin-top: 6px;
-}
-.numbers {
-	margin: 1px !important;
-}
+
 </style>
