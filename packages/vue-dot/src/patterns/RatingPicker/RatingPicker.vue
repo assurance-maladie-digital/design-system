9a1c4920d73c5bcc126aa76c4ff2b950b2894228
bidule <christophe.gaon@assurance-maladie.fr>
2023-03-28 09:44:12 +0200

    update RatingPicker.vue to force length

--- packages/vue-dot/src/patterns/RatingPicker/RatingPicker.vue
+++ packages/vue-dot/src/patterns/RatingPicker/RatingPicker.vue
@@ -2,19 +2,21 @@
 	<div>
 		<component
 			:is="type"
+			ref="textareaRef"
 			:label="label"
-			:length="length_internal"
-			@input="onFirstValidation"
+			:length="length"
+			:readonly="read_only_internal"
+			@input="onUpdate"
 		/>
-
-		<v-alert
-			v-if="read_only_internal"
-			outlined
-			type="success"
-		>
-			ca va
-		</v-alert>
-		<slot />
+		<div v-if="read_only_internal">
+			<v-alert
+				outlined
+				type="success"
+			>
+				ca va
+			</v-alert>
+			<slot />
+		</div>
 		<div class="mr-2 mt-5 d-flex justify-space-between ">
 			<v-btn
 				text
@@ -33,7 +35,6 @@
 </template>
 <script lang="ts">
 	import Component, { mixins } from 'vue-class-component';
-
 	import StarsPicker from './StarsPicker';
 	import NumberPicker from './NumberPicker';
 	import { RatingEnum, RatingMixin } from './RatingMixin';
@@ -46,10 +47,18 @@
 				type: String as PropType<RatingEnum>,
 				required: true,
 				validator: (value: string) => propValidator('type', Object.values(RatingEnum), value)
+			},
+			label: {
+				type: String,
+				required: true
+			},
+			readonly: {
+				type: Boolean,
+				default: false
 			}
 		}
 	});
-	const MixinsDeclaration = mixins(Props,RatingMixin);
+	const MixinsDeclaration = mixins(Props);
 
 	@Component({
 		components: {
@@ -59,10 +68,26 @@
 	})
 	export default class RatingPicker extends MixinsDeclaration {
 
-		currentPicker: string=this.type;
-		onFirstValidation(event: RatingMixin, value: number): void {
+		/**
+		 * losrqu on valide le premier rating du composant
+		 *
+		 * @param {RatingMixin} event le composant rating qui genere l update
+		 * @param {number} value la valeur choisie par l utilisateur sur le rating
+		 */
+
+		read_only_internal = this.readonly;
+
+		get length(): number {
+			switch (this.type) {
+				case RatingEnum.NUMBER: return 10;
+				case RatingEnum.STAR: return 5;
+			}
+			return 3;
+		}
+		onUpdate( value: number, event: RatingMixin): void {
 			this.read_only_internal = true;
 			event.blockon(value);
+
 		}
 	}
 </script>
