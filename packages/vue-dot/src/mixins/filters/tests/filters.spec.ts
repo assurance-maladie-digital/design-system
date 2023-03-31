75dbb2dd8d41e20355b30166abc42ae032f987db
Julien Baylac <julien.baylac@protonmail.com>
2023-03-31 10:14:05 +0200

    update tests

--- packages/vue-dot/src/mixins/filters/tests/filters.spec.ts
+++ packages/vue-dot/src/mixins/filters/tests/filters.spec.ts
@@ -2,47 +2,67 @@ import Vue from 'vue';
 import { mount, Wrapper } from '@vue/test-utils';
 
 import { FilterMixin } from '../';
-import { ChipItem } from '../types';
+import { FilterItem, ChipItem } from '../types';
 
 interface TestComponent extends Vue {
-	filters: Record<string, string>;
-	onChange(filters: Record<string, string>): string;
+	applyFunction(): unknown;
+	resetFilter(): unknown;
+	removeChip(event: Event | undefined): unknown;
+	closeSidebar(): unknown;
+	filters: Array<FilterItem[]>;
+	onChange(event: unknown, filters: FilterItem): unknown;
 	removeAccents(str: string): string;
 }
 
-const filtersExemple = [
-	{
-		name: 'nom',
-		label: 'Nom',
-		value: 'Salut',
-		formatting: (value: string): string => `La value est : ${value}`,
-		chips: []
-	},
-	{
-		name: 'date',
-		label: 'Date',
-		description: 'test description date',
-		chips: []
-	},
-	{
-		name: 'range',
-		label: 'Nombres',
-		description: 'test description range',
-		formatting: (values: number[]): string => values[0] + ' - ' + values[1],
-		limited: true,
-		chips: [],
-		value: [0,0],
-		defaultValue: [50,50]
-	},
-	{
-		name: 'prenom',
-		label: 'Prénom',
-		chips: [],
-		value: '',
-		clearAfterValidate: true,
-		style: 'margin-left: 50px !important'
-	}
-];
+const filterExemple: FilterItem = {
+	name: 'prenom',
+	label: 'Prénom',
+	chips: [],
+	value: '',
+	clearAfterValidate: true,
+	limited: false,
+	splited: false,
+	description: '',
+	showAll: false,
+	icon: '',
+	selection: [],
+	defaultValue: undefined,
+	style: ''
+};
+const limitedFilterExemple: FilterItem = {
+	name: 'prenom',
+	label: 'Prénom',
+	chips: [],
+	value: '',
+	clearAfterValidate: true,
+	limited: true,
+	splited: false,
+	description: '',
+	showAll: false,
+	icon: '',
+	selection: [],
+	defaultValue: undefined,
+	style: ''
+};
+const splitedFilterExemple: FilterItem = {
+	name: 'prenom',
+	label: 'Prénom',
+	chips: [],
+	value: '',
+	clearAfterValidate: true,
+	limited: false,
+	splited: true,
+	description: '',
+	showAll: false,
+	icon: '',
+	selection: [],
+	defaultValue: undefined,
+	style: ''
+};
+const chipItemEvent: ChipItem = {
+	text: 'nom',
+	value: 'event'
+};
 
 /** Create the test component */
 function createTestComponent() {
@@ -62,24 +82,59 @@ describe('filters', () => {
 		expect(wrapper.vm.filters).toMatchSnapshot();
 	});
 
-	it('onChange', () => {
+	it('onChange with event is null ', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		wrapper.vm.onChange(null, filterExemple);
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted('onchange-filters')).toBeFalsy();
+	});
+
+	it('onChange with event', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		wrapper.vm.onChange('event', filterExemple);
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted('onchange-filters')).toBeTruthy();
+	});
+
+	it('onChange with limited filter', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent, {
+			propsData: {
+				startDate: '2019-10-21'
+			},
+			mocks: {
+				date: '2019-10-22'
+			}
+		}) as unknown as Wrapper<TestComponent>;
+
+		wrapper.vm.onChange('event', limitedFilterExemple);
+		await wrapper.vm.$nextTick();
+		expect(wrapper.emitted('onchange-filters')).toBeTruthy();
+	});
+
+	// TODO tester return vide
+	it('onChange with a chip already exist', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		filterExemple.chips.push(chipItemEvent);
+
+		wrapper.vm.onChange(chipItemEvent, filterExemple);
+		await wrapper.vm.$nextTick();
+		expect(wrapper).toBeTruthy();
+	});
+
+	// TODO tester return vide
+	it('onChange with slited', async() => {
 		const testComponent = createTestComponent();
 		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
 
-		expect(wrapper.vm.onChange(
-			{
-				limited: 'true',
-				splited: 'false',
-				label: 'Nom',
-				name: 'nom',
-				description: '',
-				showAll: 'false',
-				icon: '',
-				selection: 'Elephant',
-				value: 'elephant',
-				style: ''
-			}
-		)).toBe('elephant');
+		wrapper.vm.onChange('event', splitedFilterExemple);
+		await wrapper.vm.$nextTick();
+		expect(wrapper.emitted('onchange-filters')).toBeTruthy();
 	});
 
 	it('removeAccents', () => {
@@ -89,4 +144,39 @@ describe('filters', () => {
 		expect(wrapper.vm.removeAccents('éléphant')).toBe('elephant');
 	});
 
+	it('closeSidebar', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		wrapper.vm.closeSidebar();
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted('close-sidebar')).toBeTruthy();
+	});
+
+	//it('removeChip', async() => {
+	//	const testComponent = createTestComponent();
+	//	const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+	//	wrapper.vm.removeChip(chipItemEvent);
+	//	await wrapper.vm.$nextTick();
+
+	//	expect(wrapper.emitted('remove-chip')).toBeTruthy();
+	//});
+
+	it('resetFilter', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		wrapper.vm.resetFilter();
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted('reset-filter')).toBeTruthy();
+	});
+
+	it('applyFunction', async() => {
+		const testComponent = createTestComponent();
+		const wrapper = mount(testComponent) as Wrapper<TestComponent>;
+		wrapper.vm.applyFunction();
+		await wrapper.vm.$nextTick();
+
+		expect(wrapper.emitted('apply-function')).toBeTruthy();
+	});
 });
