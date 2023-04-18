64cd5304da4cc8052a86f08452aff727b7a674fc
David FYON <davidfyon@gmail.com>
2023-04-18 10:35:30 +0200

    add doc

---
+++ packages/docs/src/data/examples/notification-bar/slot.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<NotificationBar v-show="showNotificationBar">
+			<template #actions>
+				<VBtn
+					color="white"
+					class="mr-3"
+					elevation="0"
+					outlined
+				>
+					Bouton
+				</VBtn>
+				<VBtn
+					color="white"
+					class="mr-3"
+					elevation="0"
+					text
+				>
+					Bouton
+				</VBtn>
+				<VBtn
+					color="primary"
+					elevation="3"
+					@click="clearNotification"
+				>
+					Fermer
+				</VBtn>
+			</template>
+		</NotificationBar>
+
+		<VBtn
+			color="primary"
+			@click="notifyUser"
+		>
+			Envoyer une notification
+		</VBtn>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from 'vue-class-component';
+
+	import { mapActions, mapState } from 'vuex';
+
+	const EXAMPLE_REF = 'divider-example';
+
+	@Component({
+		computed: mapState('notification', ['notification']),
+		methods: mapActions('notification', ['addNotification'])
+	})
+	export default class NotificationBarLabel extends Vue {
+		get showNotificationBar(): boolean {
+			return this.notification?.ref === EXAMPLE_REF;
+		}
+
+		notifyUser(): void {
+			this.addNotification({
+				ref: EXAMPLE_REF,
+				type: 'info',
+				message: 'Exemple de notification.'
+			});
+		}
+	}
+</script>
