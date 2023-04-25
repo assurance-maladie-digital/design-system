192464bc895a25a8b84b1f35267f880267985a05
David FYON <davidfyon@gmail.com>
2023-04-25 21:08:13 +0200

    ♿️ form-builder: Fix aria-label on button in PasswordField (#2752)
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/form-builder/src/components/FormField/fields/PasswordField.vue
+++ packages/form-builder/src/components/FormField/fields/PasswordField.vue
@@ -2,12 +2,23 @@
 	<VTextField
 		v-bind="field.fieldOptions"
 		:value="field.value"
-		:append-icon="showEyeIcon ? eyeIcon : eyeOffIcon"
 		:type="showEyeIcon ? 'text' : 'password'"
 		class="vd-form-input"
-		@click:append="showEyeIcon = !showEyeIcon"
 		@change="emitChangeEvent"
-	/>
+	>
+		<template #append>
+			<VBtn
+				:aria-label="btnLabel"
+				icon
+				class="mt-n2"
+				@click="showEyeIcon = !showEyeIcon"
+			>
+				<VIcon>
+					{{ showEyeIcon ? eyeIcon : eyeOffIcon }}
+				</VIcon>
+			</VBtn>
+		</template>
+	</VTextField>
 </template>
 
 <script lang="ts">
@@ -17,6 +28,11 @@
 
 	import { mdiEye, mdiEyeOff } from '@mdi/js';
 
+	const locales = {
+		hidePassword: 'Masquer le mot de passe',
+		showPassword: 'Afficher le mot de passe'
+	};
+
 	const MixinsDeclaration = mixins(FieldComponent);
 
 	@Component
@@ -25,5 +41,9 @@
 		eyeOffIcon = mdiEyeOff;
 
 		showEyeIcon = false;
+
+		get btnLabel(): string {
+			return this.showEyeIcon ? locales.hidePassword : locales.showPassword;
+		}
 	}
 </script>
