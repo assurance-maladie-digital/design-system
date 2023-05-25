aaede682a1de5dd3492506f009b16ef24e72babd
David FYON <davidfyon@gmail.com>
2023-05-25 17:17:26 +0200

    add franceConnect+ option and fix colors

--- packages/vue-dot/src/elements/FranceConnectBtn/locales.ts
+++ packages/vue-dot/src/elements/FranceConnectBtn/locales.ts
@@ -1,3 +1,5 @@
 export const locales = {
-	btnText: 'S’identifier avec FranceConnect'
+	btnText: 'S’identifier avec FranceConnect',
+	linkText: 'Qu’est-ce que FranceConnect ?',
+	linkTextPlus: 'Qu’est-ce que FranceConnect+ ?'
 };
