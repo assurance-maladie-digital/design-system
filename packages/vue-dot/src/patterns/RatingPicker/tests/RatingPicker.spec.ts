df87d33dad4316421b270e3acf36810a353f57dd
David FYON <davidfyon@gmail.com>
2023-06-01 21:10:54 +0200

    ♿️ vue-dot: Improve RatingPicker accessibility (#2795)
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/vue-dot/src/patterns/RatingPicker/tests/RatingPicker.spec.ts
+++ packages/vue-dot/src/patterns/RatingPicker/tests/RatingPicker.spec.ts
@@ -27,14 +27,4 @@ describe('RatingPicker', () => {
 
 		expect(wrapper).toMatchSnapshot();
 	});
-
-	it('sets readonly internal value on update', () => {
-		wrapper = wrapper = createTestComponent();
-
-		expect(wrapper.vm.$data.hasAnswered).toBeFalsy();
-
-		wrapper.vm.setValue(0);
-
-		expect(wrapper.vm.$data.hasAnswered).toBeTruthy();
-	});
 });
