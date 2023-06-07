edf375181fbb57c961bf745105506e453f726ad4
David FYON <davidfyon@gmail.com>
2023-06-07 17:59:24 +0200

    ♻️ vue-dot: Refactor FranceConnectBtn & add connect-plus prop (#2840)
    Co-authored-by: Dylan Broussard <deraw@outlook.fr>

--- packages/docs/src/data/api/france-connect-btn.ts
+++ packages/docs/src/data/api/france-connect-btn.ts
@@ -10,28 +10,10 @@ export const api: Api = {
 				description: 'L’URL de connexion à FranceConnect.'
 			},
 			{
-				name: 'label',
-				type: 'string',
-				default: `'S’identifier avec FranceConnect'`,
-				description: 'Le label accessible du bouton.',
-			},
-			{
-				name: 'width',
-				type: [
-					'string',
-					'number'
-				],
-				default: 300,
-				description: 'La largeur du bouton.'
-			},
-			{
-				name: 'height',
-				type: [
-					'string',
-					'number'
-				],
-				default: 78,
-				description: 'La hauteur du bouton.'
+				name: 'connect-plus',
+				type: 'boolean',
+				default: false,
+				description: 'Affiche la variante FranceConnect+.'
 			}
 		]
 	}
