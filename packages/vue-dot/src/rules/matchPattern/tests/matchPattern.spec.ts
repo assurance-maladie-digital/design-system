ec8ef0c88ed9fe8a6436f12f486fad17d52c5915
David FYON <davidfyon@gmail.com>
2023-06-08 11:59:24 +0200

    fix tests

--- packages/vue-dot/src/rules/matchPattern/tests/matchPattern.spec.ts
+++ packages/vue-dot/src/rules/matchPattern/tests/matchPattern.spec.ts
@@ -2,18 +2,30 @@ import { matchPattern } from '../';
 
 describe('matchPatternFn', () => {
 	it('should return true if the value is empty', () => {
-		const validator = matchPattern(/^[A-Z]+$/);
-		expect(validator('')).toBe(true);
+		const rule = matchPattern(/^[A-Z]+$/);
+
+		expect(rule('')).toBe(true);
 	});
 
 	it('should return true if the value matches the pattern', () => {
-		const validator = matchPattern(/^[A-Z]+$/);
-		expect(validator('ABC')).toBe(true);
+		const rule = matchPattern(/^[A-Z]+$/);
+
+		expect(rule('ABC')).toBe(true);
 	});
 
 	it('should return a custom error message if the validation fails', () => {
-		const errorMessages = { default: 'The value must match the pattern.' };
-		const validator = matchPattern(/^[A-Z]+$/, errorMessages);
-		expect(validator('abc')).toBe('The value must match the pattern.');
+		const rule = matchPattern(/^[A-Z]+$/, {
+			default: 'test'
+		});
+
+		expect(rule('abc')).toBe('test');
+	});
+
+	it('should not return a custom error message if the validation fails', () => {
+		const rule = matchPattern(/^[A-Z]+$/, {
+			default: 'test'
+		});
+
+		expect(rule('ABC')).toBe(true);
 	});
 });
