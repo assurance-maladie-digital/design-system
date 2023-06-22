8bfb8e93e568b7d5dcaca64f102cba98742fefbd
David FYON <davidfyon@gmail.com>
2023-06-22 12:27:33 +0200

    change PeriodField on FormField

--- packages/docs/src/data/examples/form-field/period.vue
+++ packages/docs/src/data/examples/form-field/period.vue
@@ -1,31 +1,19 @@
 <template>
-	<FormField v-model="field" />
+	<PeriodField
+		v-model="period"
+		outlined
+	/>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
 	import Component from 'vue-class-component';
 
-	import { Field } from '@cnamts/form-builder/src/components/FormField/types';
-
 	@Component
 	export default class FormFieldPeriod extends Vue {
-		field: Field = {
-			type: 'period',
-			value: {
-				from: null,
-				to: null
-			},
-			fieldOptions: {
-				from: {
-					label: 'Début',
-					outlined: true
-				},
-				to: {
-					label: 'Fin',
-					outlined: true
-				}
-			}
+		period = {
+			from: null,
+			to: null
 		};
 	}
 </script>
