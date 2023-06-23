f5a8b93290ad7f23ed99b32c2171edb051854aaa
David FYON <davidfyon@gmail.com>
2023-06-23 11:07:33 +0200

    fix tests

--- packages/form-builder/src/components/FormField/fields/Period.vue
+++ packages/form-builder/src/components/FormField/fields/Period.vue
@@ -10,9 +10,15 @@
 	import Vue from 'vue';
 	import Component from 'vue-class-component';
 
+	import PeriodField from '@cnamts/vue-dot/src/patterns/PeriodField';
+
 	import { Field } from '@cnamts/form-builder/src/components/FormField/types';
 
-	@Component
+	@Component({
+		components: {
+			PeriodField
+		}
+	})
 	export default class Period extends Vue {
 		field: Field = {
 			type: 'period',
