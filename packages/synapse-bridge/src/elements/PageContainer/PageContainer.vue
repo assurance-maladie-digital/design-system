cb389295aca70558887e52509a2cd036db77aa10
David FYON <davidfyon@gmail.com>
2023-09-13 09:38:22 +0200

    add vue-facing-decorator and change PageContainer code

--- packages/synapse-bridge/src/elements/PageContainer/PageContainer.vue
+++ packages/synapse-bridge/src/elements/PageContainer/PageContainer.vue
@@ -7,65 +7,58 @@
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { Component, Vue, Prop } from "vue-facing-decorator";
+
 import { useDisplay } from "vuetify";
+
 import { SizeEnum, SIZE_ENUM_VALUES } from "./SizeEnum";
 import { IndexedObject } from "../../types";
 import { propValidator } from "../../helpers/propValidator";
 
-export default {
-	props: {
-		size: {
-			type: String,
-			default: SizeEnum.X_LARGE,
-			validator: (value: string) =>
-				propValidator("size", SIZE_ENUM_VALUES, value),
-		},
-		spacing: {
-			type: String,
-			default: undefined,
-		},
-		color: {
-			type: String,
-			default: "transparent",
-		},
-	},
-	setup(props) {
-		const { name } = useDisplay();
+@Component
+export default class MyComponent extends Vue {
+	@Prop({
+		type: String,
+		default: SizeEnum.X_LARGE,
+		validator: (value: string) =>
+			propValidator("size", SIZE_ENUM_VALUES, value),
+	})
+	size!: string;
 
-		const spacingClass = computed(() => {
-			if (props.spacing) {
-				return props.spacing;
-			}
+	@Prop({ type: String, default: undefined })
+	spacing!: string | undefined;
 
-			const spacingMapping: IndexedObject<string> = {
-				xs: "px-0",
-				sm: "px-4",
-				md: "px-8",
-				lg: "px-8",
-				xl: "px-8",
-			};
+	@Prop({ type: String, default: "transparent" })
+	color!: string;
 
-			const spacingValue = spacingMapping[name.value];
+	get spacingClass(): string {
+		if (this.spacing) {
+			return this.spacing;
+		}
 
-			return `py-10 ${spacingValue}`;
-		});
-
-		const containerSize = computed(() => {
-			const sizeMapping: IndexedObject<number> = {
-				[SizeEnum.X_LARGE]: 1440,
-				[SizeEnum.LARGE]: 960,
-				[SizeEnum.MEDIUM]: 800,
-				[SizeEnum.SMALL]: 600,
-			};
-
-			return sizeMapping[props.size];
-		});
-
-		return {
-			spacingClass,
-			containerSize,
+		const spacingMapping: IndexedObject<string> = {
+			xs: "px-0",
+			sm: "px-4",
+			md: "px-8",
+			lg: "px-8",
+			xl: "px-8",
 		};
-	},
-};
+
+		const { name } = useDisplay();
+		const spacingValue = spacingMapping[name.value];
+
+		return `py-10 ${spacingValue}`;
+	}
+
+	get containerSize(): number {
+		const sizeMapping: IndexedObject<number> = {
+			[SizeEnum.X_LARGE]: 1440,
+			[SizeEnum.LARGE]: 960,
+			[SizeEnum.MEDIUM]: 800,
+			[SizeEnum.SMALL]: 600,
+		};
+
+		return sizeMapping[this.size];
+	}
+}
 </script>
