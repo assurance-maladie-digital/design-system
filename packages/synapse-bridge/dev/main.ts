a6db66551866c01cadadd7bb1e917060a0bf26f0
Dylan Broussard <contact@deraw.dev>
2023-09-14 23:56:39 +0200

    ✨ Add CopyBtn component

--- packages/synapse-bridge/dev/main.ts
+++ packages/synapse-bridge/dev/main.ts
@@ -3,7 +3,16 @@ import Playground from './Playground.vue'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+		aliases,
+		sets: {
+			mdi
+		}
+	}
+})
 
 createApp(Playground).use(vuetify).mount('#app')
