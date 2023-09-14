a03be22bf6f6e9aab3ca02a1771e8f76758da8dd
Dylan Broussard <contact@deraw.dev>
2023-09-14 23:59:20 +0200

    ✨ Add CopyBtn component (#3074)

---
+++ packages/synapse-bridge/src/elements/CopyBtn/CopyBtn.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="vd-copy-btn">
+		<VMenu
+			v-model="tooltip"
+			v-bind="options.menu"
+			:disabled="hideTooltip"
+		>
+			<template #activator="{ props }">
+				<VBtn
+					v-bind="{
+						...props,
+						...options.btn
+					}"
+					:aria-label="label"
+					@click="copy"
+				>
+					<slot name="icon">
+						<VIcon v-bind="options.icon">
+							{{ copyIcon }}
+						</VIcon>
+					</slot>
+				</VBtn>
+			</template>
+
+			<slot name="tooltip">
+				{{ locales.tooltip }}
+			</slot>
+		</VMenu>
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue'
+	import type { PropType } from 'vue'
+
+	import { config } from './config'
+	import { locales } from './locales'
+
+	import { customizable } from '../../mixins/customizable'
+	import { copyToClipboard } from '../../functions/copyToClipboard'
+
+	import { mdiContentCopy } from '@mdi/js'
+
+	export default defineComponent({
+		mixins: [customizable(config)],
+		props: {
+			label: {
+				type: String,
+				required: true
+			},
+			textToCopy: {
+				type: [Function, String] as PropType<() => string | string>,
+				required: true
+			},
+			hideTooltip: {
+				type: Boolean,
+				default: false
+			},
+			tooltipDuration: {
+				type: Number,
+				default: 2500
+			}
+		},
+		data() {
+			return {
+				tooltip: false,
+				copyIcon: mdiContentCopy,
+				locales
+			}
+		},
+		methods: {
+			copy(): void {
+				const contentToCopy = typeof this.textToCopy === 'function' ? this.textToCopy() : this.textToCopy
+
+				copyToClipboard(contentToCopy)
+
+				if (this.hideTooltip) {
+					return
+				}
+
+				setTimeout(() => {
+					this.tooltip = false
+				}, this.tooltipDuration)
+			}
+		}
+	})
+</script>
+
+<style lang="scss">
+	// Make the tooltip menu look like a tooltip
+	.vd-copy-tooltip-menu {
+		padding: 6px 16px;
+		box-shadow: none;
+		margin-top: 2px;
+		background: rgba(97, 97, 97, .9);
+	}
+</style>
---
+++ packages/synapse-bridge/src/elements/CopyBtn/config.ts
@@ -0,0 +1,16 @@
+export const config = {
+	menu: {
+		location: 'end center',
+		offset: 16,
+		zIndex: 8,
+		contentClass: 'vd-copy-tooltip-menu text-white text-body-2 ml-2'
+	},
+	btn: {
+		icon: true,
+		variant: 'text',
+		density: 'comfortable'
+	},
+	icon: {
+		color: 'grey-darken-2'
+	}
+}
