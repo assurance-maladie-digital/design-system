8cefb32b87037aa68bb32b744950a01d673a8fb1
chris <christophe.gaon@gmail.com>
2023-09-20 09:43:33 +0200

    Revert "BackBtn component (#3081)"
    This reverts commit bc72a474d2f2bf01883845772178377b2cf8e8b8.

--- packages/synapse-bridge/vite.config.ts
+++ packages/synapse-bridge/vite.config.ts
@@ -1,30 +1,30 @@
 import { resolve } from 'path'
-import { fileURLToPath } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	plugins: [vue(), vuetify()],
+	plugins: [
+		vue(),
+		vuetify()
+	],
 	build: {
 		lib: {
-			entry: resolve(__dirname, "dev/main.ts"),
-			name: "SynapseBridge",
-			fileName: "synapse-bridge",
+			entry: resolve(__dirname, 'src/main.ts'),
+			name: 'SynapseBridge',
+			fileName: 'synapse-bridge'
 		},
 		rollupOptions: {
-			external: ["vue", "vuetify"],
+			external: [
+				'vue',
+				'vuetify'
+			],
 			output: {
 				globals: {
-					vue: "Vue",
-					vuetify: "Vuetify",
-				},
-			},
-		},
-	},
-});
+					vue: 'Vue',
+					vuetify: 'Vuetify'
+				}
+			}
+		}
+	}
+})
