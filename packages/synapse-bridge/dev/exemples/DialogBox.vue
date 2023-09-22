f7fe34819ae5fe91219cdda75590d9d1ab6f3e0c
David FYON <davidfyon@gmail.com>
2023-09-22 11:29:56 +0200

    add exemple

---
+++ packages/synapse-bridge/dev/exemples/DialogBox.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<VBtn
+			color="primary"
+			@click="dialog = true"
+		>
+			Afficher le composant
+		</VBtn>
+
+		<DialogBox
+			v-model="dialog"
+			:width="dialogWidth"
+			title="Enregistrement"
+			@cancel="dialog = false"
+			@confirm="dialog = false"
+		>
+			<p>Souhaitez-vous procéder à l’enregistrement ?</p>
+		</DialogBox>
+	</div>
+</template>
+
+<script lang="ts">
+import DialogBox from "../src/elements/DialogBox";
+import { defineComponent } from "vue";
+
+import { tokens } from '@cnamts/design-tokens';
+
+export default defineComponent({
+	components: {
+		DialogBox,
+	},
+	data() {
+		return {
+			dialog: false,
+			dialogWidth: tokens.dialogWidth.dialogSmall
+		};
+	},
+});
+</script>
