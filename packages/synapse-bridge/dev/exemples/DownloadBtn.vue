e7fa54601cf3eb46230bf35bd6bebe4fae8caf97
David FYON <davidfyon@gmail.com>
2023-09-25 15:46:42 +0200

    add exemple

---
+++ packages/synapse-bridge/dev/exemples/DownloadBtn.vue
@@ -0,0 +1,36 @@
+<template>
+	<DownloadBtn :file-promise="getFileFromApi">
+		Télécharger mon attestation
+	</DownloadBtn>
+</template>
+
+<script lang="ts">
+import DownloadBtn from "../src/elements/DownloadBtn";
+import { defineComponent } from "vue";
+
+import { AxiosResponse } from '@/plugins/axios';
+
+const file: AxiosResponse<string> = {
+		data: 'test',
+		status: 200,
+		statusText: 'OK',
+		headers: {
+			'content-disposition': 'attachment; filename="attestation.txt"',
+			'content-type': 'text/plain'
+		},
+		config: {}
+	};
+
+export default defineComponent({
+	components: {
+		DownloadBtn,
+	},
+	methods: {
+		getFileFromApi(): Promise<AxiosResponse<string>> {
+			return new Promise((resolve) => {
+				setTimeout(() => resolve(file), 1500);
+			});
+		},
+	},
+});
+</script>
