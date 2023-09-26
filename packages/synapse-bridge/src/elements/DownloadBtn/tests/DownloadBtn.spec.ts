1a1388ce73659233ee3b97c527d8162dd6a6fba3
David FYON <davidfyon@gmail.com>
2023-09-26 10:35:02 +0200

    update test

--- packages/synapse-bridge/src/elements/DownloadBtn/tests/DownloadBtn.spec.ts
+++ packages/synapse-bridge/src/elements/DownloadBtn/tests/DownloadBtn.spec.ts
@@ -2,17 +2,54 @@ import { VueWrapper, shallowMount } from "@vue/test-utils";
 
 import DownloadBtn from "../";
 
+import { filePromise } from "./data/filePromise";
+
+interface TestComponent {
+	download: () => void;
+}
+
 let wrapper: VueWrapper<any>;
 
 describe("DownloadBtn", () => {
 	it("renders correctly", () => {
-		wrapper = shallowMount(DownloadBtn, {
-			propsData: {
-				label: "test",
-				textToCopy: "test",
+		wrapper = shallowMount(
+			DownloadBtn,
+			{
+				propsData: {
+					filePromise,
+				},
+				slots: {
+					default: "justificatif.pdf",
+				},
 			},
-		});
+			true
+		);
 
 		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("calls download function when pressed", async () => {
+		wrapper = shallowMount(
+			DownloadBtn,
+			{
+				propsData: {
+					filePromise,
+				},
+				slots: {
+					default: "justificatif.pdf",
+				},
+				mocks: {
+					download: jest.fn(),
+				},
+			},
+			true
+		) as Wrapper<TestComponent>;
+
+		const download = jest.spyOn(wrapper.vm as TestComponent, "download");
+		const actionBtn = wrapper.find("button");
+
+		actionBtn.trigger("click");
+
+		expect(download).toBeCalled();
 	});
 });
