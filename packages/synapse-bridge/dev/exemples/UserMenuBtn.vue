131d2acacc677cec702d45f51aa728034f4b9446
David FYON <davidfyon@gmail.com>
2023-09-29 09:31:06 +0200

    fix menu position

--- packages/synapse-bridge/dev/exemples/UserMenuBtn.vue
+++ packages/synapse-bridge/dev/exemples/UserMenuBtn.vue
@@ -1,21 +1,30 @@
 <template>
-	<UserMenuBtn full-name="Édith Cartier">
-		<VListItem
-			v-for="(item, index) in items"
-			:key="index"
-		>
-			<VListItemTitle>{{ item }}</VListItemTitle>
-		</VListItem>
-	</UserMenuBtn>
+	<PageContainer>
+		<UserMenuBtn
+			additional-information="Information supplémentaire"
+			full-name="Virginie Beauchesne"
+		/>
+		<br/><br/>
+		<UserMenuBtn full-name="Édith Cartier">
+			<VListItem
+				v-for="(item, index) in items"
+				:key="index"
+			>
+				<VListItemTitle>{{ item }}</VListItemTitle>
+			</VListItem>
+		</UserMenuBtn>
+	</PageContainer>
 </template>
 
 <script lang="ts">
 import UserMenuBtn from "../src/elements/UserMenuBtn";
+import PageContainer from "../src/elements/PageContainer";
 import { defineComponent } from "vue";
 
 export default defineComponent({
 	components: {
 		UserMenuBtn,
+		PageContainer,
 	},
 	data() {
 		return {
@@ -27,3 +36,4 @@ export default defineComponent({
 		};
 	},
 });
+</script>
