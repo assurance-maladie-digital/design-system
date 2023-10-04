ff2c7f8bd13a352eb2aad6c01b150da069b08f73
Dylan Broussard <contact@deraw.dev>
2023-10-04 19:00:25 +0200

    📝 docs: Remove unseless script tags in examples

--- packages/docs/src/data/examples/custom-icon/label.vue
+++ packages/docs/src/data/examples/custom-icon/label.vue
@@ -5,11 +5,3 @@
 		color="#000"
 	/>
 </template>
-
-<script lang="ts">
-	import Vue from 'vue';
-	import Component from 'vue-class-component';
-
-	@Component
-	export default class CustomIconLabel extends Vue {}
-</script>
