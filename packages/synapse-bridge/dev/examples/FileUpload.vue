cc067159e74b33ade9432d80aa6bc23b248a6090
David FYON <davidfyon@gmail.com>
2023-10-04 11:35:54 +0200

    update exemple

--- packages/synapse-bridge/dev/examples/FileUpload.vue
+++ packages/synapse-bridge/dev/examples/FileUpload.vue
@@ -1,11 +1,9 @@
 <template>
 	<div>
-		<FileUpload
-			v-model="file"
-			:class="{ 'mb-8': file !== null }"
-		/>
-
+		<FileUpload v-model="file" :class="{ 'mb-8': file !== null }" />
 		<FilePreview :file="file" />
+
+		<FilePreview :file="file2" :options="options" />
 	</div>
 </template>
 
@@ -23,7 +21,19 @@ export default defineComponent({
 	data() {
 		return {
 			file: null,
+			file2: null,
+
+			options: {
+				image: {
+					alt: "Photo de paysage montagneux du site Picsum.",
+				},
+			},
 		};
+	},
+	mounted() {
+		fetch("https://picsum.photos/seed/picsum/750/350")
+			.then((res) => res.blob())
+			.then((blob) => (this.file2 = blob));
 	},
 });
 </script>
