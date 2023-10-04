0b3745701e9ad35138c4ad751b352c28826ffa77
Dylan Broussard <contact@deraw.dev>
2023-10-04 19:16:27 +0200

    📝 docs: Remove unseless script tags in examples (#3143)

--- packages/docs/src/data/examples/back-btn/dark.vue
+++ packages/docs/src/data/examples/back-btn/dark.vue
@@ -6,11 +6,3 @@
 		<BackBtn dark />
 	</VSheet>
 </template>
-
-<script lang="ts">
-	import Vue from 'vue';
-	import Component from 'vue-class-component';
-
-	@Component
-	export default class BackBtnDark extends Vue {}
-</script>
