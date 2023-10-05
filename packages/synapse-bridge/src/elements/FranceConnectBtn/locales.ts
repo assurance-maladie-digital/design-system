36860733af4eb15307852eeb18a62bbcef87113a
David FYON <davidfyon@gmail.com>
2023-10-05 16:32:31 +0200

    add component + test

---
+++ packages/synapse-bridge/src/elements/FranceConnectBtn/locales.ts
@@ -0,0 +1,4 @@
+export const locales = {
+	btnLabel: (connectPlus: boolean): string => `S’identifier avec FranceConnect${connectPlus ? '+' : ''}`,
+	infoLinkLabel: (connectPlus: boolean): string => `Qu’est-ce que FranceConnect${connectPlus ? '+' : ''} ?`
+};
---
+++ packages/synapse-bridge/src/elements/FranceConnectBtn/tests/FranceConnectBtn.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+
+import FranceConnectBtn from "../";
+
+describe("FranceConnectBtn", () => {
+	it("renders correctly", () => {
+		const wrapper = shallowMount(FranceConnectBtn, {
+			propsData: {
+				href: "https://app.franceconnect.gouv.fr/",
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly with connect-plus", () => {
+		const wrapper = shallowMount(FranceConnectBtn, {
+			propsData: {
+				href: "https://app.franceconnect.gouv.fr/",
+				connectPlus: true,
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
