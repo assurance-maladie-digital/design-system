0448e782148558aa26e78fed4028bba71c51da82
David FYON <davidfyon@gmail.com>
2023-10-06 11:15:59 +0200

    rebase

--- packages/synapse-bridge/src/elements/index.ts
+++ packages/synapse-bridge/src/elements/index.ts
@@ -2,3 +2,4 @@ export { default as BackBtn } from './BackBtn'
 export { default as BackToTopBtn } from './BackToTopBtn'
 export { default as CopyBtn } from './CopyBtn'
 export { default as PageContainer } from './PageContainer'
+export { default as FranceConnectBtn } from "./FranceConnectBtn";
