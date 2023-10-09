5df37cf5b645d4750354492a9b7c78c8b6100e02
David FYON <davidfyon@gmail.com>
2023-10-09 14:27:13 +0200

    add component + test

---
+++ packages/synapse-bridge/src/elements/UserMenuBtn/config.ts
@@ -0,0 +1,26 @@
+import { tokens } from "@cnamts/design-tokens";
+
+export const config = {
+	menu: {
+		offsetY: true,
+		location: "bottom left",
+		zIndex: 4,
+		minWidth: "198px",
+		maxWidth: "198px",
+		nudgeRight: 0,
+	},
+	btn: {
+		variant: "text",
+		height: "auto",
+	},
+	icon: {
+		color: tokens.colors.cyan.darken20,
+	},
+	logoutListItem: {
+		class: "primary--text",
+	},
+	logoutIcon: {
+		color: "primary",
+		class: "mr-4",
+	},
+};
