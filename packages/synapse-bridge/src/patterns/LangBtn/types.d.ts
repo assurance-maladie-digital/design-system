f5a612e36c97f3766b6e750dd8605c031b865275
David FYON <davidfyon@gmail.com>
2023-10-12 09:24:01 +0200

    add component + test
    merge + fix code
    
    🏷️synapse-bridge : LangBtn fix ts warnings
    
    ✅ synaps-bridge: Improve tests for LangBtn
    
    fix comments
    
    ✨bridge: Update LangBtn component: handle available languages validation (from the v2) and update model value
    
    🏷️bridge: import Languages types declaration in global
    
    delete console log
    
    last changes
    
    rebase

---
+++ packages/synapse-bridge/src/patterns/LangBtn/types.d.ts
@@ -0,0 +1,15 @@
+import { IndexedObject } from '@/types';
+
+export interface Language {
+	direction: string;
+	name: string;
+	nativeName: string;
+}
+
+export type Languages = IndexedObject<Language>;
+export type AllLanguagesChar = '*';
+
+export interface CurrentLangData {
+	name: string;
+	label: string;
+}
