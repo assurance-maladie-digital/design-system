f1365ef62fea4260eaa8c56600c1670b3e2fbf14
David FYON <davidfyon@gmail.com>
2023-10-16 10:11:34 +0200

    component + test

---
+++ packages/synapse-bridge/src/templates/MaintenancePage/MaintenancePage.vue
@@ -0,0 +1,30 @@
+<template>
+	<ErrorPage
+		:page-title="locales.pageTitle"
+		:message="locales.message"
+		no-btn
+	>
+		<template #illustration>
+			<img
+				src="../../assets/images/maintenance.svg"
+				:alt="locales.pageTitle"
+				width="170px"
+				height="170px"
+			>
+		</template>
+	</ErrorPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { locales } from './locales';
+
+export default defineComponent({
+	data() {
+		return {
+			locales,
+		};
+	},
+});
+</script>
