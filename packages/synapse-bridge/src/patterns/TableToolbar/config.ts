e4a8eca376feadd9570c858941bb4da2c79753b2
David FYON <davidfyon@gmail.com>
2023-10-17 11:25:29 +0200

    add component + test
    fix test
    
    merg e+ fix code
    
    add last changes
    
    improve tests
    
    improve tests
    
    add tests
    
    fix useDisplay test
    
    improve coverage
    
    coverage
    
    refacto tests
    
    fix comments
    
    last test changes
    
    last tests changes
    
    remove useDisplay
    
    fix tests
    
    add tests and fix warning on test
    
    fix duplicated mount
    
    improve coverage
    
    bridge: Update search event name in TableToolbar.vue
    
    rebase + fixs display
    
    fix comments
    
    fix :nb-filtered
    
    fix test
    
    update tests
    
    💄vue-dot: Update TableToolbar styling
    
    update tests

---
+++ packages/synapse-bridge/src/patterns/TableToolbar/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+	toolbar: {
+		flat: true,
+		color: "#FFFFFF",
+		height: "auto",
+		minHeight: "56px",
+		class: "d-flex",
+	},
+	addBtn: {
+		variant: "outlined",
+		color: "primary",
+		class: "my-1 px-2 px-md-4",
+		minWidth: "44px",
+	},
+	addIconLabel: {
+		class: "mr-1",
+	},
+	textField: {
+		variant: "underlined",
+		clearable: true,
+		singleLine: true,
+		hideDetails: true,
+	},
+};
