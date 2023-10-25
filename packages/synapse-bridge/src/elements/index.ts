d1d0e2115bc21f67e8967a7bf82f7b283706aa07
David FYON <davidfyon@gmail.com>
2023-10-25 23:29:22 +0200

    ✨ Add ChipList component (#3144)
    Co-authored-by: Dylan Broussard <contact@deraw.dev>

--- packages/synapse-bridge/src/elements/index.ts
+++ packages/synapse-bridge/src/elements/index.ts
@@ -1,4 +1,5 @@
 export { default as BackBtn } from './BackBtn'
 export { default as BackToTopBtn } from './BackToTopBtn'
+export { default as ChipList } from './ChipList'
 export { default as CopyBtn } from './CopyBtn'
 export { default as PageContainer } from './PageContainer'
