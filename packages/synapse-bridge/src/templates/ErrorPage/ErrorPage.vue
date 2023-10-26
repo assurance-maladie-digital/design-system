7d1856e991437a827a1071021a7f8dca91747a1a
David FYON <davidfyon@gmail.com>
2023-10-26 22:30:52 +0200

    ✨ Add ErrorPage component (#3186)
    Co-authored-by: Dylan Broussard <contact@deraw.dev>

---
+++ packages/synapse-bridge/src/templates/ErrorPage/ErrorPage.vue
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import { defineComponent } from 'vue'
+	import type { PropType } from 'vue'
+
+	import { RouteRecordRaw } from 'vue-router'
+
+	import { locales } from './locales'
+
+	import { useDisplay } from 'vuetify'
+
+	import PageContainer from '@/elements/PageContainer'
+
+	export default defineComponent({
+		components: {
+			PageContainer,
+		},
+		props: {
+			pageTitle: {
+				type: String,
+				default: undefined,
+			},
+			message: {
+				type: String,
+				default: undefined,
+			},
+			code: {
+				type: String,
+				default: undefined,
+			},
+			btnText: {
+				type: String,
+				default: locales.btnText,
+			},
+			btnRoute: {
+				type: [Array, Object, String] as PropType<RouteRecordRaw | string>,
+				default: () => ({ name: 'home' }),
+			},
+			btnHref: {
+				type: String,
+				default: undefined,
+			},
+			noBtn: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		data() {
+			return {
+				locales,
+			}
+		},
+		computed: {
+			mobileVersion(): boolean {
+				const { name } = useDisplay()
+
+				return name.value === 'xs' || name.value === 'sm'
+			},
+		},
+	})
+</script>
+
+<template>
+	<PageContainer size="m">
+		<VCard
+			:elevation="mobileVersion ? 0 : 2"
+			class="pa-6 pa-sm-16"
+		>
+			<VRow class="max-width-none">
+				<VCol
+					:sm="$slots.illustration ? 6 : 12"
+					cols="12"
+					class="order-last order-sm-first text-center text-sm-left"
+				>
+					<div
+						aria-hidden="true"
+						class="vd-code font-weight-thin text-primary mb-4"
+					>
+						{{ code }}
+					</div>
+
+					<h2 class="mb-2 font-weight-bold text-h5 mb-4">
+						{{ pageTitle }}
+					</h2>
+
+					<p class="mb-0">
+						{{ message }}
+					</p>
+
+					<slot name="additional-content" />
+
+					<slot name="action">
+						<VBtn
+							v-if="!noBtn && btnText && btnRoute"
+							:to="btnRoute"
+							:href="btnHref"
+							color="primary"
+							exact
+							class="mt-6"
+						>
+							{{ btnText }}
+						</VBtn>
+					</slot>
+				</VCol>
+
+				<VCol
+					v-if="$slots.illustration"
+					cols="12"
+					sm="6"
+					class="d-flex align-center justify-center"
+				>
+					<slot name="illustration" />
+				</VCol>
+			</VRow>
+		</VCard>
+	</PageContainer>
+</template>
+
+<style lang="scss" scoped>
+	.vd-code {
+		font-size: 6rem;
+		line-height: 6rem;
+	}
+</style>
