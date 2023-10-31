d46c2588a9182039eacc61de59072b11bbbadcb5
David FYON <davidfyon@gmail.com>
2023-10-31 16:57:32 +0100

    merge + fix code

--- packages/synapse-bridge/src/patterns/DataListGroup/DataListGroup.vue
+++ packages/synapse-bridge/src/patterns/DataListGroup/DataListGroup.vue
@@ -1,30 +1,16 @@
-<template>
-	<div class="vd-data-list-group d-flex flex-wrap max-width-none ma-n4">
-		<DataList
-			v-for="(dataList, index) in items"
-			:key="'vd-data-list-' + index"
-			:loading="loading"
-			:render-html-value="renderHtmlValue"
-			:list-title="dataList.title"
-			:items="dataList.items"
-			:items-number-loading="dataList.itemsNumberLoading"
-			:heading-loading="dataList.headingLoading"
-			:width="itemWidth"
-			:icons="icons"
-			class="ma-4"
-			@click:item-action="emitItemAction(index, $event)"
-		/>
-	</div>
-</template>
-
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 
 import { DataListIcons } from "../../elements/DataList/types";
-
 import { DataListGroupItems, DataListActionEvent } from "./types";
 
-const Props = {
+import DataList from "../../elements/DataList/DataList.vue";
+
+export default defineComponent({
+	components: {
+		DataList,
+	},
+	emits: ['click:list-item'],
 	props: {
 		items: {
 			type: Array as PropType<DataListGroupItems>,
@@ -47,13 +33,6 @@ const Props = {
 			default: false,
 		},
 	},
-};
-
-export default defineComponent({
-	mixins: [Props],
-	props: {
-		...Props.props,
-	},
 	methods: {
 		emitItemAction(dataListIndex: number, itemIndex: number): void {
 			const eventValue: DataListActionEvent = {
@@ -66,3 +45,22 @@ export default defineComponent({
 	},
 });
 </script>
+
+<template>
+	<div class="vd-data-list-group d-flex flex-wrap max-width-none ma-n4">
+		<DataList
+			v-for="(dataList, index) in items"
+			:key="'vd-data-list-' + index"
+			:loading="loading"
+			:render-html-value="renderHtmlValue"
+			:list-title="dataList.title"
+			:items="dataList.items"
+			:items-number-loading="dataList.itemsNumberLoading"
+			:heading-loading="dataList.headingLoading"
+			:width="itemWidth"
+			:icons="icons"
+			class="ma-4"
+			@click:item-action="emitItemAction(index, $event)"
+		/>
+	</div>
+</template>
