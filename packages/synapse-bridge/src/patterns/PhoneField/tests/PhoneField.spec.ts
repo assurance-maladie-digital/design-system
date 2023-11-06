011a253d33f6c28fccb2fffde328eb4e72661f30
David FYON <davidfyon@gmail.com>
2023-11-06 11:33:27 +0100

    improve test

--- packages/synapse-bridge/src/patterns/PhoneField/tests/PhoneField.spec.ts
+++ packages/synapse-bridge/src/patterns/PhoneField/tests/PhoneField.spec.ts
@@ -29,4 +29,33 @@ describe("PhoneField", () => {
 
 		expect(wrapper.html()).toMatchSnapshot();
 	});
+
+	it("formats phone number correctly", () => {
+		const wrapper = shallowMount(PhoneField);
+
+		expect(wrapper.vm.formatPhone("")).toBe("");
+		expect(wrapper.vm.formatPhone("1234567890")).toBe("12 34 56 78 90");
+	});
+
+	it("counts spaces correctly", () => {
+		const wrapper = shallowMount(PhoneField);
+
+		expect(wrapper.vm.noSpacesCounter("")).toBe(0);
+		expect(wrapper.vm.noSpacesCounter("1234567890")).toBe(10);
+		expect(wrapper.vm.noSpacesCounter("12 34 56 78 90")).toBe(10);
+	});
+
+	// it("sets internal value correctly", () => {
+	// 	const wrapper = shallowMount(PhoneField);
+
+	// 	expect(wrapper.vm.setInternalValue("12 34 56 78 90")).toBe(
+	// 		"1234567890"
+	// 	);
+	// });
+
+	// it("emits change event correctly", () => {
+	// 	const wrapper = shallowMount(PhoneField);
+
+	// 	expect(wrapper.vm.emitChangeEvent).toBe("1234567890");
+	// });
 });
