da1ebd2ea9c22061073afce14f6f29c17489c488
adrien <adrien.charrier@outlook.fr>
2023-12-06 17:26:07 +0100

    🧪 synapse-bridge: adding test to UserMenuBtn component

--- packages/synapse-bridge/src/elements/UserMenuBtn/tests/UserMenuBtn.spec.ts
+++ packages/synapse-bridge/src/elements/UserMenuBtn/tests/UserMenuBtn.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import { vuetify } from "@tests/unit/setup";
 
 import UserMenuBtn from "../";
@@ -7,7 +7,7 @@ import UserMenuBtn from "../";
 describe("UserMenuBtn", () => {
 	it("renders correctly", () => {
 		const wrapper = shallowMount(UserMenuBtn, {
-			propsData: {
+			props: {
 				fullName: "Firstname Lastname",
 			},
 			global: {
@@ -17,4 +17,102 @@ describe("UserMenuBtn", () => {
 
 		expect(wrapper).toMatchSnapshot();
 	});
+
+	it("renders correctly without user icon", () => {
+		const wrapper = shallowMount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				hideUserIcon: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly in mobile mode", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				mobileVersion: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly in mobile mode with additional information", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				mobileVersion: true,
+				additionalInformation: "Additional information",
+			},
+			global: {
+				plugins: [vuetify],
+			},
+			slots: {
+				default: "<p>the menu</p>",
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly in mobile mode without user icon", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				mobileVersion: true,
+				hideUserIcon: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly without the logout button", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				hideLogoutBtn: true,
+				additionalInformation: "Additional information",
+				label: "Label",
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it("renders correctly when the breakpoint is xs", () => {
+
+	it("Display the menu on click", async () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+			},
+			global: {
+				plugins: [vuetify],
+			},
+			slots: {
+				default: "<p>the menu</p>",
+			},
+		});
+
+		await wrapper.find(".v-btn").trigger("click");
+		expect(wrapper.find(".v-btn").attributes()["aria-expanded"]).toBe("true");
+		expect(wrapper.html()).toContain("the menu");
+	});
 });
+
