077156d1e238ff6982f495018e025ed5966e0b68
David FYON <davidfyon@gmail.com>
2023-12-12 15:50:57 +0100

    improve tests

--- packages/synapse-bridge/src/elements/UserMenuBtn/tests/UserMenuBtn.spec.ts
+++ packages/synapse-bridge/src/elements/UserMenuBtn/tests/UserMenuBtn.spec.ts
@@ -5,111 +5,102 @@ import { vuetify } from '@tests/unit/setup';
 import UserMenuBtn from '../';
 
 describe('UserMenuBtn', () => {
-	it('renders correctly', () => {
+	it("renders correctly with props", () => {
 		const wrapper = shallowMount(UserMenuBtn, {
 			props: {
-				fullName: 'Firstname Lastname',
-			},
-			global: {
-				plugins: [vuetify],
-			},
-		});
-
-		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('renders correctly without user icon', () => {
-		const wrapper = shallowMount(UserMenuBtn, {
-			props: {
-				fullName: 'Firstname Lastname',
+				fullName: "Firstname Lastname",
+				label: "Label",
 				hideUserIcon: true,
-			},
-			global: {
-				plugins: [vuetify],
-			},
-		});
-
-		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('renders correctly in mobile mode', () => {
-		const wrapper = mount(UserMenuBtn, {
-			props: {
-				fullName: 'Firstname Lastname',
-				mobileVersion: true,
-			},
-			global: {
-				plugins: [vuetify],
-			},
-		});
-
-		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('renders correctly in mobile mode with additional information', () => {
-		const wrapper = mount(UserMenuBtn, {
-			props: {
-				fullName: 'Firstname Lastname',
-				mobileVersion: true,
-				additionalInformation: 'Additional information',
-			},
-			global: {
-				plugins: [vuetify],
-			},
-			slots: {
-				default: '<p>the menu</p>',
-			},
-		});
-
-		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('renders correctly in mobile mode without user icon', () => {
-		const wrapper = mount(UserMenuBtn, {
-			props: {
-				fullName: 'Firstname Lastname',
-				mobileVersion: true,
-				hideUserIcon: true,
-			},
-			global: {
-				plugins: [vuetify],
-			},
-		});
-
-		expect(wrapper).toMatchSnapshot();
-	});
-
-	it('renders correctly without the logout button', () => {
-		const wrapper = mount(UserMenuBtn, {
-			props: {
-				fullName: 'Firstname Lastname',
 				hideLogoutBtn: true,
-				additionalInformation: 'Additional information',
-				label: 'Label',
+				mobileVersion: true,
+				additionalInformation: "Additional information",
 			},
 			global: {
 				plugins: [vuetify],
+			},
+			slots: {
+				default: "<p>the menu</p>",
 			},
 		});
 
 		expect(wrapper).toMatchSnapshot();
 	});
 
-	it('Display the menu on click', async () => {
+	it("renders btnPadding correctly", () => {
 		const wrapper = mount(UserMenuBtn, {
 			props: {
-				fullName: 'Firstname Lastname',
+				fullName: "Firstname Lastname",
+				mobileVersion: true,
+				additionalInformation: "Additional information",
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper.vm.btnPadding).toBe("pa-0");
+	});
+
+	it("renders btnPadding correctly", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				hideUserIcon: true,
+				additionalInformation: "Additional information",
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper.vm.btnPadding).toBe("pa-1 pa-sm-2");
+	});
+
+	it("renders hasListContent correctly", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				hideLogoutBtn: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper.vm.hasListContent).toBe(false);
+	});
+
+	it("renders isMobileVersion computed correctly", () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
+				mobileVersion: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		expect(wrapper.vm.isMobileVersion).toBe(true);
+	});
+
+	it("Display the menu on click", async () => {
+		const wrapper = mount(UserMenuBtn, {
+			props: {
+				fullName: "Firstname Lastname",
 			},
 			global: {
 				plugins: [vuetify],
 			},
 			slots: {
-				default: '<p>the menu</p>',
+				default: "<p>the menu</p>",
 			},
 		});
 
-		await wrapper.find('.v-btn').trigger('click');
-		expect(wrapper.find('.v-btn').attributes()['aria-expanded']).toBe('true');
-		expect(wrapper.html()).toContain('the menu');
+		await wrapper.find(".v-btn").trigger("click");
+		expect(wrapper.find(".v-btn").attributes()["aria-expanded"]).toBe(
+			"true"
+		);
+		expect(wrapper.html()).toContain("the menu");
 	});
 });
