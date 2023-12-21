035966aee1b347edc0f3d7b53de73294c7c8d6b6
David FYON <davidfyon@gmail.com>
2023-12-21 12:33:24 +0100

    fix tests

--- packages/synapse-bridge/src/patterns/SearchListField/tests/SearchListField.spec.ts
+++ packages/synapse-bridge/src/patterns/SearchListField/tests/SearchListField.spec.ts
@@ -1,17 +1,79 @@
 import { describe, it, expect } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { vuetify } from "@tests/unit/setup";
 
 import SearchListField from "../";
 
 describe("SearchListField", () => {
 	it("renders correctly", () => {
-		const wrapper = shallowMount(SearchListField, {
+		const wrapper = mount(SearchListField, {
 			global: {
 				plugins: [vuetify],
+			},
+			propsData: {
+				items: [
+					{
+						id: 1,
+						name: "Item 1",
+					},
+					{
+						id: 2,
+						name: "Item 2",
+					},
+				],
 			},
 		});
 
 		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	it("filteredItems computed property", async () => {
+		const wrapper = mount(SearchListField, {
+			global: {
+				plugins: [vuetify],
+			},
+			propsData: {
+				items: [
+					{
+						label: "Item 1",
+						value: 1,
+					},
+					{
+						label: "Item 2",
+						value: 2,
+					},
+				],
+			},
+		});
+
+		expect(wrapper.vm.filteredItems).toHaveLength(2);
+
+		wrapper.vm.search = "Item 1";
+		await wrapper.vm.$nextTick();
+		expect(wrapper.vm.filteredItems).toHaveLength(1);
+	});
+
+	it("emitChangeEvent method", async () => {
+		const wrapper = mount(SearchListField, {
+			global: {
+				plugins: [vuetify],
+			},
+			propsData: {
+				items: [
+					{
+						label: "Item 1",
+						value: 1,
+					},
+					{
+						label: "Item 2",
+						value: 2,
+					},
+				],
+			},
+		});
+
+		wrapper.vm.emitChangeEvent();
+		await wrapper.vm.$nextTick();
+		expect(wrapper.emitted("update:modelValue")).toBeTruthy();
 	});
 });
