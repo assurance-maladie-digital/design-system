5362aaf75a44b9a86e4b4e38ec294ed93236fb4b
adrien <adrien.charrier@outlook.fr>
2023-12-21 16:13:28 +0100

    ✅ vue-dot: Test the NirField errors

--- packages/vue-dot/src/patterns/NirField/tests/NirField.spec.ts
+++ packages/vue-dot/src/patterns/NirField/tests/NirField.spec.ts
@@ -15,21 +15,50 @@ describe('NirField', () => {
 	});
 
 	it('renders correctly with 13 characters', () => {
-		wrapper = mountComponent(NirField, {
-			propsData: {
-				nirLength: 13
-			}
-		}, true);
+		wrapper = mountComponent(
+			NirField,
+			{
+				propsData: {
+					nirLength: 13
+				}
+			},
+			true
+		);
 
 		expect(wrapper).toMatchSnapshot();
 	});
 
 	it('renders correctly with a tooltip', () => {
-		wrapper = mountComponent(NirField, {
-			propsData: {
-				tooltip: 'Information sur le NIR'
-			}
-		}, true);
+		wrapper = mountComponent(
+			NirField,
+			{
+				propsData: {
+					tooltip: 'Information sur le NIR'
+				}
+			},
+			true
+		);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Displays an error message when the fields are not totaly filled', async() => {
+		wrapper = mountComponent(
+			NirField,
+			{
+			},
+			true
+		);
+
+		document.getSelection = jest.fn(() => null);
+
+		const numberField = wrapper.find('.vd-number-field input');
+		const keyField = wrapper.find('.vd-key-field input');
+
+		await numberField.setValue('123');
+		await numberField.trigger('keydown.enter');
+		await keyField.setValue('1');
+		await keyField.trigger('keydown.enter');
 
 		expect(wrapper).toMatchSnapshot();
 	});
