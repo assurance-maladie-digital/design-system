127242027b0a0ca0728ce7b43807313e575489e5
adrien <adrien.charrier@outlook.fr>
2024-01-02 13:40:24 +0100

    ✅ synaps-bridge: PasswordField improve tests

--- packages/synapse-bridge/src/patterns/PasswordField/tests/PasswordField.spec.ts
+++ packages/synapse-bridge/src/patterns/PasswordField/tests/PasswordField.spec.ts
@@ -1,12 +1,47 @@
 import { describe, it, expect } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 
 import PasswordField from "../";
+import { vuetify } from "@tests/unit/setup";
 
 describe("PasswordField", () => {
 	it("renders correctly", () => {
-		const wrapper = shallowMount(PasswordField);
+		const wrapper = mount(PasswordField, {
+			global: {
+				plugins: [vuetify],
+			},
+		});
 
 		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	it('renders correctly with the eye icon', async() => {
+		const wrapper = mount(PasswordField, {
+			props: {
+				showEyeIcon: true,
+				required: true,
+			},
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		const showBtn = wrapper.find('button');
+		expect(showBtn.attributes('aria-label')).toBe('Afficher le mot de passe');
+		await showBtn.trigger('click');
+		expect(showBtn.attributes('aria-label')).toBe('Masquer le mot de passe');
+	});
+
+	it('emit an event when the value change', async() => {
+		const wrapper = mount(PasswordField, {
+			global: {
+				plugins: [vuetify],
+			},
+		});
+
+		const input = wrapper.find('input');
+		await input.setValue('test');
+
+		expect(wrapper.emitted('change')).toContainEqual(['test']);
 	});
 });
