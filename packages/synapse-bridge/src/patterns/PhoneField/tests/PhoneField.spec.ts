130c5ef04db9df9a1c5d9e6f94b24f8e896b1e31
adrien <adrien.charrier@outlook.fr>
2024-01-02 14:39:44 +0100

    ✅ synaps-bridge: Fix tests for phoneField

--- packages/synapse-bridge/src/patterns/PhoneField/tests/PhoneField.spec.ts
+++ packages/synapse-bridge/src/patterns/PhoneField/tests/PhoneField.spec.ts
@@ -1,17 +1,25 @@
 import { describe, it, expect } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 
 import PhoneField from "../";
+import { vuetify } from "@tests/unit/setup";
 
 describe("PhoneField", () => {
 	it("renders correctly", () => {
-		const wrapper = shallowMount(PhoneField);
+		const wrapper = mount(PhoneField,{
+			global: {
+				plugins: [vuetify],
+			}
+		});
 
 		expect(wrapper.html()).toMatchSnapshot();
 	});
 
 	it("renders correctly with required prop", () => {
-		const wrapper = shallowMount(PhoneField, {
+		const wrapper = mount(PhoneField, {
+			global: {
+				plugins: [vuetify],
+			},
 			props: {
 				required: true,
 			},
@@ -21,7 +29,10 @@ describe("PhoneField", () => {
 	});
 
 	it("renders correctly with outlined prop", () => {
-		const wrapper = shallowMount(PhoneField, {
+		const wrapper = mount(PhoneField, {
+			global: {
+				plugins: [vuetify],
+			},
 			props: {
 				outlined: true,
 			},
@@ -31,7 +42,10 @@ describe("PhoneField", () => {
 	});
 
 	it("renders correctly with value prop", () => {
-		const wrapper = shallowMount(PhoneField, {
+		const wrapper = mount(PhoneField, {
+			global: {
+				plugins: [vuetify],
+			},
 			props: {
 				value: "1234 567890",
 			},
@@ -41,14 +55,20 @@ describe("PhoneField", () => {
 	})
 
 	it("formats phone number correctly", () => {
-		const wrapper = shallowMount(PhoneField);
+		const wrapper = mount(PhoneField,{global: {
+			plugins: [vuetify],
+		}});
 
 		expect(wrapper.vm.formatPhone("")).toBe("");
 		expect(wrapper.vm.formatPhone("1234567890")).toBe("12 34 56 78 90");
 	});
 
 	it("counts spaces correctly", () => {
-		const wrapper = shallowMount(PhoneField);
+		const wrapper = mount(PhoneField,{
+			global: {
+				plugins: [vuetify],
+			}
+		});
 
 		expect(wrapper.vm.noSpacesCounter("")).toBe(0);
 		expect(wrapper.vm.noSpacesCounter("1234567890")).toBe(10);
@@ -56,7 +76,11 @@ describe("PhoneField", () => {
 	});
 
 	it("sets internal value correctly", () => {
-		const wrapper = shallowMount(PhoneField);
+		const wrapper = mount(PhoneField,{
+			global: {
+				plugins: [vuetify],
+			}
+		});
 
 		wrapper.vm.setInternalValue({
 			target: {
@@ -68,7 +92,11 @@ describe("PhoneField", () => {
 	});
 
 	it("emits change event correctly", () => {
-		const wrapper = shallowMount(PhoneField);
+		const wrapper = mount(PhoneField,{
+			global: {
+				plugins: [vuetify],
+			}
+		});
 
 		wrapper.vm.emitChangeEvent();
 
