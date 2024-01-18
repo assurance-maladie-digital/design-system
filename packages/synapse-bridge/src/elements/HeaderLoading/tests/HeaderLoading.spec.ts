788454895f5ea23c3fb27f4af2464ad34d1414e6
adrien <adrien.charrier@outlook.fr>
2024-01-18 11:59:25 +0100

    ✅vue-dot: Fix tests for vuetify 3.4

--- packages/synapse-bridge/src/elements/HeaderLoading/tests/HeaderLoading.spec.ts
+++ packages/synapse-bridge/src/elements/HeaderLoading/tests/HeaderLoading.spec.ts
@@ -1,12 +1,17 @@
-import { describe, it, expect } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { describe, it, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
 
-import HeaderLoading from '../'
+import HeaderLoading from "../";
+import { vuetify } from "@tests/unit/setup";
 
-describe('HeaderLoading', () => {
-	it('renders correctly', () => {
-		const wrapper = shallowMount(HeaderLoading)
+describe("HeaderLoading", () => {
+	it("renders correctly", () => {
+		const wrapper = shallowMount(HeaderLoading, {
+			global: {
+				plugins: [vuetify],
+			},
+		});
 
-		expect(wrapper).toMatchSnapshot()
-	})
-})
+		expect(wrapper).toMatchSnapshot();
+	});
+});
