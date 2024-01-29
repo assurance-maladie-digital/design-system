58324977bb837aec5ced85c9e7ea2b49de194059
adrien <adrien.charrier@outlook.fr>
2024-01-29 13:23:53 +0100

    🚚v3: Add missing folders

---
+++ packages/design-system-v3/src/functions/validation/isWeekend/tests/isWeekend.spec.ts
@@ -0,0 +1,20 @@
+import {
+	describe,it,expect
+} from 'vitest';
+
+import { isWeekend } from '../';
+
+
+describe('isWeekend', () => {
+	it('returns true if the day is in a weekend', () => {
+		const date = '2019-10-26'; // Saturday
+
+		expect(isWeekend(date)).toBe(true);
+	});
+
+	it('returns false if the day is not in a weekend', () => {
+		const date = '2019-10-21'; // Monday
+
+		expect(isWeekend(date)).toBe(false);
+	});
+});
