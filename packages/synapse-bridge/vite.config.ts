e4efeffedc2334310ae882f0546138f3d215eed1
David FYON <davidfyon@gmail.com>
2024-02-06 13:54:03 +0100

    adtransformAssetUrls

--- packages/synapse-bridge/vite.config.ts
+++ packages/synapse-bridge/vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig, type Plugin } from 'vite'
 import { resolve } from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import dts from 'vite-plugin-dts'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig(({ mode }) => {
 	const config = {
 		plugins: [
-			vue(),
+			vue({
+				template: { transformAssetUrls }
+			}),
 			vuetify(
 				{
 					autoImport: true
