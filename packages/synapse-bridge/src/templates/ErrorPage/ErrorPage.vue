af2e491992caa3cfc4d8ad0336699f8aaeec05ae
David FYON <davidfyon@gmail.com>
2024-02-12 11:14:21 +0100

    Fix useDisplay use in components (#3384)

--- packages/synapse-bridge/src/templates/ErrorPage/ErrorPage.vue
+++ packages/synapse-bridge/src/templates/ErrorPage/ErrorPage.vue
@@ -6,8 +6,6 @@
 
 	import { locales } from './locales'
 
-	import { useDisplay } from 'vuetify'
-
 	import PageContainer from '@/elements/PageContainer'
 
 	export default defineComponent({
@@ -46,14 +44,12 @@
 		},
 		data() {
 			return {
-				locales,
+				locales
 			}
 		},
 		computed: {
 			mobileVersion(): boolean {
-				const { name } = useDisplay()
-
-				return name.value === 'xs' || name.value === 'sm'
+				return this.$vuetify.display.name === 'xs' || this.$vuetify.display.name === 'sm'
 			},
 		},
 	})
