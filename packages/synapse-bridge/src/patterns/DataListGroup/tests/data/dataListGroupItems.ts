427c5c2d0025f535f9b3730e174df8b09842728b
David FYON <davidfyon@gmail.com>
2024-02-16 11:57:32 +0100

    DataListGroup v3 (#3170)
    Co-authored-by: adrien <adrien.charrier@outlook.fr>

---
+++ packages/synapse-bridge/src/patterns/DataListGroup/tests/data/dataListGroupItems.ts
@@ -0,0 +1,41 @@
+import { DataList } from '../../../../elements/DataList/types';
+import { DataListGroupItems } from '../../types';
+
+export const dataListItems: DataList = [
+	{
+		key: 'Libellé',
+		value: 'Texte saisi'
+	},
+	{
+		key: 'Libellé',
+		value: 'Texte saisi'
+	}
+];
+
+export const dataListItemsActions: DataList = [
+	{
+		key: 'Libellé',
+		value: 'Texte à modifier',
+		action: 'Modifier'
+	},
+	{
+		key: 'Libellé',
+		value: 'Texte à modifier',
+		action: 'Modifier'
+	}
+];
+
+export const dataListGroupItems: DataListGroupItems = [
+	{
+		title: 'Catégorie 1',
+		items: dataListItems,
+		itemsNumberLoading: 2,
+		headingLoading: true
+	},
+	{
+		title: 'Catégorie 2',
+		items: dataListItemsActions,
+		itemsNumberLoading: 2,
+		headingLoading: true
+	}
+];
--- packages/synapse-bridge/src/patterns/index.ts
+++ packages/synapse-bridge/src/patterns/index.ts
@@ -1,3 +1,4 @@
 export { default as ContextualMenu } from './ContextualMenu'
 export { default as CookieBanner } from './CookieBanner'
 export { default as FooterWrapper } from './FooterWrapper'
+export { default as DataListGroup } from './DataListGroup'
