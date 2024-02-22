8a27aca9fc5ab7e5f16a7ad26ba7fa59d55aeda2
David FYON <davidfyon@gmail.com>
2024-02-22 16:05:06 +0100

    MaintenancePage v3 (#3191)

---
+++ packages/synapse-bridge/src/templates/MaintenancePage/MaintenancePage.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { locales } from './locales';
+
+import ErrorPage from "@/templates/ErrorPage";
+
+export default defineComponent({
+	components: {
+		ErrorPage,
+	},
+	data() {
+		return {
+			locales,
+		};
+	},
+});
+</script>
+
+<template>
+	<ErrorPage
+		:page-title="locales.pageTitle"
+		:message="locales.message"
+		no-btn
+	>
+		<template #illustration>
+			<img
+				src="@/assets/images/maintenance.svg"
+				:alt="locales.pageTitle"
+			>
+		</template>
+	</ErrorPage>
+</template>
+
+<style lang="scss" scoped>
+img {
+	width: 170px;
+	height: 170px;
+}
+</style>
--- packages/synapse-bridge/src/templates/index.ts
+++ packages/synapse-bridge/src/templates/index.ts
@@ -1,3 +1,4 @@
 export { default as CookiesPage } from './CookiesPage'
 export { default as ErrorPage } from './ErrorPage'
 export { default as NotFoundPage } from './NotFoundPage'
+export { default as MaintenancePage } from './MaintenancePage'
