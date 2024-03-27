d690f1cda69bef9dd436341a74e9c3ab7c35460d
virginieVachet <vachetvirginie@gmail.com>
2024-03-27 13:47:15 +0100

    wip datapicker

--- packages/synapse-bridge/src/patterns/DatePicker/DatePicker.vue
+++ packages/synapse-bridge/src/patterns/DatePicker/DatePicker.vue
@@ -1,223 +1,254 @@
+<template>
+	<div class="d-flex">
+		<v-col cols="6">
+			<div v-if="!birthdate && !period">
+				<VueDatePicker
+					v-model="date"
+					ref="myDatePicker"
+					:placeholder="placeholder"
+					:enable-time-picker="false"
+					:hide-input-icon="noPrependIcon"
+					:format="format"
+					:disabled="disabled"
+					:clearable="clearable"
+					:text-input="noCalendar ? textInputOptions : false"
+					:class="{'placeholder-no-icon': noPrependIcon || appendIcon }"
+					calendar-cell-class-name="dp-custom-cell"
+					auto-apply
+					hide-offset-dates
+					locale="fr"
+					@open="blockOpenOnclick"
+					@close="handleClose"
+				>
+					<template #input-icon>
+						<VIcon @click="handleIconClick">
+							{{ calendarIcon }}
+						</VIcon>
+					</template>
+					<template #day="{ date }">
+						<div :class="showWeekends && isWeekend(date) ? 'week-ends' : ''">
+							{{ date.getDate() }}
+						</div>
+					</template>
+				</VueDatePicker>
+				<p class="hint">Format JJ/MM/AAAA</p>
+			</div>
+			<div v-if="birthdate">
+				<VueDatePicker
+					v-model="date"
+					calendar-cell-class-name="dp-custom-cell"
+					hide-offset-dates
+					locale="fr"
+					auto-apply
+					:format="format"
+					:enable-time-picker="false"
+					:flow="['year', 'month', 'calendar']"
+					:placeholder="placeholder"
+					@open="blockOpenOnclick"
+					@close="handleClose"
+				>
+					<template #input-icon>
+						<VIcon @click="handleIconClick">
+							{{ calendarIcon }}
+						</VIcon>
+					</template>
+				</VueDatePicker>
+				<p class="hint">Format JJ/MM/AAAA</p>
+			</div>
+			<div v-if="period">
+				<VueDatePicker
+					append-icon="mdi-calendar"
+					v-model="startDate"
+					:format="format"
+					:placeholder="placeholder"
+					:enable-time-picker="false"
+					calendar-cell-class-name="dp-custom-cell"
+					auto-apply
+					hide-offset-dates
+					locale="fr"
+					@open="blockOpenOnclick"
+					@close="handleClose"
+				/>
+
+				<VueDatePicker
+					append-icon="mdi-calendar"
+					v-model="date"
+					:placeholder="placeholder"
+					:enable-time-picker="false"
+					calendar-cell-class-name="dp-custom-cell"
+					auto-apply
+					hide-offset-dates
+					locale="fr"
+					range
+					@open="blockOpenOnclick"
+					@close="handleClose"
+				>
+					<template #input-icon>
+						<VIcon @click="handleIconClick">
+							{{ calendarIcon }}
+						</VIcon>
+					</template>
+				</VueDatePicker>
+			</div>
+		</v-col>
+	</div>
+</template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
-import type { PropType } from "vue";
-
-import { config } from "./config";
-import { locales } from "./locales";
-
-import { customizable, type Options } from "../../mixins/customizable";
-import { WarningRules } from "../../mixins/warningRules";
-
-import { DateLogic } from "./mixins/dateLogic";
-import { MaskValue } from "./mixins/maskValue";
-import { ErrorProp } from "./mixins/errorProp";
-import { Birthdate } from "./mixins/birthdate";
-
-import { mdiCalendar } from "@mdi/js";
-import { vMaska } from "maska";
-
-import deepMerge from "deepmerge";
-import { VDatePicker } from "vuetify/components/VDatePicker";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+import {mdiCalendar} from "@mdi/js";
 
 export default defineComponent({
-	components: {
-		VDatePicker,
-	},
-	inheritAttrs: false,
-	directives: { maska: vMaska },
-	emits: ["update:modelValue"],
-	mixins: [
-		customizable(config),
-		WarningRules,
-		DateLogic,
-		MaskValue,
-		ErrorProp,
-		Birthdate,
-	],
+	components: { VueDatePicker },
 	props: {
-		noCalendar: {
+		birthdate: {
 			type: Boolean,
-			default: false,
-		},
-		noPrependIcon: {
-			type: Boolean,
-			default: false,
-		},
-		outlined: {
-			type: Boolean,
-			default: false,
+			default: false
 		},
 		appendIcon: {
 			type: Boolean,
-			default: false,
+			default: false
 		},
-		textFieldActivator: {
+		noCalendar: {
 			type: Boolean,
-			default: false,
+			default: false
+		},
+		noPrependIcon: {
+			type: Boolean,
+			default: false
 		},
 		disabled: {
 			type: Boolean,
-			default: false,
+			default: false
 		},
-		textFieldClass: {
-			type: [String, Array] as PropType<string | string[]>,
-			default: undefined,
+		showWeekends: {
+			type: Boolean,
+			default: false
+		},
+		textFieldActivator: {
+			type: Boolean,
+			default: false
+		},
+		outlined: {
+			type: Boolean,
+			default: false
 		},
 	},
 	data() {
 		return {
-			locales: locales,
+			date: '',
+			dateStart: new Date(),
+			placeholder: 'Date',
+			format: 'dd/MM/yyyy',
+			clearable: false,
+			period: false,
+			startDate: new Date(),
+			textInputOptions: {
+				openMenu: false,
+				format: 'dd/MM/yyyy',
+			},
+			isCalOpen: false,
 			calendarIcon: mdiCalendar,
-			menuOpen: false,
-		};
+			disallowOutlined:true
+		}
 	},
-	computed: {
-		showPrependIcon(): boolean {
-			return !this.noPrependIcon && !this.showAppendIcon;
+	watch: {
+		appendIcon(newVal: boolean) {
+			this.applyIconStyle(newVal);
 		},
-
-		showAppendIcon(): boolean {
-			return this.appendIcon || this.outlined;
+		disallowOutlined(newVal: boolean) {
+			this.applyDisallowOutlined(newVal);
 		},
-
-		menuOptions(): Options {
-			return deepMerge<Options>(this.menuOptions, this.options.menu);
+		startDate(newVal: Date) {
+			const startDate = this.dateStart || new Date();
+			this.date = [startDate];
 		},
-
-		textFieldOptions(): Options {
-			return deepMerge<Options>(this.options?.textField, this.$attrs);
-		},
-
-		textFieldClasses() {
-			const classes: (string | Record<string, boolean>)[] = [
-				{
-					"vd-warning-rules": this.warningRules.length > 0 && this.messages.length > 0 && !this.errorMessages,
-					"vd-no-prepend-icon": !this.showPrependIcon,
-				},
-			];
-
-			return classes.concat(this.textFieldClass || []);
-		},
-
-		menuOffset() {
-			const right = this.outlined ? -18 : 0;
-			const bottom = -22;
-			return [bottom, right];
-		},
-
-		hint() {
-			return !!this.textFieldOptions.successMessages || !!this.messages.length
-				? undefined
-				: this.textFieldOptions.hint;
-		},
+	},
+	mounted() {
+			this.applyIconStyle(this.appendIcon);
+			this.applyIconStyle(this.appendIcon);
+			// outline is true by default in lib
+			this.applyDisallowOutlined(!this.outlined);
+			this.applyDisallowOutlined(!this.outlined);
 	},
 	methods: {
-		textFieldClicked(): void {
-			if (this.textFieldActivator) {
-				this.menuOpen = true;
+		isWeekend(date: Date): boolean {
+			const dayOfWeek = date.getDay();
+			return dayOfWeek === 0 || dayOfWeek === 6;
+		},
+		handleIconClick(event: Event) {
+			const myDatePicker = this.$refs.myDatePicker as typeof VueDatePicker;
+				event.stopPropagation();
+				this.isCalOpen = !this.isCalOpen;
+				this.isCalOpen && !this.noCalendar ? myDatePicker.openMenu() : myDatePicker.closeMenu();
+		},
+		blockOpenOnclick() {
+			const myDatePicker = this.$refs.myDatePicker as typeof VueDatePicker;
+			return !this.isCalOpen && !this.textFieldActivator && !this.noPrependIcon ? myDatePicker.closeMenu() : this.isCalOpen = !this.isCalOpen
+		},
+		handleClose() {
+			this.isCalOpen = false;
+		},
+		applyIconStyle(appendIcon: boolean): void {
+			const icon = this.$el.querySelector('.dp__input_icon');
+			if (icon) {
+				icon.style.marginLeft = appendIcon ? '95%' : '';
 			}
 		},
-	},
-});
+		applyDisallowOutlined(disallowOutlined: boolean): void {
+			const input = this.$el.querySelector('.dp__input');
+			const borderBottomStyle = '2px solid #ced4da';
+			const noBorderStyle = 'none';
+
+			if (input) {
+				console.log(input.style)
+				input.style.borderBottomLeftRadius = '0';
+				input.style.borderBottomRightRadius = '0';
+				input.style.border = disallowOutlined ? noBorderStyle : '';
+				input.style.borderBottom = disallowOutlined ? borderBottomStyle : '';
+			}
+		}
+	}
+})
 </script>
 
-<template>
-	<VMenu
-		ref="menu"
-		v-bind="menuOptions"
-		v-model="menuOpen"
-		:offset="menuOffset"
-	>
-		<template #activator="{ props }">
-			<div v-bind="props">
-				<VTextField
-					ref="input"
-					v-maska:[maskValue]
-					v-bind="textFieldOptions"
-					:hint="hint"
-					v-model="textFieldDate"
-					:variant="outlined ? 'outlined' : undefined"
-					:class="textFieldClasses"
-					:messages="
-						textFieldOptions.successMessages || messages
-					"
-					:error-messages="
-						textFieldOptions.errorMessages || errorMessages
-					"
-					v-model:error="internalErrorProp"
-					:disabled="disabled"
-					class="vd-date-picker-text-field"
-					@blur="textFieldBlur"
-					@click.stop="textFieldClicked"
-					@paste.prevent="saveFromPasted"
-					@keydown.enter.prevent="saveFromTextField"
-				>
-					<template #prepend>
-						<VBtn
-							v-show="showPrependIcon"
-							v-bind="options.btn"
-							:aria-label="locales.openCalendar"
-							:disabled="disabled"
-							@click.stop="menuOpen = !menuOpen"
-						>
-							<slot name="prepend-icon">
-								<VIcon v-bind="options.icon">
-									{{ calendarIcon }}
-								</VIcon>
-							</slot>
-						</VBtn>
-					</template>
-
-					<template #append-inner>
-						<VBtn
-							v-show="showAppendIcon"
-							v-bind="options.btn"
-							:aria-label="locales.openCalendar"
-							:disabled="disabled"
-							@click.stop="menuOpen = !menuOpen"
-						>
-							<slot name="append-icon">
-								<VIcon v-bind="options.icon">
-									{{ calendarIcon }}
-								</VIcon>
-							</slot>
-						</VBtn>
-					</template>
-
-					<!--
-						Pass down slots
-						@see https://stackoverflow.com/questions/50891858/vue-how-to-pass-down-slots-inside-wrapper-component/52823029#52823029
-					-->
-					<slot
-						v-for="name in Object.keys($slots)"
-						:slot="name"
-						:name="name"
-					/>
-				</VTextField>
-			</div>
-		</template>
-		<VDatePicker
-			v-if="!noCalendar"
-			v-model="dateObject"
-			v-model:viewMode="activePicker"
-			v-bind="options.datePicker"
-			@update:modelValue="saveFromCalendar"
-			@update:year="changePicker"
-			:max="options.datePicker.max || max"
-			:min="options.datePicker.min || min"
-		/>
-	</VMenu>
-</template>
-
-<style lang="scss" scoped>
-@import '@cnamts/design-tokens/dist/tokens';
-.vd-date-picker-menu {
-	// Hide scrollbar in VMenu
-	overflow: hidden;
+<style lang="scss">
+.week-ends {
+	background-color: #B3B4B5;
+	border-radius: 55%;
+	width: 27px;
+	height: 25px;
+	display: inline-block;
+	padding-bottom: 1px;
+}
+.dp-custom-cell {
+	border-radius: 50%;
+}
+.dp__today {
+	border: 1px solid #2EB5E4;
+}
+.dp__active_date {
+	background: #2EB5E4;
+	color: var(--dp-primary-text-color);
+}
+.dp__input::placeholder {
+	margin-left: -10px !important;
+}
+.hint {
+	color: #888;
+	font-size: 0.8em;
+}
+.placeholder-no-icon {
+	.dp__input_icon_pad{
+		padding-inline-start: 0 !important;
+	}
 }
 
-:deep(.vd-warning-rules) {
-	color: $vd-warning !important;
-	caret-color: $vd-warning !important;
+.dp__disabled {
+	background: transparent;
+	opacity: 0.8;
 }
+
 </style>
