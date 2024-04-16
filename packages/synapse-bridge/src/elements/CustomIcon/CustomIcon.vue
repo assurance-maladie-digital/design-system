2d2cedbdacbf2da6dd7020f8830a9cddb75beca5
David FYON <davidfyon@gmail.com>
2024-04-16 11:29:13 +0200

    DownloadBtn v3 (#3151)
    add component
    
    last changes
    
    improve test
    
    fix warnings
    
    merge + fix code
    
    last changes
    
    try fix
    
    simplify component
    
    CustomIcon : use VIcon
    
    CustomIcon : add tests units
    
    fix component + tests
    
    last changes
    
    fix type
    
    fix tests
    
    add icons

---
+++ packages/synapse-bridge/src/elements/CustomIcon/CustomIcon.vue
@@ -0,0 +1,139 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { themeIcon } from "./mixins/themeIcon";
+
+export default defineComponent({
+	mixins: [themeIcon],
+	props: {
+		/**
+		 * The color of the icon (any CSS value)
+		 * By default it's inherited from the text color
+		 */
+		color: {
+			type: String,
+			default: "currentColor",
+		},
+		/** Make the icon small (1em) */
+		small: {
+			type: Boolean,
+			default: false,
+		},
+		/** Make the icon medium (1.5em) */
+		medium: {
+			type: Boolean,
+			default: false,
+		},
+		/** Make the icon large (1.85em) */
+		large: {
+			type: Boolean,
+			default: false,
+		},
+		/** Make the icon extra large (2.25em) */
+		xLarge: {
+			type: Boolean,
+			default: false,
+		},
+		/**
+		 * The CSS size of the icon (width/height)
+		 * By default, the size is 1.35em (between small & medium)
+		 */
+		size: {
+			type: String,
+			default: undefined,
+		},
+		/**
+		 * The accessible label of the icon
+		 * By default, the icon is hidden from screen readers
+		 */
+		label: {
+			type: String,
+			default: undefined,
+		},
+	},
+	methods: {
+		getIcon() {
+			if (!this.icon) {
+				return undefined
+			}
+
+			return this.getThemeIcon();
+		},
+	},
+});
+</script>
+
+<template>
+	<VIcon
+		:class="{
+			'vd-small': small,
+			'vd-medium': medium,
+			'vd-large': large,
+			'vd-x-large': xLarge,
+			'vd-custom-color': Boolean(color),
+		}"
+		:style="{
+			color,
+			width: size,
+			height: size,
+		}"
+		:aria-hidden="!label"
+		:aria-label="label"
+		role="img"
+		focusable="false"
+		class="vd-custom-icon"
+	>
+		<slot>
+			<span v-html="getIcon()" />
+		</slot>
+	</VIcon>
+</template>
+
+<style lang="scss" scoped>
+$default: 1.35em;
+$small: 1em;
+$medium: 1.5em;
+$large: 1.85em;
+$xLarge: 2.25em;
+
+.vd-custom-icon {
+	width: $default;
+	height: $default;
+	display: inline-block;
+	vertical-align: middle;
+	// Position relative the container since
+	// the child will be positioned absolutely
+	position: relative;
+	:deep(svg) {
+		// Make the SVG element take all space
+		// in the parent, with position absolute
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		// Transition when color is updated
+		transition: fill 0.15s;
+	}
+	&.vd-custom-color svg {
+		// Use color from parent,
+		// this makes the icon the color of the prop
+		fill: currentColor;
+	}
+	&.vd-small {
+		width: $small;
+		height: $small;
+	}
+	&.vd-medium {
+		width: $medium;
+		height: $medium;
+	}
+	&.vd-large {
+		width: $large;
+		height: $large;
+	}
+	&.vd-x-large {
+		width: $xLarge;
+		height: $xLarge;
+	}
+}
+</style>
