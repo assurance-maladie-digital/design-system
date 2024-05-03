e44c145e175ec7dfe467683a0343dba81d2176f7
adrien charrier <adrien.charrier@outlook.fr>
2024-05-03 13:10:18 +0200

    📝docs: Add migration documentation

---
+++ packages/docs/src/components/global/DocMigrationFaq.vue
@@ -0,0 +1,13 @@
+<template>
+	<VExpansionPanels>
+		<VExpansionPanel>
+			<VExpansionPanelHeader class="font-weight-bold">
+				Quel est le starter kit a utiliser pour migrer un projet VueJS 2 vers VueJS 3 ?
+			</VExpansionPanelHeader>
+
+			<VExpansionPanelContent>
+				Il faut utiliser le starter kit `starter-kit-vue-bridge`.
+			</VExpansionPanelContent>
+		</VExpansionPanel>
+	</VExpansionPanels>
+</template>
