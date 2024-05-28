612546d5cadbb2a4781cb10befac5fbea1586e7c
virginieVachet <vachetvirginie@gmail.com>
2024-05-28 16:36:08 +0200

    fix imgs in headers

--- packages/synapse-bridge/src/elements/LogoBrandSection/secondaryLogoMapping.ts
+++ packages/synapse-bridge/src/elements/LogoBrandSection/secondaryLogoMapping.ts
@@ -1,25 +1,28 @@
 import { IndexedObject } from "@/types";
-
 import { ThemeEnum } from "@/constants/enums/ThemeEnum";
-
 import { locales } from "./locales";
 import { LogoInfo } from "./types";
 
+import logoCnam from "../../assets/images/cnam.svg";
+import logoAmeli from "../../assets/images/ameli.svg";
+import logoAmeliPro from "../../assets/images/ameli-pro.svg";
+import logoCompteAmeli from "../../assets/images/compte-ameli.svg";
+
 export const secondaryLogoMapping: IndexedObject<LogoInfo> = {
 	[ThemeEnum.CNAM]: {
-		src: "../../../src/assets/images/cnam.svg",
+		src: logoCnam,
 		alt: locales.logoCnam,
 	},
 	[ThemeEnum.AMELI]: {
-		src: "../../../src/assets/images/ameli.svg",
+		src: logoAmeli,
 		alt: locales.logoAmeli,
 	},
 	[ThemeEnum.AMELI_PRO]: {
-		src: "../../../src/assets/images/ameli-pro.svg",
+		src: logoAmeliPro,
 		alt: locales.logoAmeliPro,
 	},
 	[ThemeEnum.COMPTE_AMELI]: {
-		src: "../../../src/assets/images/compte-ameli.svg",
+		src: logoCompteAmeli,
 		alt: locales.logoCompteAmeli,
 	},
 };
