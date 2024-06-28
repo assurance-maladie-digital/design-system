cb9d88f964c580ad3e6dd5bcf7a1370130a0ad75
David FYON <davidfyon@gmail.com>
2024-06-28 15:29:41 +0200

    add missing type (#3558)

---
+++ packages/synapse-bridge/src/rules/notAfterDate/locales.ts
@@ -0,0 +1,5 @@
+import { ErrorMessages } from "../types";
+
+export const defaultErrorMessages: ErrorMessages = {
+	default: (date: string) => `La date doit être antérieure ou égale au ${date}.`
+};
