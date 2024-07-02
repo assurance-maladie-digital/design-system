9f19c9ed44b69f5fa3b5c0ad0a4f117222acdfec
David FYON <davidfyon@gmail.com>
2024-07-02 13:56:21 +0200

    redo off

--- packages/synapse-bridge/eslint.config.js
+++ packages/synapse-bridge/eslint.config.js
@@ -3,7 +3,7 @@ export default [
         rules: {
             'vue/html-self-closing': 'off',
             'vue/multi-word-component-names': 'off',
-			'@typescript-eslint/no-explicit-any': 0,
+			'@typescript-eslint/no-explicit-any': 'off'
         },
     },
 ]
