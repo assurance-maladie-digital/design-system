bb2d6b6cfa51e853f4e528884b6c3e67f5ebff4f
virginieVachet <vachetvirginie@gmail.com>
2024-07-12 12:18:36 +0200

    fix h1/h2 font

--- packages/synapse-bridge/src/elements/LogoBrandSection/LogoBrandSection.vue
+++ packages/synapse-bridge/src/elements/LogoBrandSection/LogoBrandSection.vue
@@ -312,6 +312,7 @@ export default defineComponent({
 
 	.vd-title {
 		line-height: 1.45 !important;
+		font-family: 'Source Sans 3', 'Source Sans Pro', sans-serif !important;
 	}
 
 	.vd-compte-entreprise-title {
