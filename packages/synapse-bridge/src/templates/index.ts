618662d9678b931626c03e13f0dfcd176d317b61
adrien charrier <adrien.charrier@outlook.fr>
2024-07-16 15:36:17 +0200

    chore: Update package.json to use wildcard imports for functions, modules, helpers, and rules

--- packages/synapse-bridge/src/templates/index.ts
+++ packages/synapse-bridge/src/templates/index.ts
@@ -1,4 +1,6 @@
 export { default as CookiesPage } from './CookiesPage'
+export { TypeEnum } from './CookiesPage/CookiesInformation/TypeEnum'
+export { type CookiesPageItems } from './CookiesPage/types'
 export { default as ErrorPage } from './ErrorPage'
 export { default as MaintenancePage } from './MaintenancePage'
 export { default as NotFoundPage } from './NotFoundPage'
