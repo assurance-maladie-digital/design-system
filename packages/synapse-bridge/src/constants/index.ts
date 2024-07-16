dab38ec13733d70dd89bdb40494cf838f974ac31
virginieVachet <vachetvirginie@gmail.com>
2024-07-16 16:23:55 +0200

    add forgotten type for paginatedTable

--- packages/synapse-bridge/src/constants/index.ts
+++ packages/synapse-bridge/src/constants/index.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { formatDate } from '../functions/formatDate',
+import { formatDate } from '../functions/formatDate'
 import {StateEnum} from './enums/StateEnum'
 import { ThemeEnum, THEME_ENUM_VALUES } from './enums/ThemeEnum'
 
