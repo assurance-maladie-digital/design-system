d3a1542e8d703491b7580e77406b8f8307a4da55
David FYON <davidfyon@gmail.com>
2024-08-02 16:47:42 +0200

    add test

--- packages/synapse-bridge/src/rules/notAfterToday/index.ts
+++ packages/synapse-bridge/src/rules/notAfterToday/index.ts
@@ -4,7 +4,7 @@ import { ValidationRule, ValidationResult, ErrorMessages, Value } from '../types
 import { defaultErrorMessages } from './locales';
 import { TODAY } from '../../constants';
 
-function formatDateToDDMMYYYY(date: Date): string {
+export function formatDateToDDMMYYYY(date: Date): string {
 	const day = String(date.getDate()).padStart(2, '0');
 	const month = String(date.getMonth() + 1).padStart(2, '0');
 	const year = date.getFullYear();
