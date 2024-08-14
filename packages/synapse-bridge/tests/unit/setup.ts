f44e00c15712f29a3264f7c75446dc7a07261272
virginieVachet <vachetvirginie@gmail.com>
2024-08-14 16:48:05 +0200

    clear globally mocks and timers after tests

--- packages/synapse-bridge/tests/unit/setup.ts
+++ packages/synapse-bridge/tests/unit/setup.ts
@@ -1,20 +1,19 @@
-import vueSnapshotSerializer from 'jest-serializer-vue'
-import { expect } from 'vitest'
+import vueSnapshotSerializer from 'jest-serializer-vue';
+import { expect, beforeEach, afterEach } from 'vitest';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import Vuex from 'vuex';
+import { state, actions, mutations } from '@/modules/notification';
+import { vitest } from 'vitest'
 
-expect.addSnapshotSerializer(vueSnapshotSerializer)
-
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-import Vuex from 'vuex'
-import { state, actions, mutations } from '@/modules/notification'
-
+// Configuration de Vuetify
 export const vuetify = createVuetify({
 	components,
 	directives,
-})
+});
 
+// Fonction pour créer un store Vuex personnalisé
 export function createNotificationStore() {
 	return new Vuex.Store({
 		modules: {
@@ -25,7 +24,22 @@ export function createNotificationStore() {
 				mutations,
 			},
 		},
-	})
+	});
 }
 
-global.ResizeObserver = require('resize-observer-polyfill')
+// Ajout du sérialiseur de snapshots pour Vue
+expect.addSnapshotSerializer(vueSnapshotSerializer);
+
+// Polyfill pour ResizeObserver
+global.ResizeObserver = require('resize-observer-polyfill');
+
+
+// Ajout des hooks globaux pour isoler les tests et eviter les faux positifs
+beforeEach(() => {
+	document.body.innerHTML = ''; // Réinitialisation du DOM
+});
+
+afterEach(() => {
+	vitest.clearAllMocks(); // Réinitialisation des mocks
+	vitest.clearAllTimers(); // Réinitialisation des timers
+});
