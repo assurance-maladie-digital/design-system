89f5d71061139208a736ab574bd6573aab84da09
David FYON <davidfyon@gmail.com>
2024-10-22 10:50:34 +0200

    fix test

--- packages/vue-dot/src/elements/DialogBox/tests/DialogBox.spec.ts
+++ packages/vue-dot/src/elements/DialogBox/tests/DialogBox.spec.ts
@@ -1,16 +1,24 @@
 import Vue from 'vue';
 import { Wrapper } from '@vue/test-utils';
-
 import { mountComponent } from '@/tests';
-
 import DialogBox from '../DialogBox.vue';
+import { VDialog } from 'vuetify/lib';
 
 let wrapper: Wrapper<Vue>;
 
 describe('DialogBox', () => {
-	it('renders correctly', () => {
-		wrapper = mountComponent(DialogBox);
+	it('renders correctly', async() => {
+		wrapper = mountComponent(DialogBox, {
+			propsData: {
+				dialog: true,
+				title: 'Title'
+			}
+		}, true);
 
-		expect(wrapper).toMatchSnapshot();
+		await Vue.nextTick();
+
+		const dialog = wrapper.findComponent(VDialog);
+
+		expect(dialog.exists()).toBe(true);
 	});
 });
