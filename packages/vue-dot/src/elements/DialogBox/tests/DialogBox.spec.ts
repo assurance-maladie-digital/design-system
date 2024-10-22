e3bab15e063b3cb020ca6aed5e16ba96a4f7a4ea
David FYON <davidfyon@gmail.com>
2024-10-22 15:02:11 +0200

    DialogBox: fix aria-label (#3815)

--- packages/vue-dot/src/elements/DialogBox/tests/DialogBox.spec.ts
+++ packages/vue-dot/src/elements/DialogBox/tests/DialogBox.spec.ts
@@ -1,16 +1,26 @@
 import Vue from 'vue';
 import { Wrapper } from '@vue/test-utils';
-
 import { mountComponent } from '@/tests';
-
 import DialogBox from '../DialogBox.vue';
+import { VDialog, VCard } from 'vuetify/lib';
 
 let wrapper: Wrapper<Vue>;
 
 describe('DialogBox', () => {
-	it('renders correctly', () => {
-		wrapper = mountComponent(DialogBox);
+	it('renders correctly', async() => {
+		wrapper = mountComponent(DialogBox, {
+			propsData: {
+				value: true,
+				title: 'Title'
+			}
+		}, true);
 
-		expect(wrapper).toMatchSnapshot();
+		await Vue.nextTick();
+
+		const dialog = wrapper.findComponent(VDialog);
+		const title = wrapper.findComponent(VCard).find('h2').text();
+
+		expect(dialog.exists()).toBe(true);
+		expect(title).toBe('Title');
 	});
 });
